661a807c65a154eccdddb875b45e4782ca86132c
Philipp Hagemeister <phihag@phihag.de>
2012-01-08 17:23:10 +0100

    Release 2012.01.08b

--- youtube_dl/__init__.py
+++ youtube_dl/__init__.py
@@ -18,7 +18,7 @@ __authors__  = (
 	)
 
 __license__ = 'Public Domain'
-__version__ = '2012.01.08'
+__version__ = '2012.01.08b'
 
 UPDATE_URL = 'https://raw.github.com/rg3/youtube-dl/master/youtube-dl'
 
