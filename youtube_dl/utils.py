3c4fc580bbf7a37b4ed0f244010c248840d86afe
Juan Carlos Garcia Segovia <jcarlosgarciasegovia@gmail.com>
2012-06-06 13:24:12 +0000

    Use an User-Agent that will allow downloading from blip.tv fixes #325

--- youtube_dl/utils.py
+++ youtube_dl/utils.py
@@ -19,7 +19,7 @@ except ImportError:
 	import StringIO
 
 std_headers = {
-	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:5.0.1) Gecko/20100101 Firefox/5.0.1',
+	'User-Agent': 'iTunes/10.6.1',
 	'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	'Accept-Encoding': 'gzip, deflate',
