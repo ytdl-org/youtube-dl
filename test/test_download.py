137c5803c35f3e06d821ede1ef69733c3b8d338c
gcmalloc <gcmalloc@gmail.com>
2012-10-03 14:25:54 +0200

    some changes to keep the same standard

--- test/test_download.py
+++ test/test_download.py
@@ -7,31 +7,30 @@ from youtube_dl.FileDownloader import FileDownloader
 from youtube_dl.InfoExtractors  import YoutubeIE
 
 class DownloadTest(unittest.TestCase):
-    #calculated with the md5sum utility
-    #md5sum (GNU coreutils) 8.19
-    YOUTUBE_MD5 = "8547978241cb87dd6782b10b8e90acc3"
-    YOUTUBE_URL = "http://www.youtube.com/watch?v=BaW_jenozKc"
-    YOUTUBE_FILE = "BaW_jenozKc.flv"
+	#calculated with md5sum:
+	#	md5sum (GNU coreutils) 8.19
+	YOUTUBE_MD5 = "8547978241cb87dd6782b10b8e90acc3"
+	YOUTUBE_URL = "http://www.youtube.com/watch?v=BaW_jenozKc"
+	YOUTUBE_FILE = "BaW_jenozKc.flv"
 
-    def test_youtube(self):
-        #let's download a file from youtube
-        global YOUTUBE_URL
-        fd = FileDownloader({})
-        fd.add_info_extractor(YoutubeIE())
-        fd.download([DownloadTest.YOUTUBE_URL])
-        self.assertTrue(os.path.exists(DownloadTest.YOUTUBE_FILE))
-        md5_down_file = md5_for_file(DownloadTest.YOUTUBE_FILE)
-        self.assertEqual(md5_down_file, DownloadTest.YOUTUBE_MD5)
+	def test_youtube(self):
+		#let's download a file from youtube
+		fd = FileDownloader({})
+		fd.add_info_extractor(YoutubeIE())
+		fd.download([DownloadTest.YOUTUBE_URL])
+		self.assertTrue(os.path.exists(DownloadTest.YOUTUBE_FILE))
+		md5_down_file = md5_for_file(DownloadTest.YOUTUBE_FILE)
+		self.assertEqual(md5_down_file, DownloadTest.YOUTUBE_MD5)
 
-    def cleanUp(self):
-        if os.path.exists(DownloadTest.YOUTUBE_FILE):
-            os.remove(DownloadTest.YOUTUBE_FILE)
+	def cleanUp(self):
+		if os.path.exists(DownloadTest.YOUTUBE_FILE):
+			os.remove(DownloadTest.YOUTUBE_FILE)
 
 def md5_for_file(f, block_size=2**20):
-    md5 = hashlib.md5()
-    while True:
-        data = f.read(block_size)
-        if not data:
-            break
-        md5.update(data)
-        return md5.digest()
+	md5 = hashlib.md5()
+	while True:
+		data = f.read(block_size)
+		if not data:
+			break
+		md5.update(data)
+		return md5.digest()
--- test/test_utils.py
+++ test/test_utils.py
@@ -12,35 +12,33 @@ from youtube_dl.utils import orderedSet
 
 
 class TestUtil(unittest.TestCase):
-    def test_timeconvert(self):
-        self.assertIsNone(timeconvert(''))
-        self.assertIsNone(timeconvert('bougrg'))
+	def test_timeconvert(self):
+		self.assertIsNone(timeconvert(''))
+		self.assertIsNone(timeconvert('bougrg'))
 
-    def test_sanitize_filename(self):
-        self.assertEqual(sanitize_filename(u'abc'), u'abc')
-        self.assertEqual(sanitize_filename(u'abc_d-e'), u'abc_d-e')
+	def test_sanitize_filename(self):
+		self.assertEqual(sanitize_filename(u'abc'), u'abc')
+		self.assertEqual(sanitize_filename(u'abc_d-e'), u'abc_d-e')
 
-        self.assertEqual(sanitize_filename(u'123'), u'123')
+		self.assertEqual(sanitize_filename(u'123'), u'123')
 
-        self.assertEqual(u'abc_de', sanitize_filename(u'abc/de'))
-        self.assertIn(u'de', sanitize_filename(u'abc/de'))
-        self.assertNotIn(u'/', sanitize_filename(u'abc/de///'))
+		self.assertEqual(u'abc_de', sanitize_filename(u'abc/de'))
+		self.assertIn(u'de', sanitize_filename(u'abc/de'))
+		self.assertNotIn(u'/', sanitize_filename(u'abc/de///'))
 
-        self.assertEqual(u'abc_de', sanitize_filename(u'abc\\de'))
-        self.assertEqual(u'abc_de', sanitize_filename(u'abc\\de'))
-        self.assertIn(u'de', sanitize_filename(u'abc\\de'))
+		self.assertEqual(u'abc_de', sanitize_filename(u'abc\\de'))
+		self.assertEqual(u'abc_de', sanitize_filename(u'abc\\de'))
+		self.assertIn(u'de', sanitize_filename(u'abc\\de'))
 
-        self.assertEqual(sanitize_filename(u'ä'), u'ä')
-        self.assertEqual(sanitize_filename(u'кириллица'), u'кириллица')
+		self.assertEqual(sanitize_filename(u'ä'), u'ä')
+		self.assertEqual(sanitize_filename(u'кириллица'), u'кириллица')
 
-    def test_ordered_set(self):
-        self.assertEqual(orderedSet([1,1,2,3,4,4,5,6,7,3,5]), [1,2,3,4,5,6,7])
-        self.assertEqual(orderedSet([]), [])
-        self.assertEqual(orderedSet([1]), [1])
-        #keep the list ordered
-        self.assertEqual(orderedSet([135,1,1,1]), [135,1])
+	def test_ordered_set(self):
+		self.assertEqual(orderedSet([1,1,2,3,4,4,5,6,7,3,5]), [1,2,3,4,5,6,7])
+		self.assertEqual(orderedSet([]), [])
+		self.assertEqual(orderedSet([1]), [1])
+		#keep the list ordered
+		self.assertEqual(orderedSet([135,1,1,1]), [135,1])
 
-    def test_unescape_html(self):
-        self.assertEqual(unescapeHTML(u"%20;"), u"%20;")
-
-
+	def test_unescape_html(self):
+		self.assertEqual(unescapeHTML(u"%20;"), u"%20;")
