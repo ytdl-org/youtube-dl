36a9c0b5ffa857baeb8a97651ec223bd4b0d1a6d
Christopher Neugebauer <chrisjrn@gmail.com>
2012-11-27 21:49:27 +1100

    Points the ComedyCentral extractor at a CDN which works with more RTMPDump versions.

--- youtube_dl/InfoExtractors.py
+++ youtube_dl/InfoExtractors.py
@@ -2361,6 +2361,18 @@ class ComedyCentralIE(InfoExtractor):
 			# For now, just pick the highest bitrate
 			format,video_url = turls[-1]
 
+			# Patch to download from alternative CDN, which does not 
+                        # break on current RTMPDump builds
+            
+
+			print "HELLO, WORLD!", video_url
+			broken_cdn = "rtmpe://viacomccstrmfs.fplive.net/viacomccstrm/gsp.comedystor/"
+			better_cdn = "rtmpe://cp10740.edgefcs.net/ondemand/mtvnorigin/gsp.comedystor/"
+            
+			if video_url.startswith(broken_cdn):
+                            video_url = video_url.replace(broken_cdn, better_cdn)
+                    
+
 			effTitle = showId + u'-' + epTitle
 			info = {
 				'id': shortMediaId,
@@ -2372,7 +2384,7 @@ class ComedyCentralIE(InfoExtractor):
 				'format': format,
 				'thumbnail': None,
 				'description': officialTitle,
-				'player_url': playerUrl
+				'player_url': None #playerUrl
 			}
 
 			results.append(info)
