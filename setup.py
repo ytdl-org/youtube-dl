caaa47d37215f498c033afb42972c135be8138d4
gcmalloc <gcmalloc@gmail.com>
2012-11-29 14:12:06 +0100

    adding the script hook

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from distutils.core import setup
+from distutils.core import setup, Command
 import sys
 try:
     import py2exe
 except ImportError:
     sys.stderr.write("Cannot import py2exe")
-import os
 import subprocess
 
 """The p2exe option will create an exe that needs Microsoft Visual C++ 2008 Redistributable Package.
@@ -12,12 +11,16 @@ import subprocess
    You can also build a zip executable with
     python setup.py bdist --format=zip
 
+   The test suite can be run with
+    python setup.py test
 
+
+    The actual version is defined by the last git tag
 """
 
 # If run without args, build executables
-if len(sys.argv) == 1:
-    sys.argv.append("py2exe")
+#if len(sys.argv) == 1:
+#    sys.argv.append("py2exe")
 
 # os.chdir(os.path.dirname(os.path.abspath(sys.argv[0]))) # conflict with wine-py2exe.sh
 #sys.path.append('./youtube_dl')
@@ -38,21 +41,25 @@ console = [{
 init_file = open('./youtube_dl/__init__.py')
 
 try:
+    #return the last tag name
     version = subprocess.checkoutput(["git", "describe", "--abbrev=0", "--tags"])
 except:
     version = ''
 
 setup(name='youtube-dl',
       version=version,
-      description='Small command-line program to download videos from YouTube.com and other video sites',
+      long_description='Small command-line program to download videos from YouTube.com and other video sites',
       url='https://github.com/rg3/youtube-dl',
       packages=['youtube_dl'],
-
-      console = console,
-      options = {"py2exe": options},
-      zipfile = None,
+      #test suite
+      test_suite='nose.collector',
+      test_requires=['nosetest'],
+      console=console,
+      options={"py2exe": options},
+      scripts=['bin/youtube-dl'],
+      zipfile=None,
 )
 
-import shutil
-shutil.rmtree("build")
+#import shutil
+#shutil.rmtree("build")
 
