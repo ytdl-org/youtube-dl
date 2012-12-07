5a304a7637c033dea00ff46d6980075b63d17f24
Filippo Valsorda <filippo.valsorda@gmail.com>
2012-12-08 00:48:07 +0100

    new updating scheme, based on GH downloads; also, check if not updateable (pip installed)

--- youtube_dl/__main__.py
+++ youtube_dl/__main__.py
@@ -7,6 +7,7 @@
 import sys
 
 if __package__ is None and not hasattr(sys, "frozen"):
+    # direct call of __main__.py
     import os.path
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
