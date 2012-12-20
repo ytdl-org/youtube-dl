af42895612d0958f5c20499c5adafb25270fdb8d
Philipp Hagemeister <phihag@phihag.de>
2012-12-20 13:26:21 +0100

    Extend json info data / description file test

--- test/test_write_info_json.py
+++ test/test_write_info_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+# coding: utf-8
 
+import json
 import os
 import socket
 import sys
@@ -31,9 +33,16 @@ with io.open(PARAMETERS_FILE, encoding='utf-8') as pf:
     params = json.load(pf)
 params['writeinfojson'] = True
 params['skip_download'] = True
+params['writedescription'] = True
 
 TEST_ID = 'BaW_jenozKc'
 INFO_JSON_FILE = TEST_ID + '.mp4.info.json'
+DESCRIPTION_FILE = TEST_ID + '.mp4.description'
+EXPECTED_DESCRIPTION = u'''test chars:  "'/\ä↭𝕐
+
+This is a test video for youtube-dl.
+
+For more information, contact phihag@phihag.de .'''
 
 class TestInfoJSON(unittest.TestCase):
     def setUp(self):
@@ -46,10 +55,25 @@ class TestInfoJSON(unittest.TestCase):
         fd.add_info_extractor(ie)
         fd.download([TEST_ID])
         self.assertTrue(os.path.exists(INFO_JSON_FILE))
+        with io.open(INFO_JSON_FILE, 'r', encoding='utf-8') as jsonf:
+            jd = json.load(jsonf)
+        self.assertEqual(jd['upload_date'], u'20121002')
+        self.assertEqual(jd['description'], EXPECTED_DESCRIPTION)
+        self.assertEqual(jd['id'], TEST_ID)
+        self.assertEqual(jd['extractor'], 'youtube')
+        self.assertEqual(jd['title'], u'''youtube-dl test video "'/\ä↭𝕐''')
+        self.assertEqual(jd['uploader'], 'Philipp Hagemeister')
+
+        self.assertTrue(os.path.exists(DESCRIPTION_FILE))
+        with io.open(DESCRIPTION_FILE, 'r', encoding='utf-8') as descf:
+            descr = descf.read()
+        self.assertEqual(descr, EXPECTED_DESCRIPTION)
 
     def tearDown(self):
         if os.path.exists(INFO_JSON_FILE):
             os.remove(INFO_JSON_FILE)
+        if os.path.exists(DESCRIPTION_FILE):
+            os.remove(DESCRIPTION_FILE)
 
 if __name__ == '__main__':
     unittest.main()
