71c8858fcd1212b5cf7bb85b69f1bf63d5634992
Filippo Valsorda <filippo.valsorda@gmail.com>
2012-12-31 16:54:34 +0100

    fixes

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <?php
 $DOWNLOAD_DIR = 'downloads';
 
-$versions = array_filter(function($v) {return $v{0} != '.';}, scandir($DOWNLOAD_DIR));
+$versions = array_filter(scandir($DOWNLOAD_DIR), function($v) {return $v{0} != '.';});
 sort($versions);
 
 $latest = end($versions);
@@ -21,12 +21,12 @@ echo '<div class="latest">';
 echo '<div>Latest (v' . htmlspecialchars($latest) . ') downloads:</div>';
 echo '<a href="downloads/' . htmlspecialchars($latest) . '/youtube-dl">youtube-dl</a> ';
 echo '<a href="downloads/' . htmlspecialchars($latest) . '/youtube-dl.exe">youtube-dl.exe</a> ';
-echo '<a href="downloads/' . htmlspecialchars($latest) . '/youtube-dl-src-' . htmlspecialchars($latest) . '.tar.gz">youtube-dl-src-' . htmlspecialchars($latest) . '.tar.gz</a>';
+echo '<a href="downloads/' . htmlspecialchars($latest) . '/youtube-dl-' . htmlspecialchars($latest) . '.tar.gz">youtube-dl-' . htmlspecialchars($latest) . '.tar.gz</a>';
 echo '</div>';
 
 echo '<ul>';
-foreach ($versions as $v) {
-    echo '<li><a href="' . htmlspecialchars($version) . '">' . htmlspecialchars($version) . '</a></li>';
+foreach ($versions as $version) {
+    echo '<li><a href="downloads/' . htmlspecialchars($version) . '">' . htmlspecialchars($version) . '</a></li>';
 }
 echo '</ul>';
 
