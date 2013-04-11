ae183c84fa4bf36f995e3c097601ac9cf72207ba
Adam Mesha <adam@mesha.org>
2013-04-11 07:33:18 +0300

    Resolve the symlink if __main__.py is invoke as a symlink.

--- youtube_dl/__main__.py
+++ youtube_dl/__main__.py
@@ -9,7 +9,8 @@ import sys
 if __package__ is None and not hasattr(sys, "frozen"):
     # direct call of __main__.py
     import os.path
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    path = os.path.realpath(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(os.path.dirname(path)))
 
 import youtube_dl
 
