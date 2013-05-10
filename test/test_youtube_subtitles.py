057c9938a157bbf370097f60b37793ae94d2c411
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-05-10 08:37:49 +0200

    Import FileDownloader in test_youtube_subtitles
    Fix last commit

--- test/test_youtube_subtitles.py
+++ test/test_youtube_subtitles.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from youtube_dl.InfoExtractors import YoutubeIE
 from youtube_dl.utils import *
+from youtube_dl import FileDownloader
 
 PARAMETERS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "parameters.json")
 with io.open(PARAMETERS_FILE, encoding='utf-8') as pf:
