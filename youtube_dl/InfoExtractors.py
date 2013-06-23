9f5daf0006091124799e13c834db17201ebdbbc5
Philipp Hagemeister <phihag@phihag.de>
2013-06-23 22:27:16 +0200

    [redtube] move into own file

--- youtube_dl/InfoExtractors.py
+++ youtube_dl/InfoExtractors.py
@@ -48,6 +48,7 @@ from .extractor.statigram import StatigramIE
 from .extractor.photobucket import PhotobucketIE
 from .extractor.pornotube import PornotubeIE
 from .extractor.rbmaradio import RBMARadioIE
+from .extractor.redtube import RedTubeIE
 from .extractor.soundcloud import SoundcloudIE, SoundcloudSetIE
 from .extractor.spiegel import SpiegelIE
 from .extractor.stanfordoc import StanfordOpenClassroomIE
@@ -102,34 +103,6 @@ from .extractor.zdf import ZDFIE
 
 
 
-class RedTubeIE(InfoExtractor):
-    """Information Extractor for redtube"""
-    _VALID_URL = r'(?:http://)?(?:www\.)?redtube\.com/(?P<id>[0-9]+)'
-
-    def _real_extract(self,url):
-        mobj = re.match(self._VALID_URL, url)
-        if mobj is None:
-            raise ExtractorError(u'Invalid URL: %s' % url)
-
-        video_id = mobj.group('id')
-        video_extension = 'mp4'        
-        webpage = self._download_webpage(url, video_id)
-
-        self.report_extraction(video_id)
-
-        video_url = self._html_search_regex(r'<source src="(.+?)" type="video/mp4">',
-            webpage, u'video URL')
-
-        video_title = self._html_search_regex('<h1 class="videoTitle slidePanelMovable">(.+?)</h1>',
-            webpage, u'title')
-
-        return [{
-            'id':       video_id,
-            'url':      video_url,
-            'ext':      video_extension,
-            'title':    video_title,
-        }]
-        
 class InaIE(InfoExtractor):
     """Information Extractor for Ina.fr"""
     _VALID_URL = r'(?:http://)?(?:www\.)?ina\.fr/video/(?P<id>I[0-9]+)/.*'
