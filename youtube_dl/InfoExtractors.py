78af8eb1d14fbdc911ba87a07c124ab19c0f8843
Philipp Hagemeister <phihag@phihag.de>
2013-06-23 22:08:28 +0200

    [ustream] move into its own file

--- youtube_dl/InfoExtractors.py
+++ youtube_dl/InfoExtractors.py
@@ -45,6 +45,7 @@ from .extractor.soundcloud import SoundcloudIE, SoundcloudSetIE
 from .extractor.stanfordoc import StanfordOpenClassroomIE
 from .extractor.steam import SteamIE
 from .extractor.ted import TEDIE
+from .extractor.ustream import UstreamIE
 from .extractor.vimeo import VimeoIE
 from .extractor.worldstarhiphop import WorldStarHipHopIE
 from .extractor.xnxx import XNXXIE
@@ -69,37 +70,6 @@ from .extractor.zdf import ZDFIE
 
 
 
-class UstreamIE(InfoExtractor):
-    _VALID_URL = r'https?://www\.ustream\.tv/recorded/(?P<videoID>\d+)'
-    IE_NAME = u'ustream'
-
-    def _real_extract(self, url):
-        m = re.match(self._VALID_URL, url)
-        video_id = m.group('videoID')
-
-        video_url = u'http://tcdn.ustream.tv/video/%s' % video_id
-        webpage = self._download_webpage(url, video_id)
-
-        self.report_extraction(video_id)
-
-        video_title = self._html_search_regex(r'data-title="(?P<title>.+)"',
-            webpage, u'title')
-
-        uploader = self._html_search_regex(r'data-content-type="channel".*?>(?P<uploader>.*?)</a>',
-            webpage, u'uploader', fatal=False, flags=re.DOTALL)
-
-        thumbnail = self._html_search_regex(r'<link rel="image_src" href="(?P<thumb>.*?)"',
-            webpage, u'thumbnail', fatal=False)
-
-        info = {
-                'id': video_id,
-                'url': video_url,
-                'ext': 'flv',
-                'title': video_title,
-                'uploader': uploader,
-                'thumbnail': thumbnail,
-               }
-        return info
 
 
 class RBMARadioIE(InfoExtractor):
