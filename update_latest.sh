c052029267ba4988e50bd45a85a27dd5f1fb01de
Philipp Hagemeister <phihag@phihag.de>
2013-06-25 19:04:30 +0200

    Add an error message if number of arguments is incorrect

--- update_latest.sh
+++ update_latest.sh
@@ -3,6 +3,11 @@ set -e
 
 version="$1"
 
+if [ -z "$version" ]; then
+	echo "Usage: $0 version"
+	exit 1
+fi
+
 ln -sf ../downloads/$version latest/directory
 for f in $(ls "downloads/$version/"); do \
     ln -sf "../downloads/$version/$f" latest/$(echo $f | sed -e "s@-$version@@")
