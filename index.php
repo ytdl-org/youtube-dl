420cf88aeb8e8e5c459b4c2fbccbe04e78f5f94c
Philipp Hagemeister <phihag@phihag.de>
2013-06-25 17:28:55 +0200

    Remove copyright note (this page is trivial)

--- index.php
+++ index.php
@@ -42,10 +42,5 @@ echo '</ul>';
 	<tr><td><a class="button" id="b" href="http://rg3.github.com/youtube-dl/about.html">About</a></td></tr>
 </table>
 
-<div class="note">
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
-<img alt="Creative Commons License" style="border-width:0"
-src="http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png" /></a><br />
-Copyright © 2006-2012 Ricardo Garcia Gonzalez</div>
 </body>
 </html>
