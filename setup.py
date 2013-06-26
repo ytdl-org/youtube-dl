652e7768934025add22f1beae6a525fecd2a6f0b
Rogério Brito <rbrito@ime.usp.br>
2013-06-26 00:54:55 -0300

    setup: PEP-8 fixes.
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- setup.py
+++ setup.py
@@ -56,21 +56,22 @@ exec(compile(open('youtube_dl/version.py').read(),
              'youtube_dl/version.py', 'exec'))
 
 setup(
-    name = 'youtube_dl',
-    version = __version__,
-    description = 'YouTube video downloader',
-    long_description = 'Small command-line program to download videos from YouTube.com and other video sites.',
-    url = 'https://github.com/rg3/youtube-dl',
-    author = 'Ricardo Garcia',
-    maintainer = 'Philipp Hagemeister',
-    maintainer_email = 'phihag@phihag.de',
-    packages = ['youtube_dl', 'youtube_dl.extractor'],
+    name='youtube_dl',
+    version=__version__,
+    description='YouTube video downloader',
+    long_description='Small command-line program to download videos from'
+    ' YouTube.com and other video sites.',
+    url='https://github.com/rg3/youtube-dl',
+    author='Ricardo Garcia',
+    maintainer='Philipp Hagemeister',
+    maintainer_email='phihag@phihag.de',
+    packages=['youtube_dl', 'youtube_dl.extractor'],
 
     # Provokes warning on most systems (why?!)
-    #test_suite = 'nose.collector',
-    #test_requires = ['nosetest'],
+    # test_suite = 'nose.collector',
+    # test_requires = ['nosetest'],
 
-    classifiers = [
+    classifiers=[
         "Topic :: Multimedia :: Video",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
