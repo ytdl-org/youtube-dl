2d5a8b55129b1dc10f9e789875b46742c8f303af
M.Yasoob Khalid <yasoob.khld@gmail.com>
2013-07-13 09:18:03 +0500

    added test for criterion.com

--- youtube_dl/extractor/criterion.py
+++ youtube_dl/extractor/criterion.py
@@ -6,6 +6,15 @@ from .common import InfoExtractor
 
 class CriterionIE(InfoExtractor):
     _VALID_URL = r'http://www.criterion.com/films/(.*)'
+    _TEST = {
+        u'url': u'http://www.criterion.com/films/184-le-samourai',
+        u'file': u'184.mp4',
+        u'md5': u'bc51beba55685509883a9a7830919ec3',
+        u'info_dict': {
+            u"title": u"Le Samouraï",
+            u"description" : u"In a career-defining performance, Alain Delon plays a contract killer with samurai instincts. A razor-sharp cocktail of 1940s American gangster cinema and 1960s French pop culture, maverick director Jean-Pierre Melville&#x27;s masterpiece _Le Samouraï_ defines cool. "
+        }
+    }
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
