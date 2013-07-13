159736c1b81e2897e91369f399eceacaa09192b4
M.Yasoob Khalid <yasoob.khld@gmail.com>
2013-07-13 09:17:48 +0500

    added an IE for criterion.com

---
+++ youtube_dl/extractor/criterion.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from .common import InfoExtractor
+
+class CriterionIE(InfoExtractor):
+    _VALID_URL = r'http://www.criterion.com/films/(.*)'
+
+    def _real_extract(self, url):
+        mobj = re.match(self._VALID_URL, url)
+        video_id = mobj.group(1).split('-')[0]
+        webpage = self._download_webpage(url, video_id)
+
+        final_url = self._search_regex(r'so.addVariable\("videoURL", "(.+?)"\)\;',
+                                webpage, 'video url')
+        title = self._search_regex(r'<meta content="(.+?)" property="og:title" />',
+                                webpage, 'video title')
+        description = self._search_regex(r'<meta name="description" content="(.+?)" />',
+                                webpage, 'video description')
+        thumbnail = self._search_regex(r'so.addVariable\("thumbnailURL", "(.+?)"\)\;',
+                                webpage, 'thumbnail url')
+        ext = final_url.split('.')[-1]
+
+        return {'id': video_id,
+                'url' : final_url,
+                'title': title,
+                'ext': ext,
+                'description': description,
+                'thumbnail': thumbnail,
+                }
