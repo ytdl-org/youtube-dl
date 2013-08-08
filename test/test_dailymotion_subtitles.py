447591e1aea39f3100b66a7b94337bf67546663f
Ismael Mejia <iemejia@gmail.com>
2013-08-08 11:03:52 +0200

    [test] Cleaned subtitles tests

--- test/test_dailymotion_subtitles.py
+++ test/test_dailymotion_subtitles.py
@@ -15,74 +15,43 @@ from youtube_dl.utils import *
 from helper import FakeYDL
 
 md5 = lambda s: hashlib.md5(s.encode('utf-8')).hexdigest()
-TEST_URL = 'http://www.dailymotion.com/video/xczg00'
 
 class TestDailymotionSubtitles(unittest.TestCase):
     def setUp(self):
-        DL = FakeYDL()
-        DL.params['allsubtitles'] = False
-        DL.params['writesubtitles'] = False
-        DL.params['subtitlesformat'] = 'srt'
-        DL.params['listsubtitles'] = False
+        self.DL = FakeYDL()
+        self.url = 'http://www.dailymotion.com/video/xczg00'
+    def getInfoDict(self):
+        IE = DailymotionIE(self.DL)
+        info_dict = IE.extract(self.url)
+        return info_dict
+    def getSubtitles(self):
+        info_dict = self.getInfoDict()
+        return info_dict[0]['subtitles']
     def test_no_subtitles(self):
-        DL = FakeYDL()
-        DL.params['writesubtitles'] = False
-        IE = DailymotionIE(DL)
-        info_dict = IE.extract(TEST_URL)
-        subtitles = info_dict[0]['subtitles']
+        subtitles = self.getSubtitles()
         self.assertEqual(subtitles, None)
     def test_subtitles(self):
-        DL = FakeYDL()
-        DL.params['writesubtitles'] = True
-        IE = DailymotionIE(DL)
-        info_dict = IE.extract(TEST_URL)
-        sub = info_dict[0]['subtitles']['en']
-        self.assertEqual(md5(sub), '976553874490cba125086bbfea3ff76f')
+        self.DL.params['writesubtitles'] = True
+        subtitles = self.getSubtitles()
+        self.assertEqual(md5(subtitles['en']), '976553874490cba125086bbfea3ff76f')
     def test_subtitles_fr(self):
-        DL = FakeYDL()
-        DL.params['writesubtitles'] = True
-        DL.params['subtitleslang'] = 'fr'
-        IE = DailymotionIE(DL)
-        info_dict = IE.extract(TEST_URL)
-        sub = info_dict[0]['subtitles']['fr']
-        self.assertEqual(md5(sub), '594564ec7d588942e384e920e5341792')
+        self.DL.params['writesubtitles'] = True
+        self.DL.params['subtitleslang'] = 'fr'
+        subtitles = self.getSubtitles()
+        self.assertEqual(md5(subtitles['fr']), '594564ec7d588942e384e920e5341792')
     def test_allsubtitles(self):
-        DL = FakeYDL()
-        DL.params['allsubtitles'] = True
-        IE = DailymotionIE(DL)
-        info_dict = IE.extract(TEST_URL)
-        subtitles = info_dict[0]['subtitles']
+        self.DL.params['allsubtitles'] = True
+        subtitles = self.getSubtitles()
         self.assertEqual(len(subtitles.keys()), 5)
-    # def test_subtitles_sbv_format(self):
-    #     DL = FakeYDL()
-    #     DL.params['writesubtitles'] = True
-    #     DL.params['subtitlesformat'] = 'sbv'
-    #     IE = DailymotionIE(DL)
-    #     info_dict = IE.extract(TEST_URL)
-    #     sub = info_dict[0]['subtitles'][0]
-    #     self.assertEqual(md5(sub), '13aeaa0c245a8bed9a451cb643e3ad8b')
-    # def test_subtitles_vtt_format(self):
-    #     DL = FakeYDL()
-    #     DL.params['writesubtitles'] = True
-    #     DL.params['subtitlesformat'] = 'vtt'
-    #     IE = DailymotionIE(DL)
-    #     info_dict = IE.extract(TEST_URL)
-    #     sub = info_dict[0]['subtitles'][0]
-    #     self.assertEqual(md5(sub), '356cdc577fde0c6783b9b822e7206ff7')
-    def test_list_subtitles(self):
-        DL = FakeYDL()
-        DL.params['listsubtitles'] = True
-        IE = DailymotionIE(DL)
-        info_dict = IE.extract(TEST_URL)
+    def test_list_subtitles(self): #ojo
+        self.DL.params['listsubtitles'] = True
+        info_dict = self.getInfoDict()
         self.assertEqual(info_dict, None)
     def test_automatic_captions(self):
-        DL = FakeYDL()
-        DL.params['writeautomaticsub'] = True
-        DL.params['subtitleslang'] = 'en'
-        IE = DailymotionIE(DL)
-        info_dict = IE.extract(TEST_URL)
-        sub = info_dict[0]['subtitles']
-        self.assertTrue(len(sub) == 0)
+        self.DL.params['writeautomaticsub'] = True
+        self.DL.params['subtitleslang'] = 'en'
+        subtitles = self.getSubtitles()
+        self.assertTrue(len(subtitles.keys()) == 0)
 
 if __name__ == '__main__':
     unittest.main()
