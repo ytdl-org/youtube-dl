d2d1eb5b0a3ad2aa9d4867ba09b4b922b170f36a
Philipp Hagemeister <phihag@phihag.de>
2013-08-23 23:57:23 +0200

    Switch to domain yt-dl.org

--- devscripts/gh-pages/add-version.py
+++ devscripts/gh-pages/add-version.py
@@ -6,28 +6,32 @@ import hashlib
 import urllib.request
 
 if len(sys.argv) <= 1:
-	print('Specify the version number as parameter')
-	sys.exit()
+    print('Specify the version number as parameter')
+    sys.exit()
 version = sys.argv[1]
 
 with open('update/LATEST_VERSION', 'w') as f:
-	f.write(version)
+    f.write(version)
 
 versions_info = json.load(open('update/versions.json'))
 if 'signature' in versions_info:
-	del versions_info['signature']
+    del versions_info['signature']
 
 new_version = {}
 
-filenames = {'bin': 'youtube-dl', 'exe': 'youtube-dl.exe', 'tar': 'youtube-dl-%s.tar.gz' % version}
+filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
 for key, filename in filenames.items():
-	print('Downloading and checksumming %s...' %filename)
-	url = 'http://youtube-dl.org/downloads/%s/%s' % (version, filename)
-	data = urllib.request.urlopen(url).read()
-	sha256sum = hashlib.sha256(data).hexdigest()
-	new_version[key] = (url, sha256sum)
+    print('Downloading and checksumming %s...' % filename)
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    data = urllib.request.urlopen(url).read()
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
 
 versions_info['versions'][version] = new_version
 versions_info['latest'] = version
 
-json.dump(versions_info, open('update/versions.json', 'w'), indent=4, sort_keys=True)
+with open('update/versions.json', 'w') as jsonf:
+    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
