e86ea47c029c1f95a696e43df7bea2e3e617fbc3
Philipp Hagemeister <phihag@phihag.de>
2013-08-27 10:35:20 +0200

    [canalc2] Small improvements

--- youtube_dl/extractor/canalc2.py
+++ youtube_dl/extractor/canalc2.py
@@ -1,17 +1,17 @@
 # coding: utf-8
-"""Extractor for canalc2.tv"""
 import re
 
 from .common import InfoExtractor
 
+
 class Canalc2IE(InfoExtractor):
-    """Extractor for canalc2.tv"""
+    _IE_NAME = 'canalc2.tv'
     _VALID_URL = r'http://.*?\.canalc2\.tv/video\.asp\?idVideo=(\d+)&voir=oui'
 
     _TEST = {
         u'url': u'http://www.canalc2.tv/video.asp?idVideo=12163&voir=oui',
         u'file': u'12163.mp4',
-        u'md5': u'c00fa80517373764ff5c0b5eb5a58780',
+        u'md5': u'060158428b650f896c542dfbb3d6487f',
         u'info_dict': {
             u'title': u'Terrasses du Numérique'
         }
@@ -20,16 +20,16 @@ class Canalc2IE(InfoExtractor):
     def _real_extract(self, url):
         video_id = re.match(self._VALID_URL, url).group(1)
         webpage = self._download_webpage(url, video_id)
-        file_name = re.search(r"so\.addVariable\('file','(.*?)'\);",
-            webpage).group(1)
-        
+        file_name = self._search_regex(
+            r"so\.addVariable\('file','(.*?)'\);",
+            webpage, 'file name')
         video_url = 'http://vod-flash.u-strasbg.fr:8080/' + file_name
 
-        title = self._html_search_regex(r'class="evenement8">(.*?)</a>',
-            webpage, u'title')
+        title = self._html_search_regex(
+            r'class="evenement8">(.*?)</a>', webpage, u'title')
         
         return {'id': video_id,
-                'ext' : 'mp4',
-                'url' : video_url,
-                'title' : title
+                'ext': 'mp4',
+                'url': video_url,
+                'title': title,
                 }
