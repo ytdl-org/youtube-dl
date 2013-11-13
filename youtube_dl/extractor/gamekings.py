2c35b14de2bceadbe82d3bb8a71862ca17f94cb3
Philipp Hagemeister <phihag@phihag.de>
2013-11-13 10:50:53 +0100

    [gamekings] Minor fixes (#1759)

--- youtube_dl/extractor/gamekings.py
+++ youtube_dl/extractor/gamekings.py
@@ -7,13 +7,13 @@ from ..utils import (
 
 
 class GamekingsIE(InfoExtractor):
-    _VALID_URL = r'http?://www\.gamekings\.tv/videos/(?P<name>[0-9a-z\-])'
+    _VALID_URL = r'http?://www\.gamekings\.tv/videos/(?P<name>[0-9a-z\-]+)'
     _TEST = {
         u"url": u"http://www.gamekings.tv/videos/phoenix-wright-ace-attorney-dual-destinies-review/",
-        u'file': u'20130811_PhoenixWright.mp4',
-        u'md5': u'8d42d15381e2dfa81dee86c7956d35ff',
+        u'file': u'20130811.mp4',
+        u'md5': u'17f6088f7d0149ff2b46f2714bdb1954',
         u'info_dict': {
-            u"title": u"Phoenix Wright: Ace Attorney &#8211; Dual Destinies Review",
+            u"title": u"Phoenix Wright: Ace Attorney \u2013 Dual Destinies Review",
             u"description": u"Melle en Steven hebben voor de review een week in de rechtbank doorbracht met Phoenix Wright: Ace Attorney - Dual Destinies.",
         }
     }
@@ -23,17 +23,18 @@ class GamekingsIE(InfoExtractor):
         mobj = re.match(self._VALID_URL, url)
         name = mobj.group('name')
         webpage = self._download_webpage(url, name)
-        gamekings_url = self._og_search_video_url(webpage)
+        video_url = self._og_search_video_url(webpage)
 
-        video = re.search(r'[0-9]+',gamekings_url)
+        video = re.search(r'[0-9]+', video_url)
         video_id = video.group(0)
 
-        # Todo: add medium format 
-        gamekings_url = gamekings_url.replace(video_id,'large/' + video_id)
+        # Todo: add medium format
+        video_url = video_url.replace(video_id, 'large/' + video_id)
 
-        return {'id': video_id,
-                'ext': 'mp4',
-                'url': gamekings_url,
-                'title': self._og_search_title(webpage),
-                'description': self._og_search_description(webpage),
-                }
+        return {
+            'id': video_id,
+            'ext': 'mp4',
+            'url': video_url,
+            'title': self._og_search_title(webpage),
+            'description': self._og_search_description(webpage),
+        }
