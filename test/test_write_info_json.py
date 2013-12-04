6a656a843a629ceef6979976a353d177c97b9527
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-12-04 20:35:00 +0100

    Update description value for the write_info_json test (required after 27dcce19045670fc348ff1119c0d2283aaed3ae2)

--- test/test_write_info_json.py
+++ test/test_write_info_json.py
@@ -33,6 +33,7 @@ TEST_ID = 'BaW_jenozKc'
 INFO_JSON_FILE = TEST_ID + '.info.json'
 DESCRIPTION_FILE = TEST_ID + '.mp4.description'
 EXPECTED_DESCRIPTION = u'''test chars:  "'/\ä↭𝕐
+test URL: https://github.com/rg3/youtube-dl/issues/1892
 
 This is a test video for youtube-dl.
 
