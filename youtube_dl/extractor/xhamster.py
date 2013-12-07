b860967ce46358f6f54d2374af7e880155339392
rzhxeo <rzhxeot7z81b4700@mailcatch.com>
2013-12-07 22:17:13 +0100

    [XHamsterIE] Fix md5 in second test

--- youtube_dl/extractor/xhamster.py
+++ youtube_dl/extractor/xhamster.py
@@ -26,7 +26,7 @@ class XHamsterIE(InfoExtractor):
     {
         u'url': u'http://xhamster.com/movies/2221348/britney_spears_sexy_booty.html?hd',
         u'file': u'2221348.flv',
-        u'md5': u'970a94178ca4118c5aa3aaea21211b81',
+        u'md5': u'e767b9475de189320f691f49c679c4c7',
         u'info_dict': {
             u"upload_date": u"20130914",
             u"uploader_id": u"jojo747400",
