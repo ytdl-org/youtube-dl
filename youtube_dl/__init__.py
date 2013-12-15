dca02c80bc5c370bb3b15c9343d255dd124d2a93
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-12-15 11:42:38 +0100

    Fix detection of the extension if the 'extractaudio' is given and improve the error message (#1969)
    Using 'foo.mp4' shouldn't raise an error.
    If 'foo' is given suggest using 'foo.%(ext)s' for the template

--- youtube_dl/__init__.py
+++ youtube_dl/__init__.py
@@ -604,10 +604,10 @@ def _real_main(argv=None):
             or (opts.useid and u'%(id)s.%(ext)s')
             or (opts.autonumber and u'%(autonumber)s-%(id)s.%(ext)s')
             or u'%(title)s-%(id)s.%(ext)s')
-    if '%(ext)s' not in outtmpl and opts.extractaudio:
+    if not os.path.splitext(outtmpl)[1] and opts.extractaudio:
         parser.error(u'Cannot download a video and extract audio into the same'
-                     u' file! Use "%%(ext)s" instead of %r' %
-                     determine_ext(outtmpl, u''))
+                     u' file! Use "{0}.%(ext)s" instead of "{0}" as the output'
+                     u' template'.format(outtmpl))
 
     ydl_opts = {
         'usenetrc': opts.usenetrc,
