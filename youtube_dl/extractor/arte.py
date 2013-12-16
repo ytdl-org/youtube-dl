f7a68925724eda913c96519f2a5644aebb3688c4
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-12-16 21:42:41 +0100

    [arte:ddc] Remove test
    video seems to expire in 7 days, as arte+7

--- youtube_dl/extractor/arte.py
+++ youtube_dl/extractor/arte.py
@@ -266,20 +266,6 @@ class ArteTVDDCIE(ArteTVPlus7IE):
     IE_NAME = u'arte.tv:ddc'
     _VALID_URL = r'http?://ddc\.arte\.tv/(?P<lang>emission|folge)/(?P<id>.+)'
 
-    _TEST = {
-        u'url': u'http://ddc.arte.tv/folge/neues-aus-mauretanien',
-        u'file': u'049881-009_PLUS7-D.flv',
-        u'info_dict': {
-            u'title': u'Mit offenen Karten',
-            u'description': u'md5:57929b0eaeddeb8a0c983f58e9ebd3b6',
-            u'upload_date': u'20131207',
-        },
-        u'params': {
-            # rtmp download
-            u'skip_download': True,
-        },
-    }
-
     def _real_extract(self, url):
         video_id, lang = self._extract_url_info(url)
         if lang == 'folge':
