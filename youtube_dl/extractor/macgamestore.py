85689a531f1ba418570ce2bcde6fe16a482a5685
Philipp Hagemeister <phihag@phihag.de>
2014-01-03 13:09:39 +0100

    [macgamestore] Minor fixes (#2044)

--- youtube_dl/extractor/macgamestore.py
+++ youtube_dl/extractor/macgamestore.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import re
 
 from .common import InfoExtractor
@@ -5,34 +7,35 @@ from ..utils import ExtractorError
 
 
 class MacGameStoreIE(InfoExtractor):
-    IE_NAME = u'macgamestore'
-    IE_DESC = u'MacGameStore trailers'
+    IE_NAME = 'macgamestore'
+    IE_DESC = 'MacGameStore trailers'
     _VALID_URL = r'https?://www\.macgamestore\.com/mediaviewer\.php\?trailer=(?P<id>\d+)'
 
     _TEST = {
-        u'url': u'http://www.macgamestore.com/mediaviewer.php?trailer=2450',
-        u'file': u'2450.m4v',
-        u'md5': u'8649b8ea684b6666b4c5be736ecddc61',
-        u'info_dict': {
-            u'title': u'Crow',
+        'url': 'http://www.macgamestore.com/mediaviewer.php?trailer=2450',
+        'file': '2450.m4v',
+        'md5': '8649b8ea684b6666b4c5be736ecddc61',
+        'info_dict': {
+            'title': 'Crow',
         }
     }
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('id')
-        
-        webpage = self._download_webpage(url, video_id, u'Downloading trailer page')
-        
+
+        webpage = self._download_webpage(url, video_id, 'Downloading trailer page')
+
         if re.search(r'>Missing Media<', webpage) is not None:
-            raise ExtractorError(u'Trailer %s does not exist' % video_id, expected=True)
-        
-        mobj = re.search(r'<title>MacGameStore: (?P<title>.*?) Trailer</title>', webpage)
-        video_title = mobj.group('title')
-        
-        mobj = re.search(r'(?s)<div\s+id="video-player".*?href="(?P<video>[^"]+)"\s*>', webpage)
-        video_url = mobj.group('video')
-        
+            raise ExtractorError('Trailer %s does not exist' % video_id, expected=True)
+
+        video_title = self._html_search_regex(
+            r'<title>MacGameStore: (.*?) Trailer</title>', webpage, 'title')
+
+        video_url = self._html_search_regex(
+            r'(?s)<div\s+id="video-player".*?href="([^"]+)"\s*>',
+            webpage, 'video URL')
+
         return {
             'id': video_id,
             'url': video_url,
