49f2bf76a8e5e063a4053619dfb3153fe1c047dc
Philipp Hagemeister <phihag@phihag.de>
2014-01-05 04:44:29 +0100

    Fix make_readme on Python 2

--- devscripts/make_readme.py
+++ devscripts/make_readme.py
@@ -5,13 +5,16 @@ import re
 README_FILE = 'README.md'
 helptext = sys.stdin.read()
 
+if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
+
 with io.open(README_FILE, encoding='utf-8') as f:
     oldreadme = f.read()
 
 header = oldreadme[:oldreadme.index('# OPTIONS')]
 footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
 
-options = helptext[helptext.index('  General Options:')+19:]
+options = helptext[helptext.index('  General Options:') + 19:]
 options = re.sub(r'^  (\w.+)$', r'## \1', options, flags=re.M)
 options = '# OPTIONS\n' + options + '\n'
 
