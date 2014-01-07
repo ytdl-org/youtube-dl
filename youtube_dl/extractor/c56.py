84c92dc00fb89c52c66d659386a05b8533aa15bc
Philipp Hagemeister <phihag@phihag.de>
2014-01-07 10:19:15 +0100

    [c56] Add suppot for multiple formats

--- youtube_dl/extractor/c56.py
+++ youtube_dl/extractor/c56.py
@@ -1,21 +1,21 @@
 # coding: utf-8
+from __future__ import unicode_literals
 
 import re
 import json
 
 from .common import InfoExtractor
-from ..utils import determine_ext
+
 
 class C56IE(InfoExtractor):
     _VALID_URL = r'https?://((www|player)\.)?56\.com/(.+?/)?(v_|(play_album.+-))(?P<textid>.+?)\.(html|swf)'
-    IE_NAME = u'56.com'
-
-    _TEST ={
-        u'url': u'http://www.56.com/u39/v_OTM0NDA3MTY.html',
-        u'file': u'93440716.flv',
-        u'md5': u'e59995ac63d0457783ea05f93f12a866',
-        u'info_dict': {
-            u'title': u'网事知多少 第32期：车怒',
+    IE_NAME = '56.com'
+    _TEST = {
+        'url': 'http://www.56.com/u39/v_OTM0NDA3MTY.html',
+        'file': '93440716.flv',
+        'md5': 'e59995ac63d0457783ea05f93f12a866',
+        'info_dict': {
+            'title': '网事知多少 第32期：车怒',
         },
     }
 
@@ -23,14 +23,18 @@ class C56IE(InfoExtractor):
         mobj = re.match(self._VALID_URL, url, flags=re.VERBOSE)
         text_id = mobj.group('textid')
         info_page = self._download_webpage('http://vxml.56.com/json/%s/' % text_id,
-                                           text_id, u'Downloading video info')
+                                           text_id, 'Downloading video info')
         info = json.loads(info_page)['info']
-        best_format = sorted(info['rfiles'], key=lambda f: int(f['filesize']))[-1]
-        video_url = best_format['url']
+        formats = [{
+            'format_id': f['type'],
+            'filesize': int(f['filesize']),
+            'url': f['url']
+        } for f in info['rfiles']]
+        self._sort_formats(formats)
 
-        return {'id': info['vid'],
-                'title': info['Subject'],
-                'url': video_url,
-                'ext': determine_ext(video_url),
-                'thumbnail': info.get('bimg') or info.get('img'),
-                }
+        return {
+            'id': info['vid'],
+            'title': info['Subject'],
+            'formats': formats,
+            'thumbnail': info.get('bimg') or info.get('img'),
+        }
