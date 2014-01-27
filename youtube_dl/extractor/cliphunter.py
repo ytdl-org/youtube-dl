b6d3a99678052bb85a187268dbd50e35fbde109c
Philipp Hagemeister <phihag@phihag.de>
2014-01-27 12:39:39 +0100

    [cliphunter] Simplify (#2233)

--- youtube_dl/extractor/cliphunter.py
+++ youtube_dl/extractor/cliphunter.py
@@ -8,28 +8,22 @@ from ..utils import (
     ExtractorError,
 )
 
-translation_table = (
-    '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12'
-    '\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#:%.\'=)*+,-./0123'
-    '456789:;</>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]&_`hbcevofhdjknamoutsstupwrli{'
-    '|}~\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f'
-    '\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1'
-    '\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3'
-    '\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5'
-    '\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7'
-    '\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9'
-    '\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb'
-    '\xfc\xfd\xfe\xff'
-)
+translation_table = {
+    'a': 'h', 'd': 'e', 'e': 'v', 'f': 'o', 'g': 'f', 'i': 'd', 'l': 'n',
+    'm': 'a', 'n': 'm', 'p': 'u', 'q': 't', 'r': 's', 'v': 'p', 'x': 'r',
+    'y': 'l', 'z': 'i',
+    '$': ':', '&': '.', '(': '=', '^': '&', '=': '/',
+}
 
 
 class CliphunterIE(InfoExtractor):
     IE_NAME = 'cliphunter'
 
-    _VALID_URL = (r'(?:http://)?(?:www\.)?cliphunter\.com/w/'
-                  '(?P<id>[0-9]+)/'
-                  '(?P<seo>.+?)(?:\?.*)?')
-    _TESTS = {
+    _VALID_URL = r'''(?x)http://(?:www\.)?cliphunter\.com/w/
+        (?P<id>[0-9]+)/
+        (?P<seo>.+?)(?:$|[#\?])
+    '''
+    _TEST = {
         'url': 'http://www.cliphunter.com/w/1012420/Fun_Jynx_Maze_solo',
         'file': '1012420.flv',
         'md5': '15e7740f30428abf70f4223478dc1225',
@@ -44,15 +38,17 @@ class CliphunterIE(InfoExtractor):
 
         webpage = self._download_webpage(url, video_id)
 
-        pl_fiji = self._search_regex(r'pl_fiji = \'([^\']+)\'', webpage, 'video data')
-        pl_c_qual = self._search_regex(r'pl_c_qual = "(.)"', webpage, 'video quality')
-        video_title = self._search_regex(r'mediaTitle = "([^"]+)"', webpage, 'title')
+        pl_fiji = self._search_regex(
+            r'pl_fiji = \'([^\']+)\'', webpage, 'video data')
+        pl_c_qual = self._search_regex(
+            r'pl_c_qual = "(.)"', webpage, 'video quality')
+        video_title = self._search_regex(
+            r'mediaTitle = "([^"]+)"', webpage, 'title')
 
-        video_url = string.translate(pl_fiji.encode(), translation_table)
+        video_url = ''.join(translation_table.get(c, c) for c in pl_fiji)
 
         formats = [{
             'url': video_url,
-            'format': pl_c_qual,
             'format_id': pl_c_qual,
         }]
 
