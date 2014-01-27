9b05bd42e590ee21daba80ec7fd3fd79991a3bca
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2014-01-27 12:41:30 +0100

    [discovery] Extract more info and simplify

--- youtube_dl/extractor/discovery.py
+++ youtube_dl/extractor/discovery.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import re
 import json
+
 from .common import InfoExtractor
 
 
@@ -9,32 +10,37 @@ class DiscoveryIE(InfoExtractor):
     _VALID_URL = r'http://dsc\.discovery\.com\/[a-zA-Z0-9\-]*/[a-zA-Z0-9\-]*/videos/(?P<id>[a-zA-Z0-9\-]*)(.htm)?'
     _TEST = {
         'url': 'http://dsc.discovery.com/tv-shows/mythbusters/videos/mission-impossible-outtakes.htm',
-        'file': 'mission-impossible-outtakes.mp4',
+        'file': '614784.mp4',
         'md5': 'e12614f9ee303a6ccef415cb0793eba2',
         'info_dict': {
-            'title': 'MythBusters: Mission Impossible Outtakes'
-        }
+            'title': 'MythBusters: Mission Impossible Outtakes',
+            'description': ('Watch Jamie Hyneman and Adam Savage practice being'
+                ' each other -- to the point of confusing Jamie\'s dog -- and '
+                'don\'t miss Adam moon-walking as Jamie ... behind Jamie\'s'
+                ' back.'),
+            'duration': 156,
+        },
     }
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('id')
         webpage = self._download_webpage(url, video_id)
-        title = self._search_regex(
-            r'(?<=\"name\": ")(?P<title>.*?)(?=\"\,)', webpage, r'Filename')
-        duration = int(self._search_regex(
-            r'(?<=\"duration\"\: )(?P<duration>.*?)(?=,)', webpage, r'Duration'))
-        formats_raw = self._search_regex(
-            r'(?<=\"mp4\":)(.*?)(}])', webpage, r'formats') + '}]'
-        formats_json = json.loads(formats_raw)
+
+        video_list_json = self._search_regex(r'var videoListJSON = ({.*?});',
+            webpage, 'video list', flags=re.DOTALL)
+        video_list = json.loads(video_list_json)
+        info = video_list['clips'][0]
         formats = []
-        for f in formats_json:
+        for f in info['mp4']:
             formats.append(
                 {'url': f['src'], r'ext': r'mp4', 'tbr': int(f['bitrate'][:-1])})
 
         return {
-            'id': video_id,
-            'duration': duration,
-            'title': title,
-            'formats': formats
+            'id': info['contentId'],
+            'title': video_list['name'],
+            'formats': formats,
+            'description': info['videoCaption'],
+            'thumbnail': info.get('videoStillURL') or info.get('thumbnailURL'),
+            'duration': info['duration'],
         }
