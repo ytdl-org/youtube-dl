89acb969275da60395cea590f19ed5b5bbbd3f38
Philipp Hagemeister <phihag@phihag.de>
2014-01-30 06:09:06 +0100

    [liveleak] Support old and new URLs

--- youtube_dl/extractor/liveleak.py
+++ youtube_dl/extractor/liveleak.py
@@ -11,7 +11,7 @@ from ..utils import (
 
 class LiveLeakIE(InfoExtractor):
     _VALID_URL = r'^(?:http://)?(?:\w+\.)?liveleak\.com/view\?(?:.*?)i=(?P<video_id>[\w_]+)(?:.*)'
-    _TEST = {
+    _TESTS = [{
         'url': 'http://www.liveleak.com/view?i=757_1364311680',
         'file': '757_1364311680.mp4',
         'md5': '0813c2430bea7a46bf13acf3406992f4',
@@ -20,7 +20,17 @@ class LiveLeakIE(InfoExtractor):
             'uploader': 'ljfriel2',
             'title': 'Most unlucky car accident'
         }
-    }
+    },
+    {
+        'url': 'http://www.liveleak.com/view?i=f93_1390833151',
+        'file': 'f93_1390833151.mp4',
+        'md5': 'd3f1367d14cc3c15bf24fbfbe04b9abf',
+        'info_dict': {
+            'description': 'German Television Channel NDR does an exclusive interview with Edward Snowden.\r\nUploaded on LiveLeak cause German Television thinks the rest of the world isn\'t intereseted in Edward Snowden.',
+            'uploader': 'ARD_Stinkt',
+            'title': 'German Television does first Edward Snowden Interview (ENGLISH)',
+        }
+    }]
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
@@ -28,7 +38,11 @@ class LiveLeakIE(InfoExtractor):
         video_id = mobj.group('video_id')
         webpage = self._download_webpage(url, video_id)
         sources_raw = self._search_regex(
-            r'(?s)sources:\s*(\[.*?\]),', webpage, 'video URLs')
+            r'(?s)sources:\s*(\[.*?\]),', webpage, 'video URLs', default=None)
+        if sources_raw is None:
+            sources_raw = '[{ %s}]' % (
+                self._search_regex(r'(file: ".*?"),', webpage, 'video URL'))
+
         sources_json = re.sub(r'\s([a-z]+):\s', r'"\1": ', sources_raw)
         sources = json.loads(sources_json)
 
