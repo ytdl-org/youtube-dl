cd7ee7aa444ac85ddf6ca29645195f331d96d139
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2014-02-24 14:00:31 +0100

    [nbc] Modernize

--- youtube_dl/extractor/nbc.py
+++ youtube_dl/extractor/nbc.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import re
 
 from .common import InfoExtractor
@@ -8,12 +10,13 @@ class NBCNewsIE(InfoExtractor):
     _VALID_URL = r'https?://www\.nbcnews\.com/video/.+?/(?P<id>\d+)'
 
     _TEST = {
-        u'url': u'http://www.nbcnews.com/video/nbc-news/52753292',
-        u'file': u'52753292.flv',
-        u'md5': u'47abaac93c6eaf9ad37ee6c4463a5179',
-        u'info_dict': {
-            u'title': u'Crew emerges after four-month Mars food study',
-            u'description': u'md5:24e632ffac72b35f8b67a12d1b6ddfc1',
+        'url': 'http://www.nbcnews.com/video/nbc-news/52753292',
+        'md5': '47abaac93c6eaf9ad37ee6c4463a5179',
+        'info_dict': {
+            'id': '52753292',
+            'ext': 'flv',
+            'title': 'Crew emerges after four-month Mars food study',
+            'description': 'md5:24e632ffac72b35f8b67a12d1b6ddfc1',
         },
     }
 
@@ -23,10 +26,11 @@ class NBCNewsIE(InfoExtractor):
         all_info = self._download_xml('http://www.nbcnews.com/id/%s/displaymode/1219' % video_id, video_id)
         info = all_info.find('video')
 
-        return {'id': video_id,
-                'title': info.find('headline').text,
-                'ext': 'flv',
-                'url': find_xpath_attr(info, 'media', 'type', 'flashVideo').text,
-                'description': compat_str(info.find('caption').text),
-                'thumbnail': find_xpath_attr(info, 'media', 'type', 'thumbnail').text,
-                }
+        return {
+            'id': video_id,
+            'title': info.find('headline').text,
+            'ext': 'flv',
+            'url': find_xpath_attr(info, 'media', 'type', 'flashVideo').text,
+            'description': compat_str(info.find('caption').text),
+            'thumbnail': find_xpath_attr(info, 'media', 'type', 'thumbnail').text,
+        }
