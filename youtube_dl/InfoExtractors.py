18d367c0a5329781218cd14c626d6ccb569820cb
Philipp Hagemeister <phihag@phihag.de>
2014-03-21 01:00:06 +0100

    Remove legacy InfoExtractors file

--- youtube_dl/InfoExtractors.py
+++
@@ -1,4 +0,0 @@
-# Legacy file for backwards compatibility, use youtube_dl.extractor instead!
-
-from .extractor.common import InfoExtractor, SearchInfoExtractor
-from .extractor import gen_extractors, get_info_extractor
