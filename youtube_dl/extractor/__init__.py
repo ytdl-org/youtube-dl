87a26277000b425972ec427f86b68fef2097c1ac
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2014-03-26 15:03:34 +0100

    [vice] Remove extractor
    The generic ooyala detection works fine.

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -263,7 +263,6 @@ from .veehd import VeeHDIE
 from .veoh import VeohIE
 from .vesti import VestiIE
 from .vevo import VevoIE
-from .vice import ViceIE
 from .viddler import ViddlerIE
 from .videobam import VideoBamIE
 from .videodetective import VideoDetectiveIE
