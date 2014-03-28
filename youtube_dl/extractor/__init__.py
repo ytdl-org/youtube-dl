263f4b514b9b90a217152d2ef8fbf5b884b41b5a
Sergey M․ <dstftw@gmail.com>
2014-03-28 23:01:08 +0700

    [ntv] Add support for ntv.ru (Closes #2581)

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -177,6 +177,7 @@ from .normalboots import NormalbootsIE
 from .novamov import NovaMovIE
 from .nowness import NownessIE
 from .nowvideo import NowVideoIE
+from .ntv import NTVIE
 from .ooyala import OoyalaIE
 from .orf import ORFIE
 from .parliamentliveuk import ParliamentLiveUKIE
