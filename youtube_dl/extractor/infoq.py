c0a7c608150bfdb36a4916988fc47375db621d59
Philipp Hagemeister <phihag@phihag.de>
2014-04-21 02:55:35 +0200

    [infoq] Simplify (#2777)

--- youtube_dl/extractor/infoq.py
+++ youtube_dl/extractor/infoq.py
@@ -13,14 +13,14 @@ class InfoQIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?infoq\.com/[^/]+/(?P<id>[^/]+)$'
 
     _TEST = {
-        u'name': u'InfoQ',
-        u'url': u'http://www.infoq.com/presentations/A-Few-of-My-Favorite-Python-Things',
-        u'md5': u'fcaa3d995e04080dcb9465d86b5eef62',
-        u'info_dict': {
-            u'id': u'12-jan-pythonthings',
-            u'ext': u'mp4',
-            u'description': u'Mike Pirnat presents some tips and tricks, standard libraries and third party packages that make programming in Python a richer experience.',
-            u'title': u'A Few of My Favorite [Python] Things',
+        'name': 'InfoQ',
+        'url': 'http://www.infoq.com/presentations/A-Few-of-My-Favorite-Python-Things',
+        'md5': 'fcaa3d995e04080dcb9465d86b5eef62',
+        'info_dict': {
+            'id': '12-jan-pythonthings',
+            'ext': 'mp4',
+            'description': 'Mike Pirnat presents some tips and tricks, standard libraries and third party packages that make programming in Python a richer experience.',
+            'title': 'A Few of My Favorite [Python] Things',
         },
     }
 
@@ -29,8 +29,6 @@ class InfoQIE(InfoExtractor):
         video_id = mobj.group('id')
 
         webpage = self._download_webpage(url, video_id)
-
-        self.report_extraction(video_id)
 
         video_title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
         video_description = self._html_search_meta('description', webpage, 'description')
@@ -46,7 +44,7 @@ class InfoQIE(InfoExtractor):
         video_filename = playpath.split('/')[-1]
         video_id, extension = video_filename.split('.')
 
-        return [{
+        return {
             'id': video_id,
             'title': video_title,
             'description': video_description,
@@ -55,4 +53,4 @@ class InfoQIE(InfoExtractor):
                 'ext': extension,
                 'play_path': playpath,
             }],
-        }]
+        }
