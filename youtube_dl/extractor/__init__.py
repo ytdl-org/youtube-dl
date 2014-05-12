63b31b059c3052bd950caf2f71c3b842374c3ec9
hojel <hojelei@gmail.com>
2014-05-12 01:29:19 -0700

    [slutload] Add new extractor

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -230,6 +230,7 @@ from .scivee import SciVeeIE
 from .servingsys import ServingSysIE
 from .sina import SinaIE
 from .slideshare import SlideshareIE
+from .slutload import SlutloadIE
 from .smotri import (
     SmotriIE,
     SmotriCommunityIE,
