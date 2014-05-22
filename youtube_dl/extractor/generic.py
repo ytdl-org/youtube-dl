5e011e1505bee8107784d38eadbf98653b9e1505
rzhxeo <rzhxeot7z81b4700@mailcatch.com>
2014-05-22 09:13:28 +0200

    [Generic] Fix access to removed function in python 3.4

--- youtube_dl/extractor/generic.py
+++ youtube_dl/extractor/generic.py
@@ -308,9 +308,14 @@ class GenericIE(InfoExtractor):
 
                 newheaders = dict((k,v) for k,v in req.headers.items()
                                   if k.lower() not in ("content-length", "content-type"))
+                try:
+                    # This function was deprecated in python 3.3 and removed in 3.4
+                    origin_req_host = req.get_origin_req_host()
+                except AttributeError:
+                    origin_req_host = req.origin_req_host
                 return self.parent.open(compat_urllib_request.Request(req.get_full_url(),
                                                  headers=newheaders,
-                                                 origin_req_host=req.get_origin_req_host(),
+                                                 origin_req_host=origin_req_host,
                                                  unverifiable=True))
 
         # Build our opener
