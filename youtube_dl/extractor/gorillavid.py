5f28a1acad2f0703b4e3c345ed934202e456790d
Petr Půlpán <Pulpan3@gmail.com>
2014-06-17 15:18:46 +0200

    [GorillaVid] improve extractor

--- youtube_dl/extractor/gorillavid.py
+++ youtube_dl/extractor/gorillavid.py
@@ -1,38 +1,76 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 import re
 
 from .common import InfoExtractor
+from ..utils import (
+    determine_ext,
+    compat_urllib_parse,
+    compat_urllib_request,
+)
+
 
 class GorillaVidIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www.)?gorillavid.in/(?:embed-)?(?P<id>\w+)(?:\-\d+x\d+)?(?:.html)?'
-    _TEST = {
-        'url': "http://gorillavid.in/z08zf8le23c6",
+    _VALID_URL = r'https?://(?:www\.)?gorillavid\.in/(?:embed-)?(?P<id>[0-9a-zA-Z]+)(?:-[0-9]+x[0-9]+\.html)?'
+
+    _TESTS = [{
+        'url': 'http://gorillavid.in/06y9juieqpmi',
+        'md5': '5ae4a3580620380619678ee4875893ba',
+        'info_dict': {
+            'id': '06y9juieqpmi',
+            'ext': 'flv',
+            'title': 'Rebecca Black My Moment Official Music Video Reaction',
+            'thumbnail': 're:http://.*\.jpg',
+        },
+    }, {
+        'url': 'http://gorillavid.in/embed-z08zf8le23c6-960x480.html',
         'md5': 'c9e293ca74d46cad638e199c3f3fe604',
         'info_dict': {
             'id': 'z08zf8le23c6',
             'ext': 'mp4',
             'title': 'Say something nice',
-        }
-    }
+            'thumbnail': 're:http://.*\.jpg',
+        },
+    }]
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('id')
 
+        url = 'http://gorillavid.in/%s' % video_id
+
         webpage = self._download_webpage(url, video_id)
-        title = self._html_search_regex(r"name=['\"]fname['\"]\s+value=['\"](.*?)['\"]", webpage, u"video title")
 
-        # download embed page again with cookies to get url
-        embed_url = "http://gorillavid.in/embed-{0}-960x480.html".format(video_id)
-        webpage = self._download_webpage(embed_url, video_id, note=u'Downloading webpage again (with cookie)')
-        url = self._html_search_regex(r'file:\s+["\'](http://.*?video.\w{3})["\']', webpage, url)
+        fields = dict(re.findall(r'''(?x)<input\s+
+            type="hidden"\s+
+            name="([^"]+)"\s+
+            (?:id="[^"]+"\s+)?
+            value="([^"]*)"
+            ''', webpage))
+        
+        if fields['op'] == 'download1':
+            post = compat_urllib_parse.urlencode(fields)
 
-        info_dict = {
+            req = compat_urllib_request.Request(url, post)
+            req.add_header('Content-type', 'application/x-www-form-urlencoded')
+
+            webpage = self._download_webpage(req, video_id, 'Downloading video page')
+
+        title = self._search_regex(r'style="z-index: [0-9]+;">([0-9a-zA-Z ]+)(?:-.+)?</span>', webpage, 'title')
+        thumbnail = self._search_regex(r'image:\'(http[^\']+)\',', webpage, 'thumbnail')
+        url = self._search_regex(r'file: \'(http[^\']+)\',', webpage, 'file url')
+
+        formats = [{
+            'format_id': 'sd',
+            'url': url,
+            'ext': determine_ext(url),
+            'quality': 1,
+        }]
+
+        return {
             'id': video_id,
             'title': title,
-            'url': url,
+            'thumbnail': thumbnail,
+            'formats': formats,
         }
-
-        return info_dict
