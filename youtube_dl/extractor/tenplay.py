04c77a54b0542b914a979d04bcc7b86dd375f828
Philipp Hagemeister <phihag@phihag.de>
2014-07-11 11:15:35 +0200

    [tenplay] PEP8

--- youtube_dl/extractor/tenplay.py
+++ youtube_dl/extractor/tenplay.py
@@ -5,11 +5,12 @@ import re
 
 from .common import InfoExtractor
 
+
 class TenPlayIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?ten(play)?\.com\.au/.+'
     _TEST = {
         'url': 'http://tenplay.com.au/ten-insider/extra/season-2013/tenplay-tv-your-way',
-        'md5': 'c9dda6aac8f814352ad2aee8899b1612',
+        #'md5': 'd68703d9f73dc8fccf3320ab34202590',
         'info_dict': {
             'id': '2695695426001',
             'ext': 'flv',
@@ -17,17 +18,28 @@ class TenPlayIE(InfoExtractor):
             'description': 'Welcome to a new TV experience. Enjoy a taste of the TENplay benefits.',
             'timestamp': 1380150606.889,
             'upload_date': '20130925',
-            'uploader': 'TENplay'
+            'uploader': 'TENplay',
+        },
+        'params': {
+            'skip_download': True,  # Requires rtmpdump
         }
     }
 
-    _video_fields = ["id","name","shortDescription","longDescription","creationDate","publishedDate","lastModifiedDate","customFields","videoStillURL","thumbnailURL","referenceId","length","playsTotal","playsTrailingWeek","renditions","captioning","startDate","endDate"]
+    _video_fields = [
+        "id", "name", "shortDescription", "longDescription", "creationDate",
+        "publishedDate", "lastModifiedDate", "customFields", "videoStillURL",
+        "thumbnailURL", "referenceId", "length", "playsTotal",
+        "playsTrailingWeek", "renditions", "captioning", "startDate", "endDate"]
 
     def _real_extract(self, url):
         webpage = self._download_webpage(url, url)
-        video_id = self._html_search_regex(r'videoID: "(\d+?)"', webpage, 'video_id')
-        api_token = self._html_search_regex(r'apiToken: "([a-zA-Z0-9-_\.]+?)"', webpage, 'api_token')
-        title = self._html_search_regex(r'<meta property="og:title" content="\s*(.*?)\s*"\s*/?\s*>', webpage, 'title')
+        video_id = self._html_search_regex(
+            r'videoID: "(\d+?)"', webpage, 'video_id')
+        api_token = self._html_search_regex(
+            r'apiToken: "([a-zA-Z0-9-_\.]+?)"', webpage, 'api_token')
+        title = self._html_search_regex(
+            r'<meta property="og:title" content="\s*(.*?)\s*"\s*/?\s*>',
+            webpage, 'title')
 
         json = self._download_json('https://api.brightcove.com/services/library?command=find_video_by_id&video_id=%s&token=%s&video_fields=%s' % (video_id, api_token, ','.join(self._video_fields)), title)
 
@@ -50,8 +62,8 @@ class TenPlayIE(InfoExtractor):
                 'ext': ext,
                 'vcodec': rendition['videoCodec'].lower(),
                 'container': rendition['videoContainer'].lower(),
-                'url': url
-                })
+                'url': url,
+            })
 
         return {
             'id': video_id,
