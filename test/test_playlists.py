71b6065009d2bbc0e25b46368e73ebd807d3fca0
Ole Ernst <olebowle@gmx.com>
2014-07-23 09:32:01 +0200

    [gameone] add playlist test

--- test/test_playlists.py
+++ test/test_playlists.py
@@ -50,6 +50,7 @@ from youtube_dl.extractor import (
     InstagramUserIE,
     CSpanIE,
     AolIE,
+    GameOnePlaylistIE,
 )
 
 
@@ -394,6 +395,14 @@ class TestPlaylists(unittest.TestCase):
         self.assertIsPlaylist(result)
         self.assertEqual(result['id'], 'rbhagwati2')
         self.assertTrue(len(result['entries']) >= 179)
+
+    def test_GameOne_playlist(self):
+        dl = FakeYDL()
+        ie = GameOnePlaylistIE(dl)
+        result = ie.extract('http://www.gameone.de/tv')
+        self.assertIsPlaylist(result)
+        self.assertEqual(result['title'], 'GameOne')
+        assertGreaterEqual(self, len(result['entries']), 294)
 
 if __name__ == '__main__':
     unittest.main()
