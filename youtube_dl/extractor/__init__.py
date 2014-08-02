7a5e7b303c6186f01e475789de7ef8cc3f9eea94
Sergey M․ <dstftw@gmail.com>
2014-08-02 17:56:01 +0700

    [ubu] Add extractor (Close #3418)

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -327,6 +327,7 @@ from .tutv import TutvIE
 from .tvigle import TvigleIE
 from .tvp import TvpIE
 from .tvplay import TVPlayIE
+from.ubu import UbuIE
 from .udemy import (
     UdemyIE,
     UdemyCourseIE
