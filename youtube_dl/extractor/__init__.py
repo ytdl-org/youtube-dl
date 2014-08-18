6493f5d7045c90b2a6820b2cda58553d3a2fce8f
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2014-08-18 15:39:35 +0200

    [rtlnl] Add extractor for rtlxl.nl (closes #3523)

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -252,6 +252,7 @@ from .ro220 import Ro220IE
 from .rottentomatoes import RottenTomatoesIE
 from .roxwel import RoxwelIE
 from .rtbf import RTBFIE
+from .rtlnl import RtlXlIE
 from .rtlnow import RTLnowIE
 from .rts import RTSIE
 from .rtve import RTVEALaCartaIE
