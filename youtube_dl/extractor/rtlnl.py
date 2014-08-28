de2d9f5f1baafd7fd7c38ea0ffaf7fd6d757ff10
Sergey M․ <dstftw@gmail.com>
2014-08-28 22:54:06 +0700

    [rtlnl] Add support for progressive videos (Closes #3603)

--- youtube_dl/extractor/rtlnl.py
+++ youtube_dl/extractor/rtlnl.py
@@ -12,21 +12,15 @@ class RtlXlIE(InfoExtractor):
 
     _TEST = {
         'url': 'http://www.rtlxl.nl/#!/rtl-nieuws-132237/6e4203a6-0a5e-3596-8424-c599a59e0677',
+        'md5': 'cc16baa36a6c169391f0764fa6b16654',
         'info_dict': {
             'id': '6e4203a6-0a5e-3596-8424-c599a59e0677',
-            'ext': 'flv',
+            'ext': 'mp4',
             'title': 'RTL Nieuws - Laat',
-            'description': 'Dagelijks het laatste nieuws uit binnen- en '
-                'buitenland. Voor nog meer nieuws kunt u ook gebruikmaken van '
-                'onze mobiele apps.',
+            'description': 'md5:6b61f66510c8889923b11f2778c72dc5',
             'timestamp': 1408051800,
             'upload_date': '20140814',
             'duration': 576.880,
-        },
-        'params': {
-            # We download the first bytes of the first fragment, it can't be
-            # processed by the f4m downloader beacuse it isn't complete
-            'skip_download': True,
         },
     }
 
@@ -41,14 +35,32 @@ class RtlXlIE(InfoExtractor):
         material = info['material'][0]
         episode_info = info['episodes'][0]
 
-        f4m_url = 'http://manifest.us.rtl.nl' + material['videopath']
         progname = info['abstracts'][0]['name']
         subtitle = material['title'] or info['episodes'][0]['name']
+
+        videopath = material['videopath']
+        f4m_url = 'http://manifest.us.rtl.nl' + videopath
+
+        formats = self._extract_f4m_formats(f4m_url, uuid)
+
+        video_urlpart = videopath.split('/flash/')[1][:-4]
+        PG_URL_TEMPLATE = 'http://pg.us.rtl.nl/rtlxl/network/%s/progressive/%s.mp4'
+
+        formats.extend([
+            {
+                'url': PG_URL_TEMPLATE % ('a2m', video_urlpart),
+                'format_id': 'pg-sd',
+            },
+            {
+                'url': PG_URL_TEMPLATE % ('a3m', video_urlpart),
+                'format_id': 'pg-hd',
+            }
+        ])
 
         return {
             'id': uuid,
             'title': '%s - %s' % (progname, subtitle),
-            'formats': self._extract_f4m_formats(f4m_url, uuid),
+            'formats': formats,
             'timestamp': material['original_date'],
             'description': episode_info['synopsis'],
             'duration': parse_duration(material.get('duration')),
