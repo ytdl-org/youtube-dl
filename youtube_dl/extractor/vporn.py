12c82cf9cb59e97186b96fea76a15b52a0a9bb37
peugeot <peugeot@psa.fr>
2014-08-30 20:54:38 +0200

    add support for view count

--- youtube_dl/extractor/vporn.py
+++ youtube_dl/extractor/vporn.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import re
 
 from .common import InfoExtractor
+from ..utils import int_or_none
 
 class VpornIE(InfoExtractor):
     _VALID_URL = r'http?://(?:www\.)?vporn\.com/[a-z]+/(?P<title_dash>[a-z-]+)/(?P<id>\d+)/?'
@@ -30,9 +31,11 @@ class VpornIE(InfoExtractor):
         description = self._html_search_regex(r'<div class="description_txt">(.*?)</div>', webpage, 'description')
         thumbnail = 'http://www.vporn.com' + self._html_search_regex(r'flashvars.imageUrl = "(.*?)"', webpage, 'description')
 
-        mobj = re.search(
-            r'<span class="f_right">duration (?P<minutes>\d+) min (?P<seconds>\d+) sec </span>', webpage)
+        mobj = re.search(r'<span class="f_right">duration (?P<minutes>\d+) min (?P<seconds>\d+) sec </span>', webpage)
         duration = int(mobj.group('minutes')) * 60 + int(mobj.group('seconds')) if mobj else None
+
+        mobj = re.search(r'<span>((?P<thousands>\d+),)?(?P<units>\d+) VIEWS</span>', webpage)
+        view_count = int(mobj.group('thousands')) * 1000 + int(mobj.group('units')) if mobj else None
 
         return {
             'id': video_id,
@@ -40,5 +43,6 @@ class VpornIE(InfoExtractor):
             'thumbnail': thumbnail,
             'title': title,
             'description': description,
-            'duration': duration,
+            'duration': int_or_none(duration),
+            'view_count': int_or_none(view_count),
         }
