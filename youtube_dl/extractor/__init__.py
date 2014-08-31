7eb21356f98315b15fc05d9e616951511253076f
peugeot <peugeot@psa.fr>
2014-08-31 12:48:34 +0200

    Add support for sunporno.com

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -322,6 +322,7 @@ from .stanfordoc import StanfordOpenClassroomIE
 from .steam import SteamIE
 from .streamcloud import StreamcloudIE
 from .streamcz import StreamCZIE
+from .sunporno import SunPornoIE
 from .swrmediathek import SWRMediathekIE
 from .syfy import SyfyIE
 from .sztvhu import SztvHuIE
