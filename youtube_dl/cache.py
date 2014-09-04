674c869af485013f6121f4ec7dc6307ada3ad3a4
Philipp Hagemeister <phihag@phihag.de>
2014-09-04 04:47:56 +0200

    [cache] Allow dots in keys (Fixes #3674)

--- youtube_dl/cache.py
+++ youtube_dl/cache.py
@@ -25,8 +25,9 @@ class Cache(object):
         return os.path.expanduser(res)
 
     def _get_cache_fn(self, section, key, dtype):
-        assert re.match(r'^[a-zA-Z0-9_-]+$', section)
-        assert re.match(r'^[a-zA-Z0-9_-]+$', key)
+        assert re.match(r'^[a-zA-Z0-9_.-]+$', section), \
+            'invalid section %r' % section
+        assert re.match(r'^[a-zA-Z0-9_.-]+$', key), 'invalid key %r' % key
         return os.path.join(
             self._get_root_dir(), section, '%s.%s' % (key, dtype))
 
