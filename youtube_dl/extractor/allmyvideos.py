7cdd5339b3a02b7429c945ae4cd1e70c7112d555
Carlos Ramos <carlos.ramos1@alu.uclm.es>
2014-09-16 21:05:50 +0200

    Change tabs to spaces

--- youtube_dl/extractor/allmyvideos.py
+++ youtube_dl/extractor/allmyvideos.py
@@ -25,27 +25,27 @@ class AllmyvideosIE(InfoExtractor):
     }
 
     def _real_extract(self, url):
-		mobj = re.match(self._VALID_URL, url)
-		video_id = mobj.group('id')
+        mobj = re.match(self._VALID_URL, url)
+        video_id = mobj.group('id')
 
-		orig_webpage = self._download_webpage(url, video_id)
-		fields = re.findall(r'type="hidden" name="(.+?)"\s* value="?(.+?)">', orig_webpage)
-		data = {}
-		for name, value in fields:
-			data[name] = value
+        orig_webpage = self._download_webpage(url, video_id)
+        fields = re.findall(r'type="hidden" name="(.+?)"\s* value="?(.+?)">', orig_webpage)
+        data = {}
+        for name, value in fields:
+            data[name] = value
 
-		post = compat_urllib_parse.urlencode(data)
-		headers = {
-			b'Content-Type': b'application/x-www-form-urlencoded',
-		}
-		req = compat_urllib_request.Request(url, post, headers)
-		webpage = self._download_webpage(req, video_id, note='Downloading video page ...')
+        post = compat_urllib_parse.urlencode(data)
+        headers = {
+            b'Content-Type': b'application/x-www-form-urlencoded',
+        }
+        req = compat_urllib_request.Request(url, post, headers)
+        webpage = self._download_webpage(req, video_id, note='Downloading video page ...')
 
-		#Could be several links with different quality
-		links = re.findall(r'"file" : "?(.+?)",', webpage)
+        #Could be several links with different quality
+        links = re.findall(r'"file" : "?(.+?)",', webpage)
 
-		return {
-			'id': video_id,
-			'title': data['fname'][:len(data['fname'])-4],	#Remove .mp4 extension
-			'url': links[len(links)-1]						#Choose the higher quality link
-		}
+        return {
+            'id': video_id,
+            'title': data['fname'][:len(data['fname'])-4],  #Remove .mp4 extension
+            'url': links[len(links)-1]                      #Choose the higher quality link
+        }
