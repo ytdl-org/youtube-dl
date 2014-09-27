2a1325fdde5c88fc052710b3c42fcc0d73153901
net <net@h-MacBook-Pro-sl-net.local>
2014-09-27 20:11:22 +0300

    [ynet] Add new extractor

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -450,6 +450,7 @@ from .yahoo import (
     YahooNewsIE,
     YahooSearchIE,
 )
+from .ynet import YnetIE
 from .youjizz import YouJizzIE
 from .youku import YoukuIE
 from .youporn import YouPornIE
