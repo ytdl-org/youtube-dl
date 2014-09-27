c6641823238ac70091520fe9b4b02ec3d41cb1a9
Sergey M․ <dstftw@gmail.com>
2014-09-28 03:26:38 +0700

    [ynet] Remove unused stuff, simplify and improve

--- youtube_dl/extractor/ynet.py
+++ youtube_dl/extractor/ynet.py
@@ -2,46 +2,53 @@
 from __future__ import unicode_literals
 
 import re
-import base64
 import json
 
 from .common import InfoExtractor
-from youtube_dl.utils import compat_urllib_parse_urlparse, compat_urllib_parse
+from ..utils import compat_urllib_parse
+
 
 class YnetIE(InfoExtractor):
-    _VALID_URL = r'http://.*ynet\.co\.il/.*/0,7340,(?P<id>L(-[0-9]+)+),00\.html'
-    _TEST = {
-        'url': 'http://hot.ynet.co.il/home/0,7340,L-11659-99244,00.html',
-        'info_dict': {
-            'id': 'L-11659-99244',
-            'ext': 'flv',
-            'title': 'md5:3dba12d2837ee2ad9652cc64af652b16',
-            'thumbnail': 'http://hot.ynet.co.il/PicServer4/2014/09/23/5606015/AMERICAN_COMMUNE1_T.jpg',
+    _VALID_URL = r'http://.*ynet\.co\.il/.*/0,7340,(?P<id>L(?:-[0-9]+)+),00\.html'
+    _TESTS = [
+        {
+            'url': 'http://hot.ynet.co.il/home/0,7340,L-11659-99244,00.html',
+            'md5': '002b44ee2f33d50363a1c153bed524cf',
+            'info_dict': {
+                'id': 'L-11659-99244',
+                'ext': 'flv',
+                'title': 'איש לא יודע מאיפה באנו',
+                'thumbnail': 're:^https?://.*\.jpg',
+            }
+        }, {
+            'url': 'http://hot.ynet.co.il/home/0,7340,L-8859-84418,00.html',
+            'md5': '6455046ae1b48cf7e2b7cae285e53a16',
+            'info_dict': {
+                'id': 'L-8859-84418',
+                'ext': 'flv',
+                'title': "צפו: הנשיקה הלוהטת של תורגי' ויוליה פלוטקין",
+                'thumbnail': 're:^https?://.*\.jpg',
+            }
         }
-    }
+    ]
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
+        video_id = mobj.group('id')
         
-        id = mobj.group('id')
-        
-        webpage = self._download_webpage(url, id)
+        webpage = self._download_webpage(url, video_id)
 
-        content = compat_urllib_parse.unquote_plus(self._og_search_video_url(webpage).decode('utf-8'))
-
-        player_url = re.match('(http.*\.swf)\?' ,content).group(1)
-        
-        config = json.loads(re.match('.*config\=(.*)' ,content).group(1))
-                
-        f4m_url = config['clip']['url']    
-
-        title = re.sub(': Video$', '', self._og_search_title(webpage))
+        content = compat_urllib_parse.unquote_plus(self._og_search_video_url(webpage))
+        config = json.loads(self._search_regex(r'config=({.+?})$', content, 'video config'))
+        f4m_url = config['clip']['url']
+        title = self._og_search_title(webpage)
+        m = re.search(r'ynet - HOT -- (["\']+)(?P<title>.+?)\1', title)
+        if m:
+            title = m.group('title')
 
         return {
-            'id': id,
+            'id': video_id,
             'title': title,
-            'formats': self._extract_f4m_formats(f4m_url, id),
+            'formats': self._extract_f4m_formats(f4m_url, video_id),
             'thumbnail': self._og_search_thumbnail(webpage),
-            'player_url': player_url,
         }
-
