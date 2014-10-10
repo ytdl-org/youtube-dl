53d9009bdb064a43674136d0952a73546b828cd9
George Boyle <george@thebuds.net>
2014-10-10 09:33:41 +0100

    KeyError on initialising YoutubeDL in python3 #3910

--- youtube_dl/YoutubeDL.py
+++ youtube_dl/YoutubeDL.py
@@ -228,7 +228,7 @@ class YoutubeDL(object):
 
         if (sys.version_info >= (3,) and sys.platform != 'win32' and
                 sys.getfilesystemencoding() in ['ascii', 'ANSI_X3.4-1968']
-                and not params['restrictfilenames']):
+                and not params.get('restrictfilenames', False)):
             # On Python 3, the Unicode filesystem API will throw errors (#1474)
             self.report_warning(
                 'Assuming --restrict-filenames since file system encoding '
