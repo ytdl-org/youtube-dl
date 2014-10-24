b5a14350b9909cb9da7e82120fcaf1bf4aa61e43
Philipp Hagemeister <phihag@phihag.de>
2014-10-24 15:09:43 +0200

    [bild] Simplify (#3983)

--- youtube_dl/extractor/bild.py
+++ youtube_dl/extractor/bild.py
@@ -1,46 +1,39 @@
+#coding: utf-8
 from __future__ import unicode_literals
 
-import re
-
 from .common import InfoExtractor
+from ..utils import int_or_none
 
 
 class BildIE(InfoExtractor):
-    IE_NAME = 'bild'
+    _VALID_URL = r'https?://(?:www\.)?bild\.de/(?:[^/]+/)+(?P<display_id>[^/]+)-(?P<id>\d+)(?:,auto=true)?\.bild\.html'
+    IE_DESC = 'Bild.de'
     _TEST = {
         'url': 'http://www.bild.de/video/clip/apple-ipad-air/das-koennen-die-neuen-ipads-38184146.bild.html',
+        'md5': 'dd495cbd99f2413502a1713a1156ac8a',
         'info_dict': {
             'id': '38184146',
+            'ext': 'mp4',
             'title': 'BILD hat sie getestet',
             'thumbnail': 'http://bilder.bild.de/fotos/stand-das-koennen-die-neuen-ipads-38184138/Bild/1.bild.jpg',
             'duration': 196,
+            'description': 'Mit dem iPad Air 2 und dem iPad Mini 3 hat Apple zwei neue Tablet-Modelle präsentiert. BILD-Reporter Sven Stein durfte die Geräte bereits testen. ',
         }
     }
-    
-    #http://www.bild.de/video/clip/apple-ipad-air/das-koennen-die-neuen-ipads-38184146.bild.html
-    _VALID_URL = r'http?://(?:www\.)?bild\.de/(?:[^/]+/)+(?P<display_id>[^/]+)-(?P<id>\d+)(?:,auto=true)?\.bild\.html'
-    
+
     def _real_extract(self, url):
-        m = re.match(self._VALID_URL, url)
-        video_id = m.group('id')
-        
-        #webpage = self._download_webpage(url, video_id)
-        
-        xml_url = url.split(".bild.html")[0]+",view=xml.bild.xml"
-        
+        video_id = self._match_id(url)
+
+        xml_url = url.split(".bild.html")[0] + ",view=xml.bild.xml"
         doc = self._download_xml(xml_url, video_id)
-        
-        video_url = doc.attrib['src']
-        title = doc.attrib['ueberschrift']
-        description = doc.attrib['text']
-        thumbnail = doc.attrib['img']
-        duration = int(doc.attrib['duration'])/1000
+
+        duration = int_or_none(doc.attrib.get('duration'), scale=1000)
 
         return {
             'id': video_id,
-            'title': title,
-            'description': description,
-            'url': video_url,
-            'thumbnail': thumbnail,
+            'title': doc.attrib['ueberschrift'],
+            'description': doc.attrib.get('text'),
+            'url': doc.attrib['src'],
+            'thumbnail': doc.attrib.get('img'),
             'duration': duration,
         }
