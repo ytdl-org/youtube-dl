f9b9e88646fcad16501a8390c040f9dd2257dd15
Sergey M․ <dstftw@gmail.com>
2014-12-26 23:32:41 +0600

    [eroprofile] Simplify

--- youtube_dl/extractor/eroprofile.py
+++ youtube_dl/extractor/eroprofile.py
@@ -1,53 +1,45 @@
 from __future__ import unicode_literals
 
-import re
-
 from .common import InfoExtractor
 
+
 class EroProfileIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?eroprofile\.com/m/videos/view/(?P<display_id>[^/]+)'
+    _VALID_URL = r'https?://(?:www\.)?eroprofile\.com/m/videos/view/(?P<id>[^/]+)'
     _TEST = {
         'url': 'http://www.eroprofile.com/m/videos/view/sexy-babe-softcore',
         'md5': 'c26f351332edf23e1ea28ce9ec9de32f',
         'info_dict': {
             'id': '3733775',
+            'display_id': 'sexy-babe-softcore',
             'ext': 'm4v',
-            'title': 'HD MOVIES - sexy babe softcore',
-            'description': 'md5:831ee50526c333eb4f6c1e58d382c295',
-            'categories': list,  # NSFW
+            'title': 'sexy babe softcore',
             'thumbnail': 're:https?://.*\.jpg',
             'age_limit': 18,
         }
     }
 
     def _real_extract(self, url):
-        webpage = self._download_webpage(url, 'main')
+        display_id = self._match_id(url)
 
-        video_id = self._html_search_regex(r'glbUpdViews\s*\(\'\d*\',\'(\d+)\'', webpage, 'id')
+        webpage = self._download_webpage(url, display_id)
 
-        video_url = self._html_search_regex(r'<source src="([^"]+)', webpage, 'video_url')
+        video_id = self._search_regex(
+            [r"glbUpdViews\s*\('\d*','(\d+)'", r'p/report/video/(\d+)'],
+            webpage, 'video id', default=None)
 
+        video_url = self._search_regex(
+            r'<source src="([^"]+)', webpage, 'video url')
         title = self._html_search_regex(
-            r'<title>([^<]+)\s*-\s*EroProfile</title>', webpage, 'title')
-
-        description = self._html_search_meta('description', webpage, 'description', fatal=False)
-
-        thumbnail = self._html_search_regex(
-          r'onclick="showVideoPlayer\(\)"><img src="([^"]+)', webpage, 'thumbnail', fatal=False)
-
-        categories_str = self._html_search_meta(
-            'keywords', webpage, 'categories', fatal=False)
-        categories = (
-            None if categories_str is None
-            else categories_str.split(','))
+            r'Title:</th><td>([^<]+)</td>', webpage, 'title')
+        thumbnail = self._search_regex(
+            r'onclick="showVideoPlayer\(\)"><img src="([^"]+)',
+            webpage, 'thumbnail', fatal=False)
 
         return {
             'id': video_id,
+            'display_id': display_id,
             'url': video_url,
             'title': title,
-            'ext': 'm4v',
-            'description': description,
             'thumbnail': thumbnail,
-            'categories': categories,
             'age_limit': 18,
         }
