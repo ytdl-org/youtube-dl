6e46c3f1fddc954596d429d15d7c035d12a625a9
Gaurav <gauravb7090@gmail.com>
2015-01-03 20:30:28 +0530

    Added support for EllenTube along with EllenTV

--- youtube_dl/extractor/ellentv.py
+++ youtube_dl/extractor/ellentv.py
@@ -12,8 +12,8 @@ from ..utils import (
 
 
 class EllenTVIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?ellentv\.com/videos/(?P<id>[a-z0-9_-]+)'
-    _TEST = {
+    _VALID_URL = r'https?://(?:www\.)?(ellentv|ellentube)\.com/videos/(?P<id>[a-z0-9_-]+)'
+    _TESTS = [{
         'url': 'http://www.ellentv.com/videos/0-7jqrsr18/',
         'md5': 'e4af06f3bf0d5f471921a18db5764642',
         'info_dict': {
@@ -23,7 +23,18 @@ class EllenTVIE(InfoExtractor):
             'timestamp': 1406876400,
             'upload_date': '20140801',
         }
-    }
+    },
+    {
+        'url': 'http://ellentube.com/videos/0-dvzmabd5/',
+        'md5': '98238118eaa2bbdf6ad7f708e3e4f4eb',
+        'info_dict': {
+            'id': '0-dvzmabd5',
+            'ext': 'mp4',
+            'title': '1 year old twin sister makes her brother laugh',
+            'timestamp': 1419542075,
+            'upload_date': '20141225',
+        }
+    }]
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
@@ -33,6 +44,9 @@ class EllenTVIE(InfoExtractor):
         timestamp = parse_iso8601(self._search_regex(
             r'<span class="publish-date"><time datetime="([^"]+)">',
             webpage, 'timestamp'))
+        print "\n"
+        print timestamp
+        print "\n"
 
         return {
             'id': video_id,
