adb1307b9aeae4f011ba454a5d64d343e23503c8
Philipp Hagemeister <phihag@phihag.de>
2015-01-07 11:34:27 +0100

    [imdb] Remove test md5
    They seem to reencode quite frequently, so simply remove the md5 sum.

--- youtube_dl/extractor/imdb.py
+++ youtube_dl/extractor/imdb.py
@@ -16,7 +16,6 @@ class ImdbIE(InfoExtractor):
 
     _TEST = {
         'url': 'http://www.imdb.com/video/imdb/vi2524815897',
-        'md5': '9f34fa777ade3a6e57a054fdbcb3a068',
         'info_dict': {
             'id': '2524815897',
             'ext': 'mp4',
