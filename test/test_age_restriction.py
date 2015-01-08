5fbf25a681c2f7f6f3470e783fcac65b7c7c339d
Philipp Hagemeister <phihag@phihag.de>
2015-01-08 17:09:28 +0100

    [test_age_restriction] remove misbehaving test
    We now test for the age_limit being set right in test_download, so we don't need more than two tests for the actual age limit handling.

--- test/test_age_restriction.py
+++ test/test_age_restriction.py
@@ -45,11 +45,6 @@ class TestAgeRestriction(unittest.TestCase):
             'http://www.youporn.com/watch/505835/sex-ed-is-it-safe-to-masturbate-daily/',
             '505835.mp4', 2, old_age=25)
 
-    def test_pornotube(self):
-        self._assert_restricted(
-            'http://pornotube.com/c/173/m/1689755/Marilyn-Monroe-Bathing',
-            '1689755.flv', 13)
-
 
 if __name__ == '__main__':
     unittest.main()
