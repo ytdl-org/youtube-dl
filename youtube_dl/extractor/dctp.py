6ebb0dca9f40ecf9fb82882c097f42f497c70cda
Philipp Hagemeister <phihag@phihag.de>
2015-01-30 03:15:34 +0100

    [dctp] Improve output (#4796)

--- youtube_dl/extractor/dctp.py
+++ youtube_dl/extractor/dctp.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
+from ..compat import compat_str
 
 
 class DctpTvIE(InfoExtractor):
-    _VALID_URL = r'^http://www.dctp.tv/(#/)?filme/(?P<id>.+?)/$'
+    _VALID_URL = r'http://www.dctp.tv/(#/)?filme/(?P<id>.+?)/$'
     _TEST = {
         'url': 'http://www.dctp.tv/filme/videoinstallation-fuer-eine-kaufhausfassade/',
         'info_dict': {
@@ -18,13 +19,17 @@ class DctpTvIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         base_url = 'http://dctp-ivms2-restapi.s3.amazonaws.com/'
-        version_json = self._download_json(base_url + 'version.json', video_id)
+        version_json = self._download_json(
+            base_url + 'version.json',
+            video_id, note='Determining file version')
         version = version_json['version_name']
         info_json = self._download_json(
-            '{0}{1}/restapi/slugs/{2}.json'.format(base_url, version, video_id), video_id)
-        object_id = str(info_json['object_id'])
+            '{0}{1}/restapi/slugs/{2}.json'.format(base_url, version, video_id),
+            video_id, note='Fetching object ID')
+        object_id = compat_str(info_json['object_id'])
         meta_json = self._download_json(
-            '{0}{1}/restapi/media/{2}.json'.format(base_url, version, object_id), video_id)
+            '{0}{1}/restapi/media/{2}.json'.format(base_url, version, object_id),
+            video_id, note='Downloading metadata')
         uuid = meta_json['uuid']
         title = meta_json['title']
         wide = meta_json['is_wide']
@@ -34,7 +39,9 @@ class DctpTvIE(InfoExtractor):
             ratio = '4x3'
         play_path = 'mp4:{0}_dctp_0500_{1}.m4v'.format(uuid, ratio)
 
-        servers_json = self._download_json('http://www.dctp.tv/streaming_servers/', video_id)
+        servers_json = self._download_json(
+            'http://www.dctp.tv/streaming_servers/',
+            video_id, note='Downloading server list')
         url = servers_json[0]['endpoint']
 
         return {
