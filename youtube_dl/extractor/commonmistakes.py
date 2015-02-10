753fad4adc32b57a0d18518fddd06d2411d09635
Philipp Hagemeister <phihag@phihag.de>
2015-02-10 01:34:01 +0100

    [commonmistakes] Correct logic error

--- youtube_dl/extractor/commonmistakes.py
+++ youtube_dl/extractor/commonmistakes.py
@@ -24,6 +24,6 @@ class CommonMistakesIE(InfoExtractor):
             'That doesn\'t make any sense. '
             'Simply remove the parameter in your command or configuration.'
         ) % url
-        if self._downloader.params.get('verbose'):
+        if not self._downloader.params.get('verbose'):
             msg += ' Add -v to the command line to see what arguments and configuration youtube-dl got.'
         raise ExtractorError(msg, expected=True)
