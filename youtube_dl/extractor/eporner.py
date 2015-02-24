e765ed3a9c4ba52d4709a4a696881eae3401efa0
logon84 <rubenlogon@yahoo.es>
2015-02-24 19:41:46 +0100

    [eporner] Fix redirect_code error

--- youtube_dl/extractor/eporner.py
+++ youtube_dl/extractor/eporner.py
@@ -35,6 +35,7 @@ class EpornerIE(InfoExtractor):
         title = self._html_search_regex(
             r'<title>(.*?) - EPORNER', webpage, 'title')
 
+
         redirect_url = 'http://www.eporner.com/config5/%s/%s' % (video_id, display_id)
         player_code = self._download_webpage(
             redirect_url, display_id, note='Downloading player config')
