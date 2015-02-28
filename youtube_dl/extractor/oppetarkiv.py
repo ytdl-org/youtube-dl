e143f5dae9c767529b8b522a9df63ac0ee8fc356
Sergey M․ <dstftw@gmail.com>
2015-02-28 21:12:06 +0600

    [oppetarkiv] Extract f4m formats and age limit

--- youtube_dl/extractor/oppetarkiv.py
+++ youtube_dl/extractor/oppetarkiv.py
@@ -8,17 +8,19 @@ from ..utils import (
 
 
 class OppetArkivIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?oppetarkiv.se/video/(?P<id>[0-9]+)'
+    _VALID_URL = r'https?://(?:www\.)?oppetarkiv\.se/video/(?P<id>[0-9]+)'
     _TEST = {
         'url': 'http://www.oppetarkiv.se/video/1058509/rederiet-sasong-1-avsnitt-1-av-318',
-        'md5': '7b95ca9bedeead63012b2d7c3992c28f',
+        'md5': '5c1eb616e59f733d4af77edc5177d2fe',
         'info_dict': {
             'id': '1058509',
-            'ext': 'mp4',
+            'ext': 'flv',
             'title': 'Farlig kryssning',
             'duration': 2566,
             'thumbnail': 're:^https?://.*[\.-]jpg$',
+            'age_limit': 0,
         },
+        'skip': 'Only works from Sweden',
     }
 
     def _real_extract(self, url):
@@ -33,11 +35,16 @@ class OppetArkivIE(InfoExtractor):
         formats = []
         for vr in video_info['videoReferences']:
             vurl = vr['url']
-            if determine_ext(vurl) == 'm3u8':
+            ext = determine_ext(vurl)
+            if ext == 'm3u8':
                 formats.extend(self._extract_m3u8_formats(
                     vurl, video_id,
                     ext='mp4', entry_protocol='m3u8_native',
                     m3u8_id=vr.get('playerType')))
+            elif ext == 'f4m':
+                formats.extend(self._extract_f4m_formats(
+                    vurl + '?hdcore=3.3.0', video_id,
+                    f4m_id=vr.get('playerType')))
             else:
                 formats.append({
                     'format_id': vr.get('playerType'),
@@ -47,10 +54,13 @@ class OppetArkivIE(InfoExtractor):
 
         duration = video_info.get('materialLength')
 
+        age_limit = 18 if video_info.get('inappropriateForChildren') else 0
+
         return {
             'id': video_id,
             'title': title,
             'formats': formats,
             'thumbnail': thumbnail,
             'duration': duration,
+            'age_limit': age_limit,
         }
