d97aae75724fc301243a00c5a71ac93b235d62fe
Sergey M․ <dstftw@gmail.com>
2015-04-06 21:24:17 +0600

    [spankbang] Improve and simplify

--- youtube_dl/extractor/spankbang.py
+++ youtube_dl/extractor/spankbang.py
@@ -1,25 +1,23 @@
-# coding: utf-8
 from __future__ import unicode_literals
 
-from .common import InfoExtractor
 import re
 
+from .common import InfoExtractor
+
+
 class SpankBangIE(InfoExtractor):
-    """Extractor for http://spankbang.com"""
-    
-    _VALID_URL = r"https?://(?:www\.)?spankbang\.com/(?P<id>\w+)/video/.*"
-    
+    _VALID_URL = r'https?://(?:(?:www|[a-z]{2})\.)?spankbang\.com/(?P<id>[\da-z]+)/video'
     _TEST = {
-        "url": "http://spankbang.com/3vvn/video/fantasy+solo",
-        "md5": "1cc433e1d6aa14bc376535b8679302f7",
-        "info_dict": {
-            "id": "3vvn",
-            "title": "fantasy solo",
-            "description": "Watch fantasy solo free HD porn video - 05 minutes - dillion harper masturbates on a bed free adult movies.",
-            "format": "720p",
-            "format_id": "720p",
-            "ext": "mp4",
-            "url": "http://spankbang.com/_3vvn/IjE0MjgyNjY5MTcuMzUi.IaGrcF-vDrvktMhjd-1fWixiCzU/title/720p__mp4"
+        'url': 'http://spankbang.com/3vvn/video/fantasy+solo',
+        'md5': '1cc433e1d6aa14bc376535b8679302f7',
+        'info_dict': {
+            'id': '3vvn',
+            'ext': 'mp4',
+            'title': 'fantasy solo',
+            'description': 'dillion harper masturbates on a bed',
+            'thumbnail': 're:^https?://.*\.jpg$',
+            'uploader': 'silly2587',
+            'age_limit': 18,
         }
     }
 
@@ -27,26 +25,36 @@ class SpankBangIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        title = self._html_search_regex(r"<h1>(?:<img.+?>)?(.*?)</h1>", webpage, "title")
-        
-        stream_key = self._html_search_regex(r"""var\s+stream_key\s*[=]\s*['"](.+?)['"]\s*;""", webpage, "stream_key")
-        
-        qualities = re.findall(r"<span.+?>([0-9]+p).*?</span>", webpage)
-        
-        formats = []
-        for q in sorted(qualities):
-            formats.append({
-                "format_id": q,
-                "format": q,
-                "ext": "mp4",
-                "url": "http://spankbang.com/_{0}/{1}/title/{2}__mp4".format(video_id, stream_key, q)
-            })
+        stream_key = self._html_search_regex(
+            r'''var\s+stream_key\s*=\s*['"](.+?)['"]''',
+            webpage, 'stream key')
+
+        formats = [{
+            'url': 'http://spankbang.com/_%s/%s/title/%sp__mp4' % (video_id, stream_key, height),
+            'ext': 'mp4',
+            'format_id': '%sp' % height,
+            'height': int(height),
+        } for height in re.findall(r'<span[^>]+q_(\d+)p', webpage)]
+        self._sort_formats(formats)
+
+        title = self._html_search_regex(
+            r'(?s)<h1>(.+?)</h1>', webpage, 'title')
+        description = self._search_regex(
+            r'class="desc"[^>]*>([^<]+)',
+            webpage, 'description', default=None)
+        thumbnail = self._og_search_thumbnail(webpage)
+        uploader = self._search_regex(
+            r'class="user"[^>]*>([^<]+)',
+            webpage, 'uploader', fatal=False)
+
+        age_limit = self._rta_search(webpage)
 
         return {
-            "id": video_id,
-            "title": title,
-            "description": self._og_search_description(webpage),
-            "formats": formats
+            'id': video_id,
+            'title': title,
+            'description': description,
+            'thumbnail': thumbnail,
+            'uploader': uploader,
+            'formats': formats,
+            'age_limit': age_limit,
         }
-
-# vim: tabstop=4 expandtab
