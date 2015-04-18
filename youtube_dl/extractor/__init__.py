8b8c1093b65ee02aad859ed8d82217312ed0d9d8
Shaun Walbridge <shaun.walbridge@gmail.com>
2015-04-18 00:37:04 -0400

    [EsriVideo] Add new extractor
    Add extractor for [videos.esri.com](https://videos.esri.com), a collection
    of videos relating to GIS.

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -695,6 +695,7 @@ from .vice import ViceIE
 from .viddler import ViddlerIE
 from .videobam import VideoBamIE
 from .videodetective import VideoDetectiveIE
+from .videoesri import VideoEsriIE
 from .videolecturesnet import VideoLecturesNetIE
 from .videofyme import VideofyMeIE
 from .videomega import VideoMegaIE
