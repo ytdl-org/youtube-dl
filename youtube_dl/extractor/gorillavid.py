67a4d13d68360138e24cad444be3963c2acc7b9e
Mister Hat <misterhat144@gmail.com>
2015-04-22 14:45:05 -0500

    fixed regex

--- youtube_dl/extractor/gorillavid.py
+++ youtube_dl/extractor/gorillavid.py
@@ -18,7 +18,7 @@ class GorillaVidIE(InfoExtractor):
     IE_DESC = 'GorillaVid.in, daclips.in, movpod.in, fastvideo.in and realvid.net'
     _VALID_URL = r'''(?x)
         https?://(?P<host>(?:www\.)?
-            (?:daclips\.in|gorillavid\.in|movpod\.in|fastvideo\.in|realvid.net))/
+            (?:daclips\.in|gorillavid\.in|movpod\.in|fastvideo\.in|realvid\.net))/
         (?:embed-)?(?P<id>[0-9a-zA-Z]+)(?:-[0-9]+x[0-9]+\.html)?
     '''
 
