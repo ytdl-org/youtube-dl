d7403332248a3893810f0461a682229552e1fd9e
Sergey M․ <dstftw@gmail.com>
2015-04-23 21:59:18 +0600

    [cracked] Modernize

--- youtube_dl/extractor/cracked.py
+++ youtube_dl/extractor/cracked.py
@@ -37,8 +37,7 @@ class CrackedIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        mobj = re.match(self._VALID_URL, url)
-        video_id = mobj.group('id')
+        video_id = self._match_id(url)
 
         webpage = self._download_webpage(url, video_id)
 
