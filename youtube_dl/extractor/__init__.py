5196b988971716b9e9c5884d33c757a41aa4548a
Naglis Jonaitis <njonaitis@gmail.com>
2015-05-30 14:16:18 +0300

    [tubitv] Add new extractor (Closes #5524)

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -569,6 +569,7 @@ from .traileraddict import TrailerAddictIE
 from .trilulilu import TriluliluIE
 from .trutube import TruTubeIE
 from .tube8 import Tube8IE
+from .tubitv import TubiTvIE
 from .tudou import TudouIE
 from .tumblr import TumblrIE
 from .tunein import TuneInIE
