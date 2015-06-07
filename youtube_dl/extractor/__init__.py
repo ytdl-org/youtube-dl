87446dc6186c7e4247fd7f9bc1046ef41f5d1a0f
Hannu Lintala <hannu.lintala@gmail.com>
2015-06-07 17:25:30 +0300

    [tvc] Add extractor (Closes #5795)

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -582,6 +582,7 @@ from .tv2 import (
     TV2ArticleIE,
 )
 from .tv4 import TV4IE
+from .tvc import TVCIE
 from .tvigle import TvigleIE
 from .tvp import TvpIE, TvpSeriesIE
 from .tvplay import TVPlayIE
