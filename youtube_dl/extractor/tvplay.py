755a9d3d1a8f99b061c8d29525d629b8ad6061a4
Naglis Jonaitis <njonaitis@gmail.com>
2015-06-14 20:58:15 +0300

    [tvplay] Add support for NovaTv

--- youtube_dl/extractor/tvplay.py
+++ youtube_dl/extractor/tvplay.py
@@ -26,6 +26,7 @@ class TVPlayIE(InfoExtractor):
            viasat4play\.no/programmer|
            tv6play\.no/programmer|
            tv3play\.dk/programmer|
+           play\.novatv\.bg/programi
         )/[^/]+/(?P<id>\d+)
         '''
     _TESTS = [
@@ -167,6 +168,22 @@ class TVPlayIE(InfoExtractor):
                 'duration': 2594,
                 'timestamp': 1393236292,
                 'upload_date': '20140224',
+            },
+            'params': {
+                # rtmp download
+                'skip_download': True,
+            },
+        },
+        {
+            'url': 'http://play.novatv.bg/programi/zdravei-bulgariya/624952?autostart=true',
+            'info_dict': {
+                'id': '624952',
+                'ext': 'flv',
+                'title': 'Здравей, България (12.06.2015 г.) ',
+                'description': 'md5:99f3700451ac5bb71a260268b8daefd7',
+                'duration': 8838,
+                'timestamp': 1434100372,
+                'upload_date': '20150612',
             },
             'params': {
                 # rtmp download
