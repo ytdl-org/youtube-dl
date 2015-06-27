b2575b38e76e14b7552a43cd707a03fe78748f40
Sergey M․ <dstftw@gmail.com>
2015-06-27 14:38:41 +0600

    [options] Clarify --youtube-skip-dash-manifest

--- youtube_dl/options.py
+++ youtube_dl/options.py
@@ -346,7 +346,7 @@ def parseOpts(overrideArguments=None):
     video_format.add_option(
         '--youtube-skip-dash-manifest',
         action='store_false', dest='youtube_include_dash_manifest',
-        help='Do not download the DASH manifest on YouTube videos')
+        help='Do not download the DASH manifests and related data on YouTube videos')
     video_format.add_option(
         '--merge-output-format',
         action='store', dest='merge_output_format', metavar='FORMAT', default=None,
