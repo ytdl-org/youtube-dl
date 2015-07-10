a5839317aa5b41e7bc99ecae7405459bbcbff8a7
Sergey M․ <dstftw@gmail.com>
2015-07-10 21:52:03 +0600

    [vk] Use `_form_hidden_inputs` when logging in

--- youtube_dl/extractor/vk.py
+++ youtube_dl/extractor/vk.py
@@ -154,9 +154,7 @@ class VKIE(InfoExtractor):
         login_page = self._download_webpage(
             'https://vk.com', None, 'Downloading login page')
 
-        login_form = dict(re.findall(
-            r'<input\s+type="hidden"\s+name="([^"]+)"\s+(?:id="[^"]+"\s+)?value="([^"]*)"',
-            login_page))
+        login_form = self._form_hidden_inputs(login_page)
 
         login_form.update({
             'email': username.encode('cp1251'),
