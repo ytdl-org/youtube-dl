75a40b22511b4d4344cd6903e28429fa6c3991f1
Seamus Phelan <SeamusPhelan@gmail.com>
2015-07-13 22:35:45 +1000

    [SBS] fixes due to website changes

--- youtube_dl/extractor/sbs.py
+++ youtube_dl/extractor/sbs.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import re
 from .common import InfoExtractor
-from ..utils import (
-    js_to_json,
-    remove_end,
-)
+from ..utils import remove_end
 
 
 class SBSIE(InfoExtractor):
@@ -34,18 +30,22 @@ class SBSIE(InfoExtractor):
     def _real_extract(self, url):
         video_id = self._match_id(url)
 
-        webpage = self._download_webpage(url, video_id)
+        # the video is in the following iframe
+        iframe_url = 'http://www.sbs.com.au/ondemand/video/single/' + video_id + '?context=web'
+        webpage = self._download_webpage(iframe_url, video_id)
 
-        player = self._search_regex(
-            r'(?s)playerParams\.releaseUrls\s*=\s*(\{.*?\n\});\n',
-            webpage, 'player')
-        player = re.sub(r"'\s*\+\s*[\da-zA-Z_]+\s*\+\s*'", '', player)
+        player_params = self._search_regex(
+            r'(?s)(playerParams.+?releaseUrls.+?\n)',
+            webpage, 'playerParams')
+        player_params_js = self._search_regex(
+            r'({.*})',
+            player_params, 'player_param_js')
 
-        release_urls = self._parse_json(js_to_json(player), video_id)
+        player_params_json = self._parse_json(player_params_js, video_id)
 
-        theplatform_url = release_urls.get('progressive') or release_urls['standard']
+        theplatform_url = player_params_json.get('releaseUrls')['progressive'] or player_params_json.get('releaseUrls')['standard']
 
-        title = remove_end(self._og_search_title(webpage), ' (The Feed)')
+        title = remove_end(self._og_search_title(webpage, default=video_id, fatal=False), ' (The Feed)')
         description = self._html_search_meta('description', webpage)
         thumbnail = self._og_search_thumbnail(webpage)
 
