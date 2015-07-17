4a632911443f0dbc2384fb82ade85382aeecc8dc
fnord <fnord@fnord.mobi>
2015-07-17 09:46:08 -0500

    Add tests for compat_urllib_parse_unquote

--- test/test_compat.py
+++ test/test_compat.py
@@ -14,6 +14,7 @@ from youtube_dl.utils import get_filesystem_encoding
 from youtube_dl.compat import (
     compat_getenv,
     compat_expanduser,
+    compat_urllib_parse_unquote,
 )
 
 
@@ -41,6 +42,25 @@ class TestCompat(unittest.TestCase):
             lambda c: '_' in c and not c.startswith('_'),
             dir(youtube_dl.compat))) - set(['unicode_literals'])
         self.assertEqual(all_names, sorted(present_names))
+
+    def test_compat_urllib_parse_unquote(self):
+        test_strings = [
+            ['''''', ''''''],
+            ['''津波''', '''%E6%B4%A5%E6%B3%A2'''],
+            ['''津波''', str('%E6%B4%A5%E6%B3%A2')],
+            ['''<meta property="og:description" content="▁▂▃▄%▅▆▇█" />
+%<a href="https://ar.wikipedia.org/wiki/تسونامي">%a''',
+             '''<meta property="og:description" content="%E2%96%81%E2%96%82%E2%96%83%E2%96%84%25%E2%96%85%E2%96%86%E2%96%87%E2%96%88" />
+%<a href="https://ar.wikipedia.org/wiki/%D8%AA%D8%B3%D9%88%D9%86%D8%A7%D9%85%D9%8A">%a'''],
+            ['''(^◣_◢^)っ︻デ═一    ⇀    ⇀    ⇀    ⇀    ⇀    ↶%I%Break%Things%''',
+             '''%28%5E%E2%97%A3_%E2%97%A2%5E%29%E3%81%A3%EF%B8%BB%E3%83%87%E2%95%90%E4%B8%80    %E2%87%80    %E2%87%80    %E2%87%80    %E2%87%80    %E2%87%80    %E2%86%B6%I%Break%25Things%''']
+        ]
+        for test in test_strings:
+            strutf = test[0]
+            strurlenc = test[1]
+            strurldec = compat_urllib_parse_unquote(strurlenc)
+            self.assertEqual(strutf, strurldec)
+            self.assertEqual(strutf, compat_urllib_parse_unquote(strurlenc))
 
 if __name__ == '__main__':
     unittest.main()
