a0f28f90fa277d9c00f0305624dea36a20b8066e
fnord <fnord@fnord.mobi>
2015-07-17 01:50:43 -0500

    remove kebab

--- youtube_dl/compat.py
+++ youtube_dl/compat.py
@@ -108,7 +108,7 @@ except ImportError:
 
     compat_urllib_parse_asciire = re.compile('([\x00-\x7f]+)')
 
-    def new_compat_urllib_parse_unquote(string, encoding='utf-8', errors='replace'):
+    def compat_urllib_parse_unquote(string, encoding='utf-8', errors='replace'):
         """Replace %xx escapes by their single-character equivalent. The optional
         encoding and errors parameters specify how to decode percent-encoded
         sequences into Unicode characters, as accepted by the bytes.decode()
@@ -141,45 +141,6 @@ except ImportError:
                     bar = bar.decode('utf-8')
                 append(bar)
         return ''.join(res)
-
-    def old_compat_urllib_parse_unquote(string, encoding='utf-8', errors='replace'):
-        if string == '':
-            return string
-        res = string.split('%')
-        if len(res) == 1:
-            return string
-        if encoding is None:
-            encoding = 'utf-8'
-        if errors is None:
-            errors = 'replace'
-        # pct_sequence: contiguous sequence of percent-encoded bytes, decoded
-        pct_sequence = b''
-        string = res[0]
-        for item in res[1:]:
-            try:
-                if not item:
-                    raise ValueError
-                if not re.match('[0-9a-fA-F][0-9a-fA-F]',item[:2]):
-                    raise ValueError
-                pct_sequence += item[:2].decode('hex')
-                rest = item[2:]
-                if not rest:
-                    # This segment was just a single percent-encoded character.
-                    # May be part of a sequence of code units, so delay decoding.
-                    # (Stored in pct_sequence).
-                    continue
-            except ValueError:
-                rest = '%' + item
-            # Encountered non-percent-encoded characters. Flush the current
-            # pct_sequence.
-            string += pct_sequence.decode(encoding, errors) + rest
-            pct_sequence = b''
-        if pct_sequence:
-            # Flush the final pct_sequence
-            string += pct_sequence.decode(encoding, errors)
-        return string
-
-    compat_urllib_parse_unquote = new_compat_urllib_parse_unquote
 
 try:
     compat_str = unicode  # Python 2
