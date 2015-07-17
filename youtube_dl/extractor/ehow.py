c60e8cfaf7405e8b8075f06a62e7547adad8beba
Sergey M․ <dstftw@gmail.com>
2015-07-17 23:54:38 +0600

    [ehow] Simplify

--- youtube_dl/extractor/ehow.py
+++ youtube_dl/extractor/ehow.py
@@ -1,9 +1,7 @@
 from __future__ import unicode_literals
 
-from ..compat import (
-    compat_urllib_parse_unquote,
-)
 from .common import InfoExtractor
+from ..compat import compat_urllib_parse_unquote
 
 
 class EHowIE(InfoExtractor):
