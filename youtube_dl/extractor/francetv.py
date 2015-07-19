1d18e26ecad636cb88ee9710807e9583eec98cd4
Sergey M․ <dstftw@gmail.com>
2015-07-19 21:54:12 +0600

    [francetv] Remove unused import

--- youtube_dl/extractor/francetv.py
+++ youtube_dl/extractor/francetv.py
@@ -6,10 +6,7 @@ import re
 import json
 
 from .common import InfoExtractor
-from ..compat import (
-    compat_urllib_parse_urlparse,
-    compat_urlparse,
-)
+from ..compat import compat_urlparse
 from ..utils import (
     clean_html,
     ExtractorError,
