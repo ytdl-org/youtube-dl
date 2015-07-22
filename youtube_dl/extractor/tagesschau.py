ca4456eda8f49ed2f939c7ae5cfe55b45777217c
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2015-07-22 23:23:38 +0200

    [tagesschau] Remove unused import

--- youtube_dl/extractor/tagesschau.py
+++ youtube_dl/extractor/tagesschau.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import re
 
 from .common import InfoExtractor
-from ..utils import parse_filesize, ExtractorError
+from ..utils import parse_filesize
 
 
 class TagesschauIE(InfoExtractor):
