240ca32e57a027ff8cec8617c154bb7100bead1a
Sergey M․ <dstftw@gmail.com>
2015-08-13 01:00:05 +0600

    [rtvnh] Carry long lines

--- youtube_dl/extractor/rtvnh.py
+++ youtube_dl/extractor/rtvnh.py
@@ -19,12 +19,16 @@ class RTVNHIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        meta = self._parse_json(self._download_webpage('http://www.rtvnh.nl/video/json?m=' + video_id, video_id), video_id)
-        formats = self._extract_smil_formats('http://www.rtvnh.nl/video/smil?m=' + video_id, video_id)
+
+        meta = self._parse_json(self._download_webpage(
+            'http://www.rtvnh.nl/video/json?m=' + video_id, video_id), video_id)
+        formats = self._extract_smil_formats(
+            'http://www.rtvnh.nl/video/smil?m=' + video_id, video_id)
 
         for item in meta['source']['fb']:
             if item.get('type') == 'hls':
-                formats.extend(self._extract_m3u8_formats(item['file'], video_id, ext='mp4', entry_protocol='m3u8_native'))
+                formats.extend(self._extract_m3u8_formats(
+                    item['file'], video_id, ext='mp4', entry_protocol='m3u8_native'))
             elif item.get('type') == '':
                 formats.append({'url': item['file']})
         
