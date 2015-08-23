3905b06f29748a4bf5cb9113e00bac4c22a49fea
clauderains <clauderains3319@gmail.com>
2015-08-23 02:47:20 -0700

    [spankwire] Support new cdn video url format

--- youtube_dl/extractor/spankwire.py
+++ youtube_dl/extractor/spankwire.py
@@ -17,20 +17,34 @@ from ..aes import aes_decrypt_text
 
 class SpankwireIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?(?P<url>spankwire\.com/[^/]*/video(?P<videoid>[0-9]+)/?)'
-    _TEST = {
-        'url': 'http://www.spankwire.com/Buckcherry-s-X-Rated-Music-Video-Crazy-Bitch/video103545/',
-        'md5': '8bbfde12b101204b39e4b9fe7eb67095',
-        'info_dict': {
-            'id': '103545',
-            'ext': 'mp4',
-            'title': 'Buckcherry`s X Rated Music Video Crazy Bitch',
-            'description': 'Crazy Bitch X rated music video.',
-            'uploader': 'oreusz',
-            'uploader_id': '124697',
-            'upload_date': '20070507',
-            'age_limit': 18,
-        }
-    }
+    _TESTS = [{
+                'url': 'http://www.spankwire.com/Buckcherry-s-X-Rated-Music-Video-Crazy-Bitch/video103545/',
+                'md5': '8bbfde12b101204b39e4b9fe7eb67095',
+                    'info_dict': {
+                                    'id': '103545',
+                                    'ext': 'mp4',
+                                    'title': 'Buckcherry`s X Rated Music Video Crazy Bitch',
+                                    'description': 'Crazy Bitch X rated music video.',
+                                    'uploader': 'oreusz',
+                                    'uploader_id': '124697',
+                                    'upload_date': '20070507',
+                                    'age_limit': 18,
+                    }
+              },
+              {
+                'url': 'http://www.spankwire.com/Titcums-Compiloation-I/video1921551/',
+                'md5': '09b3c20833308b736ae8902db2f8d7e6',
+                    'info_dict': {
+                                    'id': '1921551',
+                                    'ext': 'mp4',
+                                    'title': 'Titcums Compiloation I',
+                                    'description': 'cum on tits',
+                                    'uploader': 'dannyh78999',
+                                    'uploader_id': '3056053',
+                                    'upload_date': '20150822',
+                                    'age_limit': 18,
+                    }
+              }]
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
@@ -82,18 +96,36 @@ class SpankwireIE(InfoExtractor):
         for video_url in video_urls:
             path = compat_urllib_parse_urlparse(video_url).path
             format = path.split('/')[4].split('_')[:2]
-            resolution, bitrate_str = format
-            format = "-".join(format)
-            height = int(resolution.rstrip('Pp'))
-            tbr = int(bitrate_str.rstrip('Kk'))
-            formats.append({
-                'url': video_url,
-                'resolution': resolution,
-                'format': format,
-                'tbr': tbr,
-                'height': height,
-                'format_id': format,
-            })
+            if format[0] == 'mp4':
+                format_id, quality = format
+                format = "-".join(format)
+                if quality == 'normal':
+                    height = 180
+                elif quality == 'high':
+                    height = 240
+                elif quality == 'ultra':
+                    height = 480
+                elif quality == '720p':
+                    height = 720
+                formats.append({
+                    'url': video_url,
+                    'format': format,
+                    'height': height,
+                    'format_id': format,
+                })
+            else:
+                resolution, bitrate_str = format
+                format = "-".join(format)
+                height = int(resolution.rstrip('Pp'))
+                tbr = int(bitrate_str.rstrip('Kk'))
+                formats.append({
+                    'url': video_url,
+                    'resolution': resolution,
+                    'format': format,
+                    'tbr': tbr,
+                    'height': height,
+                    'format_id': format,
+                })
         self._sort_formats(formats)
 
         age_limit = self._rta_search(webpage)
