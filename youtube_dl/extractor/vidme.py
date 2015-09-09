482aa3feccd70e773346bfd4598cba4ea6e80621
Sergey M․ <dstftw@gmail.com>
2015-09-09 22:04:05 +0600

    [vidme] Simplify, make more robust, extract more metadata and capture errors (Closes #6812)

--- youtube_dl/extractor/vidme.py
+++ youtube_dl/extractor/vidme.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
-from .common import InfoExtractor, ExtractorError
+from .common import InfoExtractor
+from ..compat import compat_HTTPError
 from ..utils import (
+    ExtractorError,
     int_or_none,
     float_or_none,
     parse_iso8601,
@@ -18,10 +20,11 @@ class VidmeIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'Fishing for piranha - the easy way',
             'description': 'source: https://www.facebook.com/photo.php?v=312276045600871',
-            'duration': 119.92,
+            'thumbnail': 're:^https?://.*\.jpg',
             'timestamp': 1406313244,
             'upload_date': '20140725',
-            'thumbnail': 're:^https?://.*\.jpg',
+            'age_limit': 0,
+            'duration': 119.92,
             'view_count': int,
             'like_count': int,
             'comment_count': int,
@@ -33,13 +36,15 @@ class VidmeIE(InfoExtractor):
             'id': 'Gc6M',
             'ext': 'mp4',
             'title': 'O Mere Dil ke chain - Arnav and Khushi VM',
-            'duration': 223.72,
+            'thumbnail': 're:^https?://.*\.jpg',
             'timestamp': 1441211642,
             'upload_date': '20150902',
-            'thumbnail': 're:^https?://.*\.jpg',
+            'uploader': 'SunshineM',
+            'uploader_id': '3552827',
+            'age_limit': 0,
+            'duration': 223.72,
             'view_count': int,
             'like_count': int,
-            'comment_count': int,
             'comment_count': int,
         },
         'params': {
@@ -53,11 +58,13 @@ class VidmeIE(InfoExtractor):
             'ext': 'mp4',
             'title': 'The Carver',
             'description': 'md5:e9c24870018ae8113be936645b93ba3c',
-            'duration': 97.859999999999999,
+            'thumbnail': 're:^https?://.*\.jpg',
             'timestamp': 1433203629,
             'upload_date': '20150602',
             'uploader': 'Thomas',
-            'thumbnail': 're:^https?://.*\.jpg',
+            'uploader_id': '109747',
+            'age_limit': 0,
+            'duration': 97.859999999999999,
             'view_count': int,
             'like_count': int,
             'comment_count': int,
@@ -66,52 +73,79 @@ class VidmeIE(InfoExtractor):
             'skip_download': True,
         },
     }, {
-        # From http://naked-yogi.tumblr.com/post/118312946248/naked-smoking-stretching
+        # nsfw test from http://naked-yogi.tumblr.com/post/118312946248/naked-smoking-stretching
         'url': 'https://vid.me/e/Wmur',
-        'only_matching': True,
+        'info_dict': {
+            'id': 'Wmur',
+            'ext': 'mp4',
+            'title': 'naked smoking & stretching',
+            'thumbnail': 're:^https?://.*\.jpg',
+            'timestamp': 1430931613,
+            'upload_date': '20150506',
+            'uploader': 'naked-yogi',
+            'uploader_id': '1638622',
+            'age_limit': 18,
+            'duration': 653.26999999999998,
+            'view_count': int,
+            'like_count': int,
+            'comment_count': int,
+        },
+        'params': {
+            'skip_download': True,
+        },
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        api_url = 'https://api.vid.me/videoByUrl/' + video_id
-        data = self._download_json(api_url, video_id)
 
-        video_data = data.get('video')
-        if video_data is None:
-            raise ExtractorError('Could not extract the vid.me video data')
+        try:
+            response = self._download_json(
+                'https://api.vid.me/videoByUrl/%s' % video_id, video_id)
+        except ExtractorError as e:
+            if isinstance(e.cause, compat_HTTPError) and e.cause.code == 400:
+                response = self._parse_json(e.cause.read(), video_id)
+            else:
+                raise
 
-        title = video_data.get('title')
-        description = video_data.get('description')
-        thumbnail = video_data.get('thumbnail_url')
-        timestamp = parse_iso8601(video_data.get('date_created'), ' ')
-        duration = float_or_none(video_data.get('duration'))
-        view_count = int_or_none(video_data.get('view_count'))
-        like_count = int_or_none(video_data.get('likes_count'))
-        comment_count = int_or_none(video_data.get('comment_count'))
+        error = response.get('error')
+        if error:
+            raise ExtractorError(
+                '%s returned error: %s' % (self.IE_NAME, error), expected=True)
 
-        uploader = None
-        user_data = video_data.get('user')
-        if user_data is not None:
-            uploader = user_data.get('username')
+        video = response['video']
 
         formats = [{
-            'format_id': format['type'],
-            'url': format['uri'],
-            'width': int_or_none(format['width']),
-            'height': int_or_none(format['height']),
-        } for format in video_data.get('formats', [])]
+            'format_id': f.get('type'),
+            'url': f['uri'],
+            'width': int_or_none(f.get('width')),
+            'height': int_or_none(f.get('height')),
+        } for f in video.get('formats', []) if f.get('uri')]
         self._sort_formats(formats)
+
+        title = video['title']
+        description = video.get('description')
+        thumbnail = video.get('thumbnail_url')
+        timestamp = parse_iso8601(video.get('date_created'), ' ')
+        uploader = video.get('user', {}).get('username')
+        uploader_id = video.get('user', {}).get('user_id')
+        age_limit = 18 if video.get('nsfw') is True else 0
+        duration = float_or_none(video.get('duration'))
+        view_count = int_or_none(video.get('view_count'))
+        like_count = int_or_none(video.get('likes_count'))
+        comment_count = int_or_none(video.get('comment_count'))
 
         return {
             'id': video_id,
             'title': title,
             'description': description,
             'thumbnail': thumbnail,
+            'uploader': uploader,
+            'uploader_id': uploader_id,
+            'age_limit': age_limit,
             'timestamp': timestamp,
             'duration': duration,
             'view_count': view_count,
             'like_count': like_count,
             'comment_count': comment_count,
-            'uploader': uploader,
             'formats': formats,
         }
