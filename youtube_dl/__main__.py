f04e2382ac7281adac655619f1b7bcc59ae72bb0
Yen Chi Hsuan <yan12125@gmail.com>
2015-09-15 21:30:24 +0800

    Use `insert` for all sys.path manipulations

--- youtube_dl/__main__.py
+++ youtube_dl/__main__.py
@@ -11,7 +11,7 @@ if __package__ is None and not hasattr(sys, "frozen"):
     # direct call of __main__.py
     import os.path
     path = os.path.realpath(os.path.abspath(__file__))
-    sys.path.append(os.path.dirname(os.path.dirname(path)))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
 import youtube_dl
 
