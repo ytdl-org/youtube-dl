b24f40fa752fc1754f28ef750f4a094072d8078c
Yen Chi Hsuan <yan12125@gmail.com>
2015-09-25 17:58:44 +0800

    [fktv] Correct thumbnail extraction and add the test

--- youtube_dl/extractor/fktv.py
+++ youtube_dl/extractor/fktv.py
@@ -21,6 +21,7 @@ class FKTVIE(InfoExtractor):
             'id': '1',
             'ext': 'mp4',
             'title': 'Folge 1 vom 10. April 2007',
+            'thumbnail': 're:^https?://.*\.jpg$',
         },
     }
 
@@ -29,11 +30,14 @@ class FKTVIE(InfoExtractor):
 
         webpage = self._download_webpage('http://fernsehkritik.tv/folge-%s/play' % episode, episode)
         title = clean_html(self._html_search_regex('<h3>([^<]+)</h3>', webpage, 'title'))
-        matches = re.search(r'(?s)<video[^>]+poster="([^"]+)"[^>]*>(.*)</video>', webpage)
+        matches = re.search(r'(?s)<video[^>]+(?:poster="([^"]+)")?[^>]*>(.*)</video>', webpage)
         if matches is None:
             raise ExtractorError('Unable to extract the video')
 
         poster, sources = matches.groups()
+        if poster is None:
+            self.report_warning('unable to extract thumbnail')
+
         urls = re.findall(r'<source[^>]+src="([^"]+)"', sources)
         formats = [{'url': url, 'format_id': determine_ext(url)} for url in urls]
         return {
