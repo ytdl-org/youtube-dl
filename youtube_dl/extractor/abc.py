638a71d9be1afb4efcd695ab824901c89d2aaf6e
remitamine <remitamine@gmail.com>
2015-10-23 07:09:41 +0100

    [abc] add support for audio extraction

--- youtube_dl/extractor/abc.py
+++ youtube_dl/extractor/abc.py
@@ -12,7 +12,7 @@ from ..utils import (
 
 class ABCIE(InfoExtractor):
     IE_NAME = 'abc.net.au'
-    _VALID_URL = r'http://www\.abc\.net\.au/news/[^/]+/[^/]+/(?P<id>\d+)'
+    _VALID_URL = r'http://www\.abc\.net\.au/news/([^/]+/){1,2}(?P<id>\d+)'
 
     _TESTS = [{
         'url': 'http://www.abc.net.au/news/2014-11-05/australia-to-staff-ebola-treatment-centre-in-sierra-leone/5868334',
@@ -36,6 +36,15 @@ class ABCIE(InfoExtractor):
             'title': 'Marriage Equality: Warren Entsch introduces same sex marriage bill',
         },
         'add_ie': ['Youtube'],
+    }, {
+        'url': 'http://www.abc.net.au/news/2015-10-23/nab-lifts-interest-rates-following-westpac-and-cba/6880080',
+        'md5': 'b96eee7c9edf4fc5a358a0252881cc1f',
+        'info_dict': {
+            'id': '6880080',
+            'ext': 'mp3',
+            'title': 'NAB lifts interest rates, following Westpac and CBA',
+            'description': 'md5:f13d8edc81e462fce4a0437c7dc04728',
+        },
     }]
 
     def _real_extract(self, url):
@@ -43,7 +52,7 @@ class ABCIE(InfoExtractor):
         webpage = self._download_webpage(url, video_id)
 
         mobj = re.search(
-            r'inline(?P<type>Video|YouTube)Data\.push\((?P<json_data>[^)]+)\);',
+            r'inline(?P<type>Video|Audio|YouTube)Data\.push\((?P<json_data>[^)]+)\);',
             webpage)
         if mobj is None:
             raise ExtractorError('Unable to extract video urls')
@@ -58,13 +67,22 @@ class ABCIE(InfoExtractor):
             return self.playlist_result([
                 self.url_result(url_info['url']) for url_info in urls_info])
 
-        formats = [{
-            'url': url_info['url'],
-            'width': int_or_none(url_info.get('width')),
-            'height': int_or_none(url_info.get('height')),
-            'tbr': int_or_none(url_info.get('bitrate')),
-            'filesize': int_or_none(url_info.get('filesize')),
-        } for url_info in urls_info]
+        if mobj.group('type') == 'Video':
+            formats = [{
+                'url': url_info['url'],
+                'vcodec': url_info.get('codec'),
+                'width': int_or_none(url_info.get('width')),
+                'height': int_or_none(url_info.get('height')),
+                'tbr': int_or_none(url_info.get('bitrate')),
+                'filesize': int_or_none(url_info.get('filesize')),
+            } for url_info in urls_info]
+        else:
+            formats = [{
+                'url': url_info['url'],
+                'vcodec': 'none',
+                'tbr': int_or_none(url_info.get('bitrate')),
+                'filesize': int_or_none(url_info.get('filesize')),
+            } for url_info in urls_info]
         self._sort_formats(formats)
 
         return {
