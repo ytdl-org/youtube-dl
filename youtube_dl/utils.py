7aefc49c4013efb5056b2c1237e22c52cb5d3c49
Sergey M․ <dstftw@gmail.com>
2015-11-16 20:20:16 +0600

    [utils] Skip invalid/non HTML entities (Closes #7518)

--- youtube_dl/utils.py
+++ youtube_dl/utils.py
@@ -396,7 +396,11 @@ def _htmlentity_transform(entity):
             numstr = '0%s' % numstr
         else:
             base = 10
-        return compat_chr(int(numstr, base))
+        # See https://github.com/rg3/youtube-dl/issues/7518
+        try:
+            return compat_chr(int(numstr, base))
+        except ValueError:
+            pass
 
     # Unknown entity in name, return its literal representation
     return ('&%s;' % entity)
