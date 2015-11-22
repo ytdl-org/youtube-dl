7cff2793bf1a104754721c08b8f37a55b6e95b5a
mdf <>
2015-11-21 23:27:22 -0500

    [youtube] Fallback instead of failing when title can't be determined

--- youtube_dl/extractor/youtube.py
+++ youtube_dl/extractor/youtube.py
@@ -1631,9 +1631,14 @@ class YoutubePlaylistIE(YoutubeBaseInfoExtractor, YoutubePlaylistBaseInfoExtract
             else:
                 self.report_warning('Youtube gives an alert message: ' + match)
 
-        playlist_title = self._html_search_regex(
-            r'(?s)<h1 class="pl-header-title[^"]*"[^>]*>\s*(.*?)\s*</h1>',
-            page, 'title')
+        playlist_title = str(playlist_id)
+
+        try:
+            playlist_title = self._html_search_regex(
+                r'(?s)<h1 class="pl-header-title[^"]*"[^>]*>\s*(.*?)\s*</h1>',
+                page, 'title')
+        except:
+            self.report_warning('Cannot extract playlist title, using playlist id instead')
 
         return self.playlist_result(self._entries(page, playlist_id), playlist_id, playlist_title)
 
