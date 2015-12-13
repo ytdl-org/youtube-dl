322ec4c96294efde940334b9ccb00e25841a4a93
Boudjada Messaoud <messaoudinelec@gmail.com>
2015-12-13 19:07:29 +0100

    add tor proxy

--- youtube_dl/compat.py
+++ youtube_dl/compat.py
@@ -11,12 +11,20 @@ import re
 import shlex
 import shutil
 import socket
+import socks
 import subprocess
 import sys
 import itertools
 import xml.etree.ElementTree
+def create_connection(address, timeout=None, source_address=None):
+	sock = socks.socksocket()
+	sock.connect(address)
+	return sock
 
-
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
+# patch the socket module
+socket.socket = socks.socksocket
+socket.create_connection = create_connection
 try:
     import urllib.request as compat_urllib_request
 except ImportError:  # Python 2
