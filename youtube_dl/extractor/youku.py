1d189d15c51c0a51e7e74c4babe06575f3f475e2
Celthi <txtlu0@gmail.com>
2015-12-17 10:25:32 +0800

    [youku] adapt my modification to main repo

--- youtube_dl/extractor/youku.py
+++ youtube_dl/extractor/youku.py
@@ -87,10 +87,6 @@ class YoukuIE(InfoExtractor):
         # get oip
         oip = data['security']['ip']
 
-<<<<<<< HEAD
-=======
-        # get fileid
->>>>>>> githubMaster/youku_bugfix
         fileid_dict = {}
         for stream in data['stream']:
             format = stream.get('stream_type')
