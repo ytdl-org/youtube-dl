34a9da136f1a29889af01815b696672b9dd5b57d
Sergey M․ <dstftw@gmail.com>
2015-12-31 22:12:47 +0600

    [regiotv] Improve extraction (Closes #7915)

--- youtube_dl/extractor/regiotv.py
+++ youtube_dl/extractor/regiotv.py
@@ -1,61 +1,62 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 from __future__ import unicode_literals
-
-import re
 
 from .common import InfoExtractor
 
 from ..utils import (
     sanitized_Request,
+    xpath_text,
     xpath_with_ns,
 )
 
 
 class RegioTVIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?regio-tv\.de/video/(?P<id>[0-9]+).html'
-    _TESTS = [
-        {
-            'url': 'http://www.regio-tv.de/video/395808.html',
-            'info_dict': {
-                'id': '395808',
-                'ext': 'mp4',
-                'title': u'Wir in Ludwigsburg',
-                'description': u'Mit unseren zuckers\xfc\xdfen Adventskindern, au\xdferdem besuchen wir die Abendsterne!',
-            }
-        },
-    ]
+    _VALID_URL = r'https?://(?:www\.)?regio-tv\.de/video/(?P<id>[0-9]+)'
+    _TESTS = [{
+        'url': 'http://www.regio-tv.de/video/395808.html',
+        'info_dict': {
+            'id': '395808',
+            'ext': 'mp4',
+            'title': 'Wir in Ludwigsburg',
+            'description': 'Mit unseren zuckersüßen Adventskindern, außerdem besuchen wir die Abendsterne!',
+        }
+    }, {
+        'url': 'http://www.regio-tv.de/video/395808',
+        'only_matching': True,
+    }]
 
     def _real_extract(self, url):
-        mobj = re.match(self._VALID_URL, url)
-        video_id = mobj.group('id')
+        video_id = self._match_id(url)
 
         webpage = self._download_webpage(url, video_id)
-        key = self._html_search_regex(r''',key: "(.*?)"''', webpage, 'key')
 
-        title = self._html_search_regex(
-            r'<meta property="og:title" content="\s*(.*?)\s*"\s*/?\s*>',
-            webpage, 'title')
+        key = self._search_regex(
+            r'key\s*:\s*(["\'])(?P<key>.+?)\1', webpage, 'key', group='key')
+        title = self._og_search_title(webpage)
 
-        soapxml = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><getHTML5VideoData xmlns="http://v.telvi.de/"><key xsi:type="xsd:string">%s</key></getHTML5VideoData></soap:Body></soap:Envelope>' % key
-        request = sanitized_Request('http://v.telvi.de/?wsdl', soapxml)
-        request.add_header('Origin', 'http://www.regio-tv.de')
-        request.add_header('Referer', url)
-        video_data = self._download_xml(request, video_id, 'video data')
+        SOAP_TEMPLATE = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><{0} xmlns="http://v.telvi.de/"><key xsi:type="xsd:string">{1}</key></{0}></soap:Body></soap:Envelope>'
+
+        request = sanitized_Request(
+            'http://v.telvi.de/',
+            SOAP_TEMPLATE.format('GetHTML5VideoData', key).encode('utf-8'))
+        video_data = self._download_xml(request, video_id, 'Downloading video XML')
 
         NS_MAP = {
             'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
             'soap': 'http://schemas.xmlsoap.org/soap/envelope/',
         }
 
-        url = video_data.find(xpath_with_ns('.//video', NS_MAP)).text
-        thumbnail = video_data.find(xpath_with_ns('.//image', NS_MAP)).text
-
-        description = self._html_search_meta('description', webpage)
+        video_url = xpath_text(
+            video_data, xpath_with_ns('.//video', NS_MAP), 'video url', fatal=True)
+        thumbnail = xpath_text(
+            video_data, xpath_with_ns('.//image', NS_MAP), 'thumbnail')
+        description = self._og_search_description(
+            webpage) or self._html_search_meta('description', webpage)
 
         return {
             'id': video_id,
+            'url': video_url,
             'title': title,
-            'url': url,
-            'thumbnail': thumbnail,
             'description': description,
+            'thumbnail': thumbnail,
         }
