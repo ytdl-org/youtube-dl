a50a8003a08bc67bc99363f6738147a394a43210
Sergey M․ <dstftw@gmail.com>
2016-01-16 07:10:51 +0600

    [cultureunplugged] Improve (Closes #8060)

--- youtube_dl/extractor/__init__.py
+++ youtube_dl/extractor/__init__.py
@@ -130,6 +130,7 @@ from .crunchyroll import (
 )
 from .cspan import CSpanIE
 from .ctsnews import CtsNewsIE
+from .cultureunplugged import CultureUnpluggedIE
 from .cwtv import CWTVIE
 from .dailymotion import (
     DailymotionIE,
