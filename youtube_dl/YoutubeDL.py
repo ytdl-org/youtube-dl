4c77d3f52ad16d9d9ac0ff0f15a62beef3158ca4
Sergey M․ <dstftw@gmail.com>
2016-02-13 00:23:14 +0600

    [YoutubeDL] Allow bestvideo+bestaudio for any extractor

--- youtube_dl/YoutubeDL.py
+++ youtube_dl/YoutubeDL.py
@@ -1341,7 +1341,6 @@ class YoutubeDL(object):
         if req_format is None:
             req_format_list = []
             if (self.params.get('outtmpl', DEFAULT_OUTTMPL) != '-' and
-                    info_dict['extractor'] in ['youtube', 'ted'] and
                     not info_dict.get('is_live')):
                 merger = FFmpegMergerPP(self)
                 if merger.available and merger.can_merge():
