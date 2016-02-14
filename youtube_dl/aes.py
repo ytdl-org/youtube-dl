080e09557d0c323a9abf5f72e9d307f8bb545be6
Sergey M․ <dstftw@gmail.com>
2016-02-14 14:01:43 +0600

    [aes] Switch to new-style classes

--- youtube_dl/aes.py
+++ youtube_dl/aes.py
@@ -161,7 +161,7 @@ def aes_decrypt_text(data, password, key_size_bytes):
     nonce = data[:NONCE_LENGTH_BYTES]
     cipher = data[NONCE_LENGTH_BYTES:]
 
-    class Counter:
+    class Counter(object):
         __value = nonce + [0] * (BLOCK_SIZE_BYTES - NONCE_LENGTH_BYTES)
 
         def next_value(self):
