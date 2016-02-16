cb64ccc71531fd7491f65ad12fa1d68ac5de70e1
Yen Chi Hsuan <yan12125@gmail.com>
2016-02-16 09:07:38 +0800

    [facebook] Improve error handling (#8572)

--- youtube_dl/extractor/facebook.py
+++ youtube_dl/extractor/facebook.py
@@ -186,7 +186,7 @@ class FacebookIE(InfoExtractor):
         if not video_data:
             server_js_data = self._parse_json(self._search_regex(
                 r'handleServerJS\(({.+})\);', webpage, 'server js data'), video_id)
-            for item in server_js_data['instances']:
+            for item in server_js_data.get('instances', []):
                 if item[1][0] == 'VideoConfig':
                     video_data = video_data_list2dict(item[2][0]['videoData'])
                     break
