7710bdf4e813879dbd8c5857e13a2c64e0ce8837
Sander van den Oever <sandervdo@gmail.com>
2016-03-06 23:16:13 +0100

    Add initial ISSUE_TEMPLATE
    Add auto-updating of youtube-dl version in ISSUE_TEMPLATE
    
    Move parts of template text and adopt makefile to new format
    
    Moved the 'kind-of-issue' section and rephrased a bit
    
    Rephrased and moved Example URL section upwards
    
    Moved ISSUE_TEMPLATE inside .github folder.
    
    Update makefile to match new folderstructure

---
+++ devscripts/make_issue_template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
+import io
+import optparse
+import re
+
+
+def main():
+    parser = optparse.OptionParser(usage='%prog FILE')
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error('Expected an filename')
+
+    with io.open(args[0], encoding='utf-8') as inf:
+        issue_template_text = inf.read()
+
+    # Get the version from youtube_dl/version.py without importing the package
+    exec(compile(open('youtube_dl/version.py').read(),
+         'youtube_dl/version.py', 'exec'))
+
+    issue_template_text = re.sub(
+        r'(?<=\*\*)(?P<version>[0-9\.]+)(?=\*\*)',
+        __version__,
+        issue_template_text
+    )
+
+    with io.open(args[0], 'w', encoding='utf-8') as outf:
+         outf.write(issue_template_text)
+
+if __name__ == '__main__':
+    main()
