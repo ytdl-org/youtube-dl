a28d4654f3f4136929c772dadb1bc6df25968238
Quan Hua <quanhua92@gmail.com>
2016-03-16 10:15:39 +0700

    Change to tuble search_regex to fix #8753

--- youtube_dl/extractor/udemy.py
+++ youtube_dl/extractor/udemy.py
@@ -142,12 +142,10 @@ class UdemyIE(InfoExtractor):
         lecture_id = self._match_id(url)
 
         webpage = self._download_webpage(url, lecture_id)
-        try:
-            course_id = self._search_regex(
-            r'data-course-id=["\'](\d+)', webpage, 'course id')
-        except ExtractorError as e:
-            course_id = self._search_regex(
-               r'&quot;id&quot;: (\d+)', webpage, 'course id')
+        
+        course_id = self._search_regex(
+            (r'data-course-id=["\'](\d+)', r'&quot;id&quot;: (\d+)'),
+            webpage, 'course id')
 
         try:
             lecture = self._download_lecture(course_id, lecture_id)
