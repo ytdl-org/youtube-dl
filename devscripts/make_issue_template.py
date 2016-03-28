7168a6c874c1c0e4cffb6c1e29eeda322051def7
Sergey M․ <dstftw@gmail.com>
2016-03-29 03:05:15 +0600

    [devscripts/make_issue_template] Fix __version__ again

--- devscripts/make_issue_template.py
+++ devscripts/make_issue_template.py
@@ -16,13 +16,11 @@ def main():
     with io.open(infile, encoding='utf-8') as inf:
         issue_template_tmpl = inf.read()
 
-    __version__ = None
-
     # Get the version from youtube_dl/version.py without importing the package
     exec(compile(open('youtube_dl/version.py').read(),
                  'youtube_dl/version.py', 'exec'))
 
-    out = issue_template_tmpl % {'version': __version__}
+    out = issue_template_tmpl % {'version': locals()['__version__']}
 
     with io.open(outfile, 'w', encoding='utf-8') as outf:
         outf.write(out)
