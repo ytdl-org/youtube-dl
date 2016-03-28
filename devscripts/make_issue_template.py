3bf1df51fda4189eaa9164134b56393e2c4a7f72
Sergey M․ <dstftw@gmail.com>
2016-03-29 02:16:38 +0600

    [devscripts/make_issue_template] Rework to use ISSUE_TEMPLATE.tmpl (Closes #8785)

--- devscripts/make_issue_template.py
+++ devscripts/make_issue_template.py
@@ -3,30 +3,27 @@ from __future__ import unicode_literals
 
 import io
 import optparse
-import re
 
 
 def main():
-    parser = optparse.OptionParser(usage='%prog FILE')
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
     options, args = parser.parse_args()
-    if len(args) != 1:
-        parser.error('Expected an filename')
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
 
-    with io.open(args[0], encoding='utf-8') as inf:
-        issue_template_text = inf.read()
+    infile, outfile = args
+
+    with io.open(infile, encoding='utf-8') as inf:
+        issue_template_tmpl = inf.read()
 
     # Get the version from youtube_dl/version.py without importing the package
     exec(compile(open('youtube_dl/version.py').read(),
-         'youtube_dl/version.py', 'exec'))
+                 'youtube_dl/version.py', 'exec'))
 
-    issue_template_text = re.sub(
-        r'(?<=\*\*)(?P<version>[0-9\.]+)(?=\*\*)',
-        __version__,
-        issue_template_text
-    )
+    out = issue_template_tmpl % {'version': __version__}
 
-    with io.open(args[0], 'w', encoding='utf-8') as outf:
-         outf.write(issue_template_text)
+    with io.open(outfile, 'w', encoding='utf-8') as outf:
+        outf.write(out)
 
 if __name__ == '__main__':
     main()
