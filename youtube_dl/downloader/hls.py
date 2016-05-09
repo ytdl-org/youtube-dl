c15c47d19bfeeacd42f44dd7736f175711a91346
Sergey M․ <dstftw@gmail.com>
2016-05-09 20:45:03 +0600

    [downloader/hls] Remove EXT-X-MEDIA-SEQUENCE from unsupported features for hlsnative

--- youtube_dl/downloader/hls.py
+++ youtube_dl/downloader/hls.py
@@ -23,7 +23,9 @@ class HlsFD(FragmentFD):
         UNSUPPORTED_FEATURES = (
             r'#EXT-X-KEY:METHOD=(?!NONE)',  # encrypted streams [1]
             r'#EXT-X-BYTERANGE',  # playlists composed of byte ranges of media files [2]
-            r'#EXT-X-MEDIA-SEQUENCE:(?!0$)',  # live streams [3]
+            # Live streams heuristic does not always work (e.g. geo restricted to Germany
+            # http://hls-geo.daserste.de/i/videoportal/Film/c_620000/622873/format,716451,716457,716450,716458,716459,.mp4.csmil/index_4_av.m3u8?null=0)
+            #r'#EXT-X-MEDIA-SEQUENCE:(?!0$)',  # live streams [3]
             # 1. https://tools.ietf.org/html/draft-pantos-http-live-streaming-17#section-4.3.2.4
             # 2. https://tools.ietf.org/html/draft-pantos-http-live-streaming-17#section-4.3.2.2
             # 3. https://tools.ietf.org/html/draft-pantos-http-live-streaming-17#section-4.3.3.2
