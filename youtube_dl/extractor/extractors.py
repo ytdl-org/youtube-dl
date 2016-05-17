9c3c447eb389726d98189d972a2d772ef729132d
TRox1972 <archcr8@gmail.com>
2016-05-17 16:21:52 +0200

    [loc] Add extractor (Closes #3188)
    Added extractor of loc.gov, which closes #3188. I am not an experienced programmer, so I am sure I did a bunch of mistakes, but the extractor works (for me at least).
    
    [LibraryOfCongress] don't use video_id for _search_regex()
    
    [LibraryOfCongress] Improvements

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -382,6 +382,7 @@ from .leeco import (
     LePlaylistIE,
     LetvCloudIE,
 )
+from .libraryofcongress import LibraryOfCongressIE
 from .libsyn import LibsynIE
 from .lifenews import (
     LifeNewsIE,
