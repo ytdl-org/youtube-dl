6bc3e36dd82c1b247a43f9171de2e660df85974e
TRox1972 <TRox1972@users.noreply.github.com>
2016-07-03 01:06:55 +0200

    [Nintedo] Add playlist support

--- youtube_dl/extractor/nintendo.py
+++ youtube_dl/extractor/nintendo.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from .common import InfoExtractor
 from .ooyala import OoyalaIE
 
+import re
+
 
 class NintendoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?nintendo\.com/games/detail/(?P<id>[\w-]+)'
@@ -20,15 +22,26 @@ class NintendoIE(InfoExtractor):
         'add_ie': ['Ooyala'],
     }, {
         'url': 'http://www.nintendo.com/games/detail/tokyo-mirage-sessions-fe-wii-u',
-        'only_matching': True,
+        'info_dict': {
+            'id': 'tokyo-mirage-sessions-fe-wii-u',
+        },
+        'params': {
+            'skip_download': True,
+        },
+        'add_ie': ['Ooyala'],
+        'playlist_count': 4,
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        ooyala_code = self._search_regex(
+        ooyala_codes = re.findall(
             r'data-video-code=(["\'])(?P<code>.+?)\1',
-            webpage, 'ooyala code', group='code')
+            webpage)
 
-        return OoyalaIE._build_url_result(ooyala_code)
+        entries = []
+        for ooyala_code in ooyala_codes:
+            entries.append(OoyalaIE._build_url_result(ooyala_code[1]))
+
+        return self.playlist_result(entries, video_id, self._og_search_title(webpage))
