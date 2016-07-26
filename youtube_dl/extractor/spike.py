a3aa814b774a413d9e7f4fbfadf06fe6dcc59b25
Yen Chi Hsuan <yan12125@gmail.com>
2016-07-26 11:10:41 +0800

    Update _TESTS for MTV sites

--- youtube_dl/extractor/spike.py
+++ youtube_dl/extractor/spike.py
@@ -11,8 +11,10 @@ class SpikeIE(MTVServicesInfoExtractor):
         'info_dict': {
             'id': 'b9c8221a-4e50-479a-b86d-3333323e38ba',
             'ext': 'mp4',
-            'title': 'Auction Hunters|Can Allen Ride A Hundred Year-Old Motorcycle?',
+            'title': 'Auction Hunters|December 27, 2013|4|414|Can Allen Ride A Hundred Year-Old Motorcycle?',
             'description': 'md5:fbed7e82ed5fad493615b3094a9499cb',
+            'timestamp': 1388120400,
+            'upload_date': '20131227',
         },
     }, {
         'url': 'http://www.spike.com/video-clips/lhtu8m/',
