98bde0f6341dd4136fa12c12a243a3b5ad68bd8d
Petr Zvoníček <zvonicek@gmail.com>
2016-08-06 15:26:48 +0200

    [rbmaradio] Fixed extractor

--- youtube_dl/extractor/rbmaradio.py
+++ youtube_dl/extractor/rbmaradio.py
@@ -1,55 +1,46 @@
 # encoding: utf-8
 from __future__ import unicode_literals
 
-import json
-import re
-
 from .common import InfoExtractor
 from ..utils import (
-    ExtractorError,
+    clean_html
 )
 
 
 class RBMARadioIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?rbmaradio\.com/shows/(?P<videoID>[^/]+)$'
+    _VALID_URL = r'https?://(?:www\.)?rbmaradio\.com/shows/[^/]+/episodes/(?P<id>[^/]+)$'
     _TEST = {
-        'url': 'http://www.rbmaradio.com/shows/ford-lopatin-live-at-primavera-sound-2011',
+        'url': 'https://www.rbmaradio.com/shows/main-stage/episodes/ford-lopatin-live-at-primavera-sound-2011',
         'md5': '6bc6f9bcb18994b4c983bc3bf4384d95',
         'info_dict': {
             'id': 'ford-lopatin-live-at-primavera-sound-2011',
             'ext': 'mp3',
-            'uploader_id': 'ford-lopatin',
-            'location': 'Spain',
             'description': 'Joel Ford and Daniel ’Oneohtrix Point Never’ Lopatin fly their midified pop extravaganza to Spain. Live at Primavera Sound 2011.',
-            'uploader': 'Ford & Lopatin',
-            'title': 'Live at Primavera Sound 2011',
+            'title': 'Ford & Lopatin - Main Stage',
         },
     }
 
     def _real_extract(self, url):
-        m = re.match(self._VALID_URL, url)
-        video_id = m.group('videoID')
+        video_id = self._match_id(url)
 
         webpage = self._download_webpage(url, video_id)
+        json_data = self._search_regex(r'<script>window\.__INITIAL_STATE__\s*=\s*(.+?)</script>',
+                                       webpage, 'json data')
+        data = self._parse_json(json_data, video_id)
 
-        json_data = self._search_regex(r'window\.gon.*?gon\.show=(.+?);$',
-                                       webpage, 'json data', flags=re.MULTILINE)
+        item = None
+        for episode in data['episodes']:
+            items = data['episodes'][episode]
+            if video_id in items:
+                item = items[video_id]
 
-        try:
-            data = json.loads(json_data)
-        except ValueError as e:
-            raise ExtractorError('Invalid JSON: ' + str(e))
-
-        video_url = data['akamai_url'] + '&cbr=256'
+        video_url = item['audioURL'] + '?cbr=256'
 
         return {
             'id': video_id,
             'url': video_url,
-            'title': data['title'],
-            'description': data.get('teaser_text'),
-            'location': data.get('country_of_origin'),
-            'uploader': data.get('host', {}).get('name'),
-            'uploader_id': data.get('host', {}).get('slug'),
-            'thumbnail': data.get('image', {}).get('large_url_2x'),
-            'duration': data.get('duration'),
+            'title': item.get('title') + ' - ' + item.get('showTitle'),
+            'description': clean_html(item.get('longTeaser')),
+            'thumbnail': self._proto_relative_url(item.get('imageURL', {}).get('landscape')),
+            'duration': item.get('duration'),
         }
