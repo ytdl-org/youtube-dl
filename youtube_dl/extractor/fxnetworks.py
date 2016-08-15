cbef4d5c9ff5013d0c10b960e1690805724120cd
Remita Amine <remitamine@gmail.com>
2016-08-15 17:10:45 +0100

    [fxnetworks] add test and check geo restriction

--- youtube_dl/extractor/fxnetworks.py
+++ youtube_dl/extractor/fxnetworks.py
@@ -12,10 +12,27 @@ from ..utils import (
 
 class FXNetworksIE(AdobePass):
     _VALID_URL = r'https?://(?:www\.)?fxnetworks\.com/video/(?P<id>\d+)'
+    _TEST = {
+        'url': 'http://www.fxnetworks.com/video/719841347694',
+        'md5': '1447d4722e42ebca19e5232ab93abb22',
+        'info_dict': {
+            'id': '719841347694',
+            'ext': 'mp4',
+            'title': 'Vanpage',
+            'description': 'F*ck settling down. You\'re the Worst returns for an all new season August 31st on FXX.',
+            'age_limit': 14,
+            'uploader': 'NEWA-FNG-FX',
+            'upload_date': '20160706',
+            'timestamp': 1467844741,
+        },
+        'add_ie': ['ThePlatform'],
+    }
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
+        if 'The content you are trying to access is not available in your region.' in webpage:
+            self.raise_geo_restricted()
         video_data = extract_attributes(self._search_regex(
             r'(<a.+?rel="http://link\.theplatform\.com/s/.+?</a>)', webpage, 'video data'))
         player_type = self._search_regex(r'playerType\s*=\s*[\'"]([^\'"]+)', webpage, 'player type', fatal=False)
@@ -42,6 +59,7 @@ class FXNetworksIE(AdobePass):
         return {
             '_type': 'url_transparent',
             'id': video_id,
+            'title': title,
             'url': smuggle_url(update_url_query(release_url, query), {'force_smil_url': True}),
             'thumbnail': video_data.get('data-large-thumb'),
             'age_limit': parse_age_limit(rating),
