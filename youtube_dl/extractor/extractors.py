b3eaeded12f470afd6f0cb851e6b7dd2ee78b7c5
Remita Amine <remitamine@gmail.com>
2016-08-28 16:50:32 +0100

    [tbs] Add new extractor(#10222)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -824,6 +824,7 @@ from .tagesschau import (
     TagesschauIE,
 )
 from .tass import TassIE
+from .tbs import TBSIE
 from .tdslifeway import TDSLifewayIE
 from .teachertube import (
     TeacherTubeIE,
