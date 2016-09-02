349fc5c705d6b81ae53d698972f40b1125bee13e
Sergey M․ <dstftw@gmail.com>
2016-09-02 21:13:50 +0700

    [facebook:plugins:video] Add extractor (Closes #10530)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -264,7 +264,10 @@ from .everyonesmixtape import EveryonesMixtapeIE
 from .expotv import ExpoTVIE
 from .extremetube import ExtremeTubeIE
 from .eyedotv import EyedoTVIE
-from .facebook import FacebookIE
+from .facebook import (
+    FacebookIE,
+    FacebookPluginsVideoIE,
+)
 from .faz import FazIE
 from .fc2 import FC2IE
 from .fczenit import FczenitIE
