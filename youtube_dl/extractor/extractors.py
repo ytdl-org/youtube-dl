cb9cbd84ed2471140ce58480b8ed373f8abb5816
Remita Amine <remitamine@gmail.com>
2016-09-08 22:55:27 +0100

    [extractors] add import for TeleQuebecIE

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -861,6 +861,7 @@ from .telebruxelles import TeleBruxellesIE
 from .telecinco import TelecincoIE
 from .telegraaf import TelegraafIE
 from .telemb import TeleMBIE
+from .telequebec import TeleQuebecIE
 from .teletask import TeleTaskIE
 from .telewebion import TelewebionIE
 from .testurl import TestURLIE
