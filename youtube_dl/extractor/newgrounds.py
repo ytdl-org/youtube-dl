dc6a9c542f4a4ead2c5d612a3e55473d7fd6bb6e
stepshal <nessento@openmailbox.org>
2016-09-08 13:20:37 +0700

    [Newgrounds] Make test urls with https.

--- youtube_dl/extractor/newgrounds.py
+++ youtube_dl/extractor/newgrounds.py
@@ -9,7 +9,7 @@ from .common import InfoExtractor
 class NewgroundsIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?newgrounds\.com/(?:audio/listen|portal/view)/(?P<id>[0-9]+)'
     _TESTS = [{
-        'url': 'http://www.newgrounds.com/audio/listen/549479',
+        'url': 'https://www.newgrounds.com/audio/listen/549479',
         'md5': 'fe6033d297591288fa1c1f780386f07a',
         'info_dict': {
             'id': '549479',
@@ -18,7 +18,7 @@ class NewgroundsIE(InfoExtractor):
             'uploader': 'Burn7',
         }
     }, {
-        'url': 'http://www.newgrounds.com/portal/view/673111',
+        'url': 'https://www.newgrounds.com/portal/view/673111',
         'md5': '3394735822aab2478c31b1004fe5e5bc',
         'info_dict': {
             'id': '673111',
