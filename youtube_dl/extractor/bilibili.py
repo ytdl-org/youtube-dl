7ead562295994beaf72e25c0a42066faebba6929
Yen Chi Hsuan <yan12125@gmail.com>
2016-09-14 22:01:31 +0800

    [bilibili] Remove copyrighted test cases
    I can't find any English or Chinese material that claims BiliBili has
    bought legal redistribution permissions for copyrighted products from
    copyrighted holders.
    
    References for removed test cases:
    "刀语": https://en.wikipedia.org/wiki/Katanagatari, by White Fox
    "哆啦A梦": https://en.wikipedia.org/wiki/Doraemon, by Shin-Ei Animation
    "岳父岳母真难当": https://en.wikipedia.org/wiki/Serial_(Bad)_Weddings, by Les films du 24
    "混沌武士": https://en.wikipedia.org/wiki/Samurai_Champloo, by Manglobe
    
    I shouldn't have added them to _TESTS

--- youtube_dl/extractor/bilibili.py
+++ youtube_dl/extractor/bilibili.py
@@ -17,7 +17,7 @@ from ..utils import (
 class BiliBiliIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.|bangumi\.|)bilibili\.(?:tv|com)/(?:video/av|anime/v/)(?P<id>\d+)'
 
-    _TESTS = [{
+    _TEST = {
         'url': 'http://www.bilibili.tv/video/av1074402/',
         'md5': '9fa226fe2b8a9a4d5a69b4c6a183417e',
         'info_dict': {
@@ -32,64 +32,7 @@ class BiliBiliIE(InfoExtractor):
             'uploader': '菊子桑',
             'uploader_id': '156160',
         },
-    }, {
-        'url': 'http://www.bilibili.com/video/av1041170/',
-        'info_dict': {
-            'id': '1041170',
-            'ext': 'mp4',
-            'title': '【BD1080P】刀语【诸神&异域】',
-            'description': '这是个神奇的故事~每个人不留弹幕不给走哦~切利哦！~',
-            'duration': 3382.259,
-            'timestamp': 1396530060,
-            'upload_date': '20140403',
-            'thumbnail': 're:^https?://.+\.jpg',
-            'uploader': '枫叶逝去',
-            'uploader_id': '520116',
-        },
-    }, {
-        'url': 'http://www.bilibili.com/video/av4808130/',
-        'info_dict': {
-            'id': '4808130',
-            'ext': 'mp4',
-            'title': '【长篇】哆啦A梦443【钉铛】',
-            'description': '(2016.05.27)来组合客人的脸吧&amp;amp;寻母六千里锭 抱歉，又轮到周日上班现在才到家 封面www.pixiv.net/member_illust.php?mode=medium&amp;amp;illust_id=56912929',
-            'duration': 1493.995,
-            'timestamp': 1464564180,
-            'upload_date': '20160529',
-            'thumbnail': 're:^https?://.+\.jpg',
-            'uploader': '喜欢拉面',
-            'uploader_id': '151066',
-        },
-    }, {
-        # Missing upload time
-        'url': 'http://www.bilibili.com/video/av1867637/',
-        'info_dict': {
-            'id': '1867637',
-            'ext': 'mp4',
-            'title': '【HDTV】【喜剧】岳父岳母真难当 （2014）【法国票房冠军】',
-            'description': '一个信奉天主教的法国旧式传统资产阶级家庭中有四个女儿。三个女儿却分别找了阿拉伯、犹太、中国丈夫，老夫老妻唯独期盼剩下未嫁的小女儿能找一个信奉天主教的法国白人，结果没想到小女儿找了一位非裔黑人……【这次应该不会跳帧了】',
-            'duration': 5760.0,
-            'uploader': '黑夜为猫',
-            'uploader_id': '610729',
-            'thumbnail': 're:^https?://.+\.jpg',
-        },
-        'params': {
-            # Just to test metadata extraction
-            'skip_download': True,
-        },
-        'expected_warnings': ['upload time'],
-    }, {
-        'url': 'http://bangumi.bilibili.com/anime/v/40068',
-        'md5': '08d539a0884f3deb7b698fb13ba69696',
-        'info_dict': {
-            'id': '40068',
-            'ext': 'mp4',
-            'duration': 1402.357,
-            'title': '混沌武士 : 第7集 四面楚歌 A Risky Racket',
-            'description': 'md5:6a9622b911565794c11f25f81d6a97d2',
-            'thumbnail': 're:^http?://.+\.jpg',
-        },
-    }]
+    }
 
     _APP_KEY = '6f90a59ac58a4123'
     _BILIBILI_KEY = '0bfd84cc3940035173f35e6777508326'
