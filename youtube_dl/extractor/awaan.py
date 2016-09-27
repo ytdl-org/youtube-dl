93933c9819fa1282081a5f0761cbeabc9fbea336
Remita Amine <remitamine@gmail.com>
2016-09-27 15:28:37 +0100

    [awaan:video] fix test(closes #10773)

--- youtube_dl/extractor/awaan.py
+++ youtube_dl/extractor/awaan.py
@@ -66,6 +66,7 @@ class AWAANVideoIE(AWAANBaseIE):
             'duration': 2041,
             'timestamp': 1227504126,
             'upload_date': '20081124',
+            'uploader_id': '71',
         },
     }, {
         'url': 'http://awaan.ae/video/26723981/%D8%AF%D8%A7%D8%B1-%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85:-%D8%AE%D9%8A%D8%B1-%D8%AF%D9%88%D8%B1-%D8%A7%D9%84%D8%A3%D9%86%D8%B5%D8%A7%D8%B1',
