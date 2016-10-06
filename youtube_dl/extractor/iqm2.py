78210b31fa7e62defcd3489c0e456be1f6f23533
John Hawkinson <jhawk@mit.edu>
2016-10-06 01:51:06 -0400

    copyedit comments

--- youtube_dl/extractor/iqm2.py
+++ youtube_dl/extractor/iqm2.py
@@ -10,38 +10,38 @@ from .generic import GenericIE
 # IQM2 aka Accela is a municipal meeting management platform that
 # (among other things) stores livestreamed video from municipal
 # meetings.  After a hefty (several-hour) processing time, that video
-# is avaialble in easily downloadable form from their web portal, but
+# is available in easily downloadable form from their web portal, but
 # prior to that, the video can only be watched in realtime through
 # JWPlayer. This extractor is designed to download the realtime video
-# prior to download links being available. See:
+# prior to download links being available. For more info on Accela, see:
 #   http://www.iqm2.com/About/Accela.aspx
 #   http://www.accela.com/
 
-# This makes it challenging to produce a test case for, because the
-# extractor will want to follow the processed and easily downloadble
-# version. So there may be interesting bugs during the race condition
-# time before the processed video is available (which is really the
-# only time this extractor is especially important).
+# This processing makes it challenging to produce a test case for,
+# because the extractor will want to find the processed and easily
+# downloadable version. So there may be interesting bugs during the
+# race condition time before the processed video is available (which
+# is really the only time this extractor is especially important).
 
 # This is also a relatively braindead extractor. It parses a given page like
-#   http://cambridgema.iqm2.com/Citizens/SplitView.aspx?Mode=Video&MeetingID=1679#
+#   http://cambridgema.iqm2.com/Citizens/SplitView.aspx?Mode=Video&MeetingID=1679
 # to determine the location of an inner div defined by a URL of the form
 #   http://cambridgema.iqm2.com/Citizens/VideoScreen.aspx?MediaID=1563&Frame=SplitView
 
-# and then simply hands that URL to the generic extractor, which
-# matches it under the "Broaden the findall a little bit: JWPlayer JS
-# loader" (line 2372 as of 6 Oct 2016).
+# and then simply hands that URL to the GenericIE generic extractor,
+# which matches it under the "Broaden the findall a little bit:
+# JWPlayer JS loader" (line 2372 as of 6 Oct 2016).
 
-# It appears that the metadata associated with the video (like it's
+# It appears that the metadata associated with the video (like its
 # title) does not appear anywhere in the 2 HTML pages that get
 # downloaded through this extractor. So it would need to download
-# additional HTTP resources in order to get appropriate metadata.
+# additional HTTP resources in order to get "real" metadata.
 
 # This also appears to be the only example to date of an extractor
-# that calls-out to the GenericIE generic extractor, so it may be
+# that calls-out to the generic extractor, so it may be
 # useful as an example. Or perhaps it means that there's a better way
 # to do this and it should be rewritten differently, esp. to not
-# leverage the generic.
+# leverage the generic? (xxx)
 
 # Contributed by John Hawkinson <jhawk@mit.edu>, 6 Oct 2016.
 
@@ -56,10 +56,10 @@ class IQM2IE(InfoExtractor):
     # for the path portion of a URL to be case-sensitive, but the hostname
     # ought not to be. And it seems like strict adherence might mess up a
     # bunch of extractors in funny-cased URLs? Redefine suitable() to search
-    # case-insentitively. Note this also changes the re.match() call at the
+    # case-insensitively. Note this also changes the re.match() call at the
     # start of _real_extract()
     #
-    # In this case, we commonly see both iqm2.com and IQM2.com
+    # In this case, we commonly see both iqm2.com and IQM2.com.
     
     @classmethod
     def suitable(cls, url):
