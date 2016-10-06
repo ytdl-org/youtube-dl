33898fb19c1af161c503ebce8f9a4774fecee45e
Remita Amine <remitamine@gmail.com>
2016-10-06 10:45:57 +0100

    [nzz] Add new extractor(#4407)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -638,6 +638,7 @@ from .nytimes import (
     NYTimesArticleIE,
 )
 from .nuvid import NuvidIE
+from .nzz import NZZIE
 from .odatv import OdaTVIE
 from .odnoklassniki import OdnoklassnikiIE
 from .oktoberfesttv import OktoberfestTVIE
