8d8acd193efdb1441276bcf35fed2231c3dc1c60
John Hawkinson <jhawk@mit.edu>
2016-10-09 08:00:15 -0400

    Use _match_id() instead of re.match()
    Oops, when I created this extractor I copied the sample code from the
    2014 manpage on my system, thus missing 4bc77c8417ca0340d09dcebb311d06aa7d5ba0ac's
    introduction of the _match_id() helper function.

--- youtube_dl/extractor/iqm2.py
+++ youtube_dl/extractor/iqm2.py
@@ -57,9 +57,7 @@ class IQM2IE(InfoExtractor):
         }]
 
     def _real_extract(self, url):
-        mobj = re.match(self._VALID_URL, url)
-        video_id = mobj.group('id')
-
+        video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
         # print "Original URL is", url
