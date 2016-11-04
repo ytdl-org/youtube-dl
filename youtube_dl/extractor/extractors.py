f3c705f8ec3505240bdfbe622693c3cd2ce10857
Sergey M․ <dstftw@gmail.com>
2016-11-04 21:32:30 +0700

    [fox9] Add extractor (closes #11110)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -296,6 +296,7 @@ from .footyroom import FootyRoomIE
 from .formula1 import Formula1IE
 from .fourtube import FourTubeIE
 from .fox import FOXIE
+from .fox9 import FOX9IE
 from .foxgay import FoxgayIE
 from .foxnews import (
     FoxNewsIE,
