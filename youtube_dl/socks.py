83442966194640d9bc00e7f3086aa5e8b25c4ae3
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-03 21:53:41 +0800

    [socks] Fix error reporting (#11355)

--- youtube_dl/socks.py
+++ youtube_dl/socks.py
@@ -60,7 +60,7 @@ class ProxyError(IOError):
 
     def __init__(self, code=None, msg=None):
         if code is not None and msg is None:
-            msg = self.CODES.get(code) and 'unknown error'
+            msg = self.CODES.get(code) or 'unknown error'
         super(ProxyError, self).__init__(code, msg)
 
 
