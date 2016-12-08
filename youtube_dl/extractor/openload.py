6c20a0bb99e626db6870747b6329ad9c9064c123
Yen Chi Hsuan <yan12125@gmail.com>
2016-12-09 02:15:16 +0800

    [openload] Fix extraction (closes #10408)

--- youtube_dl/extractor/openload.py
+++ youtube_dl/extractor/openload.py
@@ -1,20 +1,11 @@
 # coding: utf-8
-from __future__ import unicode_literals, division
-
-import re
+from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..compat import (
-    compat_chr,
-    compat_ord,
-)
+from ..compat import compat_chr
 from ..utils import (
     determine_ext,
     ExtractorError,
-)
-from ..jsinterp import (
-    JSInterpreter,
-    _NAME_RE
 )
 
 
@@ -62,44 +53,6 @@ class OpenloadIE(InfoExtractor):
         'only_matching': True,
     }]
 
-    def openload_decode(self, txt):
-        symbol_dict = {
-            '(ﾟДﾟ) [ﾟΘﾟ]': '_',
-            '(ﾟДﾟ) [ﾟωﾟﾉ]': 'a',
-            '(ﾟДﾟ) [ﾟΘﾟﾉ]': 'b',
-            '(ﾟДﾟ) [\'c\']': 'c',
-            '(ﾟДﾟ) [ﾟｰﾟﾉ]': 'd',
-            '(ﾟДﾟ) [ﾟДﾟﾉ]': 'e',
-            '(ﾟДﾟ) [1]': 'f',
-            '(ﾟДﾟ) [\'o\']': 'o',
-            '(oﾟｰﾟo)': 'u',
-            '(ﾟДﾟ) [\'c\']': 'c',
-            '((ﾟｰﾟ) + (o^_^o))': '7',
-            '((o^_^o) +(o^_^o) +(c^_^o))': '6',
-            '((ﾟｰﾟ) + (ﾟΘﾟ))': '5',
-            '(-~3)': '4',
-            '(-~-~1)': '3',
-            '(-~1)': '2',
-            '(-~0)': '1',
-            '((c^_^o)-(c^_^o))': '0',
-        }
-        delim = '(ﾟДﾟ)[ﾟεﾟ]+'
-        end_token = '(ﾟДﾟ)[ﾟoﾟ]'
-        symbols = '|'.join(map(re.escape, symbol_dict.keys()))
-        txt = re.sub('(%s)\+\s?' % symbols, lambda m: symbol_dict[m.group(1)], txt)
-        ret = ''
-        for aacode in re.findall(r'{0}\+\s?{1}(.*?){0}'.format(re.escape(end_token), re.escape(delim)), txt):
-            for aachar in aacode.split(delim):
-                if aachar.isdigit():
-                    ret += compat_chr(int(aachar, 8))
-                else:
-                    m = re.match(r'^u([\da-f]{4})$', aachar)
-                    if m:
-                        ret += compat_chr(int(m.group(1), 16))
-                    else:
-                        self.report_warning("Cannot decode: %s" % aachar)
-        return ret
-
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage('https://openload.co/embed/%s/' % video_id, video_id)
@@ -107,36 +60,20 @@ class OpenloadIE(InfoExtractor):
         if 'File not found' in webpage or 'deleted by the owner' in webpage:
             raise ExtractorError('File not found', expected=True)
 
-        # The following decryption algorithm is written by @yokrysty and
-        # declared to be freely used in youtube-dl
-        # See https://github.com/rg3/youtube-dl/issues/10408
-        enc_data = self._html_search_regex(
-            r'<span[^>]*>([^<]+)</span>\s*<span[^>]*>[^<]+</span>\s*<span[^>]+id="streamurl"',
-            webpage, 'encrypted data')
+        ol_id = self._search_regex(
+            '<span[^>]+id="[a-zA-Z0-9]+x"[^>]*>([0-9]+)</span>',
+            webpage, 'openload ID')
 
-        enc_code = self._html_search_regex(r'<script[^>]+>(ﾟωﾟ[^<]+)</script>',
-                                           webpage, 'encrypted code')
+        first_two_chars = int(float(ol_id[0:][:2]))
+        urlcode = ''
+        num = 2
 
-        js_code = self.openload_decode(enc_code)
-        jsi = JSInterpreter(js_code)
+        while num < len(ol_id):
+            urlcode += compat_chr(int(float(ol_id[num:][:3])) -
+                                  first_two_chars * int(float(ol_id[num + 3:][:2])))
+            num += 5
 
-        m_offset_fun = self._search_regex(r'slice\(0\s*-\s*(%s)\(\)' % _NAME_RE, js_code, 'javascript offset function')
-        m_diff_fun = self._search_regex(r'charCodeAt\(0\)\s*\+\s*(%s)\(\)' % _NAME_RE, js_code, 'javascript diff function')
-
-        offset = jsi.call_function(m_offset_fun)
-        diff = jsi.call_function(m_diff_fun)
-
-        video_url_chars = []
-
-        for idx, c in enumerate(enc_data):
-            j = compat_ord(c)
-            if j >= 33 and j <= 126:
-                j = ((j + 14) % 94) + 33
-            if idx == len(enc_data) - offset:
-                j += diff
-            video_url_chars += compat_chr(j)
-
-        video_url = 'https://openload.co/stream/%s?mime=true' % ''.join(video_url_chars)
+        video_url = 'https://openload.co/stream/' + urlcode
 
         title = self._og_search_title(webpage, default=None) or self._search_regex(
             r'<span[^>]+class=["\']title["\'][^>]*>([^<]+)', webpage,
@@ -155,5 +92,4 @@ class OpenloadIE(InfoExtractor):
             'ext': determine_ext(title),
             'subtitles': subtitles,
         }
-
         return info_dict
