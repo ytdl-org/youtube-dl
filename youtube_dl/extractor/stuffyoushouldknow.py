b1317fb7590fa96dd7bd249f9a292b1eb20dffc9
galdwulf <ragnarock@gmx.co.uk>
2016-12-15 11:14:39 +0000

    fixing stuffyoushouldknow extractor test case

--- youtube_dl/extractor/stuffyoushouldknow.py
+++ youtube_dl/extractor/stuffyoushouldknow.py
@@ -9,13 +9,12 @@ class StuffyoushouldknowIE(InfoExtractor):
     _VALID_URL = r'https?://?(www).stuffyoushouldknow.com/podcasts/(?P<id>[[a-zA-Z0-9_-]+)'
     _TEST = {
         'url': 'http://www.stuffyoushouldknow.com/podcasts/banned-kids-advertising.htm',
-        'md5': '12cfeb58e11776addb58ce37c12711b7',
+        'md5': 'e128341f40a8be82ac8f55cb0e402d7d',
         'info_dict': {
+            'id':'banned-kids-advertising',
+            'ext':'mp3',
             'title': 'Should Advertising to Kids Be Banned?',
-            'url': 'http://www.stuffyoushouldknow.com/podcasts/banned-kids-advertising.htm',
-            'site_name': 'Stuff You Should Know',
             'description': 'As kids’ buying power in America has exploded in recent decades, so too has the amount companies spend advertising to them. But because of a quirk of brain development, kids aren’t equipped to understand ads are manipulating them. Should they be banned?',
-            'content': 'http://s.hswstatic.com/gif/banned-kids-advertising-sysk.jpg',
         },
 
     }
@@ -27,14 +26,15 @@ class StuffyoushouldknowIE(InfoExtractor):
 
         video_url = re.search(r'https?://www.podtrac.com/pts/redirect.mp3/podcasts.howstuffworks.com/hsw/podcasts'
                               r'/sysk/[0-9a-zA-Z_-]*.mp3', webpage)
-        title = self._og_search_title(webpage)
         site_name= self._og_search_title(webpage)
         description = self._og_search_description(webpage)
 
         return {
             'id': video_id,
-            'title': title,
+            'title': site_name,
             'description': description,
-            'site name': site_name,
-            'url': video_url.group(0)
+            'url': video_url.group(0),
+            'ext': 'mp3',
+            'site_name': site_name,
+
         }
