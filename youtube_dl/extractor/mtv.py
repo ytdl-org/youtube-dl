cdd11c054013997b6f0053a3958b6e5b0aa698b6
Sergey M․ <dstftw@gmail.com>
2017-01-10 22:31:20 +0700

    [mtv] Use native hls by default

--- youtube_dl/extractor/mtv.py
+++ youtube_dl/extractor/mtv.py
@@ -90,7 +90,8 @@ class MTVServicesInfoExtractor(InfoExtractor):
         for rendition in mdoc.findall('.//rendition'):
             if rendition.get('method') == 'hls':
                 hls_url = rendition.find('./src').text
-                formats.extend(self._extract_m3u8_formats(hls_url, video_id, ext='mp4'))
+                formats.extend(self._extract_m3u8_formats(
+                    hls_url, video_id, ext='mp4', entry_protocol='m3u8_native'))
             else:
                 # fms
                 try:
