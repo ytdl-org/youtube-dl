290d3387e7ec3a699040a6b30da957ca5e5a39fb
Sergey M․ <dstftw@gmail.com>
2017-01-26 23:23:08 +0700

    [utils] Improve parse_duration

--- test/test_utils.py
+++ test/test_utils.py
@@ -510,6 +510,7 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(parse_duration('1 hour 3 minutes'), 3780)
         self.assertEqual(parse_duration('87 Min.'), 5220)
         self.assertEqual(parse_duration('PT1H0.040S'), 3600.04)
+        self.assertEqual(parse_duration('PT00H03M30SZ'), 210)
 
     def test_fix_xml_ampersands(self):
         self.assertEqual(
