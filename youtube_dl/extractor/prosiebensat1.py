9964e6b39044787fb6546dd85ff19cb1451549fc
Thomas Christlieb <thomaschristlieb@hotmail.com>
2017-02-01 16:00:41 +0100

    Added new Regex for prosiebensat1 Extractor Description. Fixes #11810 (#11929)
    * Added new Regex for prosiebensat1 Extractor Description. Fixes #11810
    
    * Using _og_search_description() as a Fallback for Description-Regex
    
    * Using _og_search_description() as a Fallback for Description-Regex - Second try
    
    * Also added fallback regex
    
    * Using _og_search_description() as a Fallback for Description-Regex - Third try
    
    * removed fatal=False from search for description regex. default=None should be preferred only
    
    * Using fatal=false for _og_search_description
    
    * Revert "Using fatal=false for _og_search_description"
    
    This reverts commit 2b7e123f9d0f2bd6ada54fa8e4e6035fece5dbf4.
    
    * Deleted default=None Parameter for _og_search_property

--- youtube_dl/extractor/prosiebensat1.py
+++ youtube_dl/extractor/prosiebensat1.py
@@ -375,7 +375,9 @@ class ProSiebenSat1IE(ProSiebenSat1BaseIE):
         title = self._html_search_regex(self._TITLE_REGEXES, webpage, 'title')
         info = self._extract_video_info(url, clip_id)
         description = self._html_search_regex(
-            self._DESCRIPTION_REGEXES, webpage, 'description', fatal=False)
+            self._DESCRIPTION_REGEXES, webpage, 'description', default=None)
+        if description is None: 
+            description = self._og_search_description(webpage)
         thumbnail = self._og_search_thumbnail(webpage)
         upload_date = unified_strdate(self._html_search_regex(
             self._UPLOAD_DATE_REGEXES, webpage, 'upload date', default=None))
