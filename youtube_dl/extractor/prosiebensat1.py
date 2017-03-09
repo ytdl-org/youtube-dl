2913821723c826a0d2bfc16427592bf2b9d6d31c
Thomas Christlieb <thomaschristlieb@hotmail.com>
2017-03-09 17:18:37 +0100

    [prosiebensat1] Improve title extraction (closes #12318)

--- youtube_dl/extractor/prosiebensat1.py
+++ youtube_dl/extractor/prosiebensat1.py
@@ -369,7 +369,9 @@ class ProSiebenSat1IE(ProSiebenSat1BaseIE):
     def _extract_clip(self, url, webpage):
         clip_id = self._html_search_regex(
             self._CLIPID_REGEXES, webpage, 'clip id')
-        title = self._html_search_regex(self._TITLE_REGEXES, webpage, 'title')
+        title = self._html_search_regex(self._TITLE_REGEXES, webpage, 'title', default=None)
+        if title is None:
+            title = self._og_search_title(webpage)
         info = self._extract_video_info(url, clip_id)
         description = self._html_search_regex(
             self._DESCRIPTION_REGEXES, webpage, 'description', default=None)
