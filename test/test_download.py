320775e606fd6ec7839e89f8a556811a40a727a0
John Hawkinson <jhawk@mit.edu>
2017-04-08 00:21:11 -0400

    [test_download] Improve diagnostic on wrong 'id'

--- test/test_download.py
+++ test/test_download.py
@@ -11,6 +11,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from test.helper import (
     assertGreaterEqual,
     expect_warnings,
+    expect_value,
     get_params,
     gettestcases,
     expect_info_dict,
@@ -200,6 +201,7 @@ def generator(test_case, tname):
                     test_case['playlist_duration_sum'], got_duration)
 
             for tc in test_cases:
+                expect_value(self, res_dict['id'], tc['info_dict']['id'], 'id')
                 tc_filename = get_tc_filename(tc)
                 if not test_case.get('params', {}).get('skip_download', False):
                     self.assertTrue(os.path.exists(tc_filename), msg='Missing file ' + tc_filename)
