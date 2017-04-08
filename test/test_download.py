80b2fdf9ac99ebcbd6eec87670aa6d646da7dac4
Sergey M․ <dstftw@gmail.com>
2017-04-08 14:10:12 +0700

    [test_download] Match info dicts against tests before matching info file

--- test/test_download.py
+++ test/test_download.py
@@ -200,8 +200,9 @@ def generator(test_case, tname):
                 self.assertEqual(
                     test_case['playlist_duration_sum'], got_duration)
 
-            for tc in test_cases:
-                expect_value(self, res_dict['id'], tc['info_dict']['id'], 'id')
+            for tc_num, tc in enumerate(test_cases):
+                tc_res_dict = res_dict['entries'][tc_num] if is_playlist else res_dict
+                expect_info_dict(self, tc_res_dict, tc.get('info_dict', {}))
                 tc_filename = get_tc_filename(tc)
                 if not test_case.get('params', {}).get('skip_download', False):
                     self.assertTrue(os.path.exists(tc_filename), msg='Missing file ' + tc_filename)
