4d75f2cb31bd4c92ebf0dadb190e60ae01df5f32
Zhang Weijie <email@zhangweijie.cn>
2017-05-11 16:37:04 +0800

    [meipai] decode url

--- youtube_dl/extractor/meipai.py
+++ youtube_dl/extractor/meipai.py
@@ -7,7 +7,35 @@ from ..utils import (
     parse_duration,
     unified_timestamp,
 )
+import base64
 
+def decode(encoded_string):
+    def getHex(param1):
+        return {
+            'str': param1[4:],
+            'hex': ''.join(list(param1[:4])[::-1]),
+        }
+
+    def getDec(param1):
+        loc2 = str(int(param1, 16))
+        return {
+            'pre': list(loc2[:2]),
+            'tail': list(loc2[2:]),
+        }
+
+    def substr(param1, param2):
+        loc3 = param1[0: int(param2[0])]
+        loc4 = param1[int(param2[0]): int(param2[0]) + int(param2[1])]
+        return loc3 + param1[int(param2[0]):].replace(loc4, "")
+
+    def getPos(param1, param2):
+        param2[0] = len(param1) - int(param2[0]) - int(param2[1])
+        return param2
+
+    dict2 = getHex(encoded_string)
+    dict3 = getDec(dict2['hex'])
+    str4 = substr(dict2['str'], dict3['pre'])
+    return base64.b64decode(substr(str4, getPos(str4, dict3['tail'])))
 
 class MeipaiIE(InfoExtractor):
     IE_DESC = '美拍'
@@ -73,7 +101,7 @@ class MeipaiIE(InfoExtractor):
                 group='url', default=None)
             if video_url:
                 formats.append({
-                    'url': video_url,
+                    'url': decode(video_url),
                     'format_id': 'http',
                 })
 
