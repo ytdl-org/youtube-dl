3ca5c363ff290fe686206702216eb1bcac28d7ac
d2au <d2au.dev@gmail.com>
2017-06-25 14:01:43 +0930

    [abc.net.au:iview] Support hq streams when available

--- youtube_dl/extractor/abc.py
+++ youtube_dl/extractor/abc.py
@@ -124,7 +124,12 @@ class ABCIViewIE(InfoExtractor):
         title = video_params.get('title') or video_params['seriesTitle']
         stream = next(s for s in video_params['playlist'] if s.get('type') == 'program')
 
-        formats = self._extract_akamai_formats(stream['hds-unmetered'], video_id)
+        if 'sd' in stream["stream-labels"]:
+            hds = stream['streams']['hds']['sd'].replace('metered', 'um')
+        else:
+            hds = stream['hds-unmetered']
+
+        formats = self._extract_akamai_formats(hds, video_id)
         self._sort_formats(formats)
 
         subtitles = {}
