99a7e76240f3e79d770bff0029dc3321e14b2a97
Parmjit Virk <pvirk@mts.net>
2017-07-01 21:11:58 -0500

    [thisoldhouse] Update test

--- youtube_dl/extractor/thisoldhouse.py
+++ youtube_dl/extractor/thisoldhouse.py
@@ -8,7 +8,7 @@ class ThisOldHouseIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?thisoldhouse\.com/(?:watch|how-to|tv-episode)/(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://www.thisoldhouse.com/how-to/how-to-build-storage-bench',
-        'md5': '946f05bbaa12a33f9ae35580d2dfcfe3',
+        'md5': '568acf9ca25a639f0c4ff905826b662f',
         'info_dict': {
             'id': '2REGtUDQ',
             'ext': 'mp4',
