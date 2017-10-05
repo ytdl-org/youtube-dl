07c8dbe2354efea1666af5e60dc5491ccc8a871e
Khang Nguyen <khang.neon.1997@gmail.com>
2017-10-05 21:37:18 +0700

    Remove YoutubeSharedVideoIE  https://github.com/rg3/youtube-dl/issues/14303

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1345,7 +1345,6 @@ from .youtube import (
     YoutubeSearchDateIE,
     YoutubeSearchIE,
     YoutubeSearchURLIE,
-    YoutubeSharedVideoIE,
     YoutubeShowIE,
     YoutubeSubscriptionsIE,
     YoutubeTruncatedIDIE,
