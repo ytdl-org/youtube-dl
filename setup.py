e672f9519b3f6329f1e82a9752c83b29932f80f4
Yen Chi Hsuan <yan12125@gmail.com>
2017-10-16 23:52:29 +0800

    Dropping support for Python 3.2 and 3.3

--- setup.py
+++ setup.py
@@ -127,8 +127,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
