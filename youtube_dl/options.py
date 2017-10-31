3cd4ff0d4a5f0ba91e74a1841b563624b6717077
Seth Prime <sprime@getchalk.com>
2017-10-31 08:37:19 -0300

    Update --help documentation for SOCKS proxy to include authentication syntax

--- youtube_dl/options.py
+++ youtube_dl/options.py
@@ -204,9 +204,11 @@ def parseOpts(overrideArguments=None):
         '--proxy', dest='proxy',
         default=None, metavar='URL',
         help='Use the specified HTTP/HTTPS/SOCKS proxy. To enable experimental '
-             'SOCKS proxy, specify a proper scheme. For example '
-             'socks5://127.0.0.1:1080/. Pass in an empty string (--proxy "") '
-             'for direct connection')
+             'SOCKS proxy, specify a proper scheme. a proper scheme. '
+             'For example socks5://hostname:port/ or '
+             'socks5://username:password@hostname:port/ for username and '
+             'password authentication. Pass in an empty string (--proxy "") for '
+             'direct connection.')
     network.add_option(
         '--socket-timeout',
         dest='socket_timeout', type=float, default=None, metavar='SECONDS',
