f8d9f1cd07364c759426f01878a57f243ccdb60d
Gabor Miklos <miklosgabor86@gmail.com>
2017-11-20 13:27:52 +0200

    [slutload] Support mobile site

--- youtube_dl/extractor/slutload.py
+++ youtube_dl/extractor/slutload.py
@@ -1,25 +1,42 @@
 from __future__ import unicode_literals
 
+import re
+
 from .common import InfoExtractor
 
 
 class SlutloadIE(InfoExtractor):
     _VALID_URL = r'^https?://(?:\w+\.)?slutload\.com/video/[^/]+/(?P<id>[^/]+)/?$'
-    _TEST = {
+    _TESTS = [{
         'url': 'http://www.slutload.com/video/virginie-baisee-en-cam/TD73btpBqSxc/',
         'md5': '868309628ba00fd488cf516a113fd717',
         'info_dict': {
             'id': 'TD73btpBqSxc',
             'ext': 'mp4',
-            'title': 'virginie baisee en cam',
             'age_limit': 18,
+            'title': 'virginie baisee en cam',
             'thumbnail': r're:https?://.*?\.jpg'
         }
-    }
+    }, {
+        # mobile site
+        'url': 'http://mobile.slutload.com/video/masturbation-solo/fviFLmc6kzJ/',
+        'info_dict': {
+            'id': 'fviFLmc6kzJ',
+            'ext': 'mp4',
+            'age_limit': 18,
+            'title': 'Masturbation Solo',
+            'thumbnail': r're:https?://.*?\.jpg'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
+
+        desktop_url = re.sub(r'^(https?://)mobile\.', r'\1', url)
+        webpage = self._download_webpage(desktop_url, video_id)
 
         video_title = self._html_search_regex(r'<h1><strong>([^<]+)</strong>',
                                               webpage, 'title').strip()
