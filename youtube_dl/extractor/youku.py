0981585befc7af01331b72304ed11b9865b40401
zcanfly <zcanfly@126.com>
2017-12-02 19:16:22 +0800

    [youku] Update ccode (closes #14872)

--- youtube_dl/extractor/youku.py
+++ youtube_dl/extractor/youku.py
@@ -154,7 +154,7 @@ class YoukuIE(InfoExtractor):
         # request basic data
         basic_data_params = {
             'vid': video_id,
-            'ccode': '0502',
+            'ccode': '0501',
             'client_ip': '192.168.1.1',
             'utid': cna,
             'client_ts': time.time() / 1000,
