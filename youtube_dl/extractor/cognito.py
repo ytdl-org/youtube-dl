efc2dbd3a5299512b7cb5511803bd035fc6bb74d
wernerkarlheisenberg <bartsmeets@MacBook-van-Bart.local>
2018-01-24 21:10:50 +0100

    soft import warrant and boto3
    Hard import broke build, now using soft import.

--- youtube_dl/extractor/cognito.py
+++ youtube_dl/extractor/cognito.py
@@ -1,12 +1,18 @@
 from .common import InfoExtractor
-
-import boto3
-from warrant import Cognito
-from warrant.aws_srp import AWSSRP
+from ..utils import ExtractorError
 
 class CognitoBaseIE(InfoExtractor):
 
+
     def _cognito_login(self, auth_data):
+
+        try:
+            import boto3
+            from warrant import Cognito
+            from warrant.aws_srp import AWSSRP
+        except ImportError:
+            raise ExtractorError('%s depends on boto3 and warrant.' % self.IE_NAME)
+
         region = auth_data['PoolId'].split('_')[0]
         client = boto3.client(
             'cognito-idp',
