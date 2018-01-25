fa278f94267ba14ece5f925fcfdb2100f4431e20
Ozzy Walsh <novigradian@users.noreply.github.com>
2018-01-25 08:56:54 +0000

    Change Spike extractor tests to Paramount Network
    "Spike TV" has become "Paramount Network" and the domain has changed,
    breaking it's extractor. This commit changes the tests to use the new
    domain in preparation for fixing the extractor.

--- youtube_dl/extractor/spike.py
+++ youtube_dl/extractor/spike.py
@@ -8,37 +8,24 @@ from .mtv import MTVServicesInfoExtractor
 class SpikeIE(MTVServicesInfoExtractor):
     _VALID_URL = r'https?://(?:[^/]+\.)?spike\.com/[^/]+/[\da-z]{6}(?:[/?#&]|$)'
     _TESTS = [{
-        'url': 'http://www.spike.com/video-clips/lhtu8m/auction-hunters-can-allen-ride-a-hundred-year-old-motorcycle',
-        'md5': '1a9265f32b0c375793d6c4ce45255256',
+        'url': 'http://www.paramountnetwork.com/video-clips/e1ktem/nobodies-gangbanged',
+        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
         'info_dict': {
-            'id': 'b9c8221a-4e50-479a-b86d-3333323e38ba',
+            'id': 'e1ktem',
             'ext': 'mp4',
-            'title': 'Auction Hunters|December 27, 2013|4|414|Can Allen Ride A Hundred Year-Old Motorcycle?',
-            'description': 'md5:fbed7e82ed5fad493615b3094a9499cb',
-            'timestamp': 1388120400,
-            'upload_date': '20131227',
+            'title': 'Gangbanged - NOBODIES | Paramount Network',
+            'description': 'TODO: Add description checksum',
+            'upload_date': 'TODO: Add upload date',
         },
     }, {
-        'url': 'http://www.spike.com/full-episodes/j830qm/lip-sync-battle-joel-mchale-vs-jim-rash-season-2-ep-209',
-        'md5': 'b25c6f16418aefb9ad5a6cae2559321f',
+        'url': 'http://www.paramountnetwork.com/episodes/j830qm/lip-sync-battle-joel-mchale-vs-jim-rash-season-2-ep-13',
+        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
         'info_dict': {
-            'id': '37ace3a8-1df6-48be-85b8-38df8229e241',
+            'id': 'j830qm',
             'ext': 'mp4',
-            'title': 'Lip Sync Battle|April 28, 2016|2|209|Joel McHale Vs. Jim Rash|Act 1',
-            'description': 'md5:a739ca8f978a7802f67f8016d27ce114',
+            'title': 'Lip Sync Battle - Season 2, Ep. 13 - Joel McHale Vs. Jim Rash - Full Episode | Paramount Network',
+            'description': 'TODO: Add description checksum',
         },
-    }, {
-        'url': 'http://www.spike.com/video-clips/lhtu8m/',
-        'only_matching': True,
-    }, {
-        'url': 'http://www.spike.com/video-clips/lhtu8m',
-        'only_matching': True,
-    }, {
-        'url': 'http://bellator.spike.com/fight/atwr7k/bellator-158-michael-page-vs-evangelista-cyborg',
-        'only_matching': True,
-    }, {
-        'url': 'http://bellator.spike.com/video-clips/bw6k7n/bellator-158-foundations-michael-venom-page',
-        'only_matching': True,
     }]
 
     _FEED_URL = 'http://www.spike.com/feeds/mrss/'
