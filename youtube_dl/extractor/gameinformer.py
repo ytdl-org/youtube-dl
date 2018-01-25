559e40be2d948d45fdc8dc0f961b6ebb00ea6d67
Peter Pitzulo <pongopeter826@gmail.com>
2018-01-24 23:06:33 -0500

    [GameInformer] Fix brightcove_id regex.

--- youtube_dl/extractor/gameinformer.py
+++ youtube_dl/extractor/gameinformer.py
@@ -24,5 +24,5 @@ class GameInformerIE(InfoExtractor):
     def _real_extract(self, url):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
-        brightcove_id = self._search_regex(r"getVideo\('[^']+video_id=(\d+)", webpage, 'brightcove id')
+        brightcove_id = self._search_regex(r"<p><div id=\"bc_(\d+)", webpage, 'brightcove id')
         return self.url_result(self.BRIGHTCOVE_URL_TEMPLATE % brightcove_id, 'BrightcoveNew', brightcove_id)
