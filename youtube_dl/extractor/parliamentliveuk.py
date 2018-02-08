bfe4a2ef8e02f3577aaa9328c09187bdfead9c7e
Benedikt Gollatz <ben@differentialschokolade.org>
2018-02-08 19:52:44 +0100

    [parliamentliveuk] Fix kaltura widget config detection

--- youtube_dl/extractor/parliamentliveuk.py
+++ youtube_dl/extractor/parliamentliveuk.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from .common import InfoExtractor
 
+from re import DOTALL
 
 class ParliamentLiveUKIE(InfoExtractor):
     IE_NAME = 'parliamentlive.tv'
@@ -29,7 +30,7 @@ class ParliamentLiveUKIE(InfoExtractor):
             'http://vodplayer.parliamentlive.tv/?mid=' + video_id, video_id)
         widget_config = self._parse_json(self._search_regex(
             r'(?s)kWidgetConfig\s*=\s*({.+});',
-            webpage, 'kaltura widget config'), video_id)
+            webpage, 'kaltura widget config', flags=DOTALL), video_id)
         kaltura_url = 'kaltura:%s:%s' % (
             widget_config['wid'][1:], widget_config['entry_id'])
         event_title = self._download_json(
