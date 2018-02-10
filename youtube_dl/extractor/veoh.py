72a4bf0c5c6623089d9e129be95599cda66f0319
Jan Schär <jscissr@gmail.com>
2018-02-11 00:18:11 +0100

    [veoh] test only_matching

--- youtube_dl/extractor/veoh.py
+++ youtube_dl/extractor/veoh.py
@@ -26,14 +26,7 @@ class VeohIE(InfoExtractor):
         },
     }, {
         'url': 'http://www.veoh.com/embed/v56314296nk7Zdmz3',
-        'md5': '620e68e6a3cff80086df3348426c9ca3',
-        'info_dict': {
-            'id': '56314296',
-            'ext': 'mp4',
-            'title': 'Straight Backs Are Stronger',
-            'uploader': 'LUMOback',
-            'description': 'At LUMOback, we believe straight backs are stronger.  The LUMOback Posture & Movement Sensor:  It gently vibrates when you slouch, inspiring improved posture and mobility.  Use the app to track your data and improve your posture over time. ',
-        },
+        'only_matching': True,
     }, {
         'url': 'http://www.veoh.com/watch/v27701988pbTc4wzN?h1=Chile+workers+cover+up+to+avoid+skin+damage',
         'md5': '4a6ff84b87d536a6a71e6aa6c0ad07fa',
