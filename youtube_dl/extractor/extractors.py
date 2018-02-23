f2908d072edb898bc3e9b181729e7d0b9be950fd
Sergey M․ <dstftw@gmail.com>
2018-02-24 04:52:55 +0700

    [mailru:music] Add extractor (closes #15618)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -566,7 +566,11 @@ from .lynda import (
 )
 from .m6 import M6IE
 from .macgamestore import MacGameStoreIE
-from .mailru import MailRuIE
+from .mailru import (
+    MailRuIE,
+    MailRuMusicIE,
+    MailRuMusicSearchIE,
+)
 from .makerschannel import MakersChannelIE
 from .makertv import MakerTVIE
 from .mangomolo import (
