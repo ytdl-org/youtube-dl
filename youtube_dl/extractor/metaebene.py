2475076b2a665f241fe41ec4514c7313d6c73006
Kay B <>
2018-03-15 00:37:39 +0100

    [metaebene] add some rare podcasts which I forgot

--- youtube_dl/extractor/metaebene.py
+++ youtube_dl/extractor/metaebene.py
@@ -13,7 +13,12 @@ class MetaebeneIE(InfoExtractor):
                     |forschergeist\.de/podcast/
                     |freakshow\.fm/podcast/
                     |raumzeit-podcast.de/([^/]+/){3}
-                    |fokus-europa.de/podcast/)
+                    |fokus-europa.de/podcast/
+                    |not-safe-for-work.de/
+                    |kolophon.oreilly.de/
+                    |der-lautsprecher.de/
+                    |newz-of-the-world.com/
+                    |diegesellschafter.metaebene.me/)
                     (?P<id>[^-]+)[^\s]*'''
     _TESTS = [{
         'url': 'https://cre.fm/cre217-mythos-68er',
@@ -85,6 +90,74 @@ class MetaebeneIE(InfoExtractor):
         'params': {
             'skip_download': True,
         },
+    }, {
+        'url': 'https://not-safe-for-work.de/nsfw098-publikationspromiskuitaet/',
+        'info_dict': {
+            'id': 'nsfw098',
+            'ext': 'opus',
+            'formats': 'mincount:4',
+            'title': 'NSFW098 Publikationspromiskuität',
+            'description': 'md5:b4b503a38c1dc37950fd40b7969c28d5',
+            'site_name': 'Not Safe For Work',
+            'thumbnail': r're:^https?://.*\.jpg$',
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'https://kolophon.oreilly.de/kol017-startups/',
+        'info_dict': {
+            'id': 'kol017',
+            'ext': 'opus',
+            'formats': 'mincount:4',
+            'title': 'KOL017 Startups',
+            'description': 'md5:c2db87c7be046c9f3c712584912db999',
+            'site_name': 'Kolophon',
+            'thumbnail': r're:^https?://.*\.jpg$',
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'https://der-lautsprecher.de/ls018-audioformate-fuer-podcasts',
+        'info_dict': {
+            'id': 'ls018',
+            'ext': 'opus',
+            'formats': 'mincount:4',
+            'title': 'LS018 Audioformate für Podcasts',
+            'description': 'md5:9eea235a49b6ed8bf6b11fa112735e47',
+            'site_name': 'Der Lautsprecher',
+            'thumbnail': r're:^https?://.*\.jpg$',
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'https://newz-of-the-world.com/newz088-were-gonna-miss-him-when-hes-gone/',
+        'info_dict': {
+            'id': 'newz088',
+            'ext': 'oga',
+            'formats': 'mincount:4',
+            'title': 'NEWZ088 We\'re gonna miss him when he\'s gone',
+            'description': 'md5:f48c83fc3f10c950307dca5d318ae346',
+            'site_name': 'Newz of the World',
+            'thumbnail': r're:^https?://.*\.jpg$',
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'https://diegesellschafter.metaebene.me/dg024-freiwilliges-soziales-jahr/',
+        'info_dict': {
+            'id': 'dg024',
+            'ext': 'oga',
+            'formats': 'mincount:4',
+            'title': 'DG024 Freiwilliges Soziales Jahr',
+            'thumbnail': r're:^https?://.*\.jpg$',
+        },
+        'params': {
+            'skip_download': True,
+        },
     }]
 
     def _real_extract(self, url):
@@ -103,11 +176,18 @@ class MetaebeneIE(InfoExtractor):
                 'url': audio_url[0],
                 'format_id': audio_type[0]})
 
+        # The "Die Gesellschafter" series is discontinued and the description can't be extracted easily.
+        # This is a workaround to suppress the warning and thus make it testable and avoid bug reports.
+        if re.match(r'https?://(?:www\.)?diegesellschafter.metaebene.me/(?P<id>[^-]+)[^\s]*', url):
+            description = None
+        else:
+            description = self._og_search_description(webpage)
+
         return {
             'id': content_id,
             'title': self._og_search_title(webpage),
             'site_name': self._og_search_property('site_name', webpage),
-            'description': self._og_search_description(webpage),
+            'description': description,
             'thumbnail': self._og_search_thumbnail(webpage),
             'formats': formats,
         }
