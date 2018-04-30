2605e97bda37927fd9ac676489ab3077fa42d566
Parth Verma <v.parth98@gmail.com>
2018-04-30 21:22:27 +0530

    Made playlist title non fatal

--- youtube_dl/extractor/ibm_think.py
+++ youtube_dl/extractor/ibm_think.py
@@ -50,6 +50,6 @@ class IbmThinkPlaylistIE(InfoExtractor):
         playlist_id = self._match_id(url)
         webpage = self._download_webpage(url, playlist_id)
         entries = [self.url_result(m, GenericIE.ie_key()) for m in re.findall(r'<a href="(.+?)" class="video-list-item js-video-list-item?">', webpage)]
-        title = self._html_search_regex(r'<title>.+?\s\|\s.+?\s\|\s(.+?)</title>', webpage, 'title')
+        title = self._html_search_regex(r'<title>.+?\s\|\s.+?\s\|\s(.+?)</title>', webpage, 'title', fatal=False)
         description = self._og_search_description(webpage)
         return self.playlist_result(entries, playlist_id, title, description)
