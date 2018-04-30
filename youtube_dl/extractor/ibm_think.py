201f149cf7e4398976fe1a04c50e31fb2594e509
Parth Verma <v.parth98@gmail.com>
2018-04-30 21:21:58 +0530

    Replaced entry match regex to match links with specified classes only

--- youtube_dl/extractor/ibm_think.py
+++ youtube_dl/extractor/ibm_think.py
@@ -49,7 +49,7 @@ class IbmThinkPlaylistIE(InfoExtractor):
     def _real_extract(self, url):
         playlist_id = self._match_id(url)
         webpage = self._download_webpage(url, playlist_id)
-        entries = [self.url_result(m, GenericIE.ie_key()) for m in re.findall(r'<a href="(.+?)" class="(?:video-list-item)?\s?(?:js-video-list-item)?">', webpage)]
+        entries = [self.url_result(m, GenericIE.ie_key()) for m in re.findall(r'<a href="(.+?)" class="video-list-item js-video-list-item?">', webpage)]
         title = self._html_search_regex(r'<title>.+?\s\|\s.+?\s\|\s(.+?)</title>', webpage, 'title')
         description = self._og_search_description(webpage)
         return self.playlist_result(entries, playlist_id, title, description)
