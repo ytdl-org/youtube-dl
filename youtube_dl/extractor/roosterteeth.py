d2b1e0c733d1e955b6aae33ade37122cadaa4541
Urgau <lolo.branstett@numericable.fr>
2018-06-11 17:34:56 +0200

    Remove unnecessary brackets

--- youtube_dl/extractor/roosterteeth.py
+++ youtube_dl/extractor/roosterteeth.py
@@ -27,7 +27,6 @@ class RoosterTeethIE(InfoExtractor):
             'description': 'md5:0cc3b21986d54ed815f5faeccd9a9ca5',
             'thumbnail': r're:^https?://.*\.png$',
             'series': 'Million Dollars, But...',
-            'episode': 'Million Dollars, But... The Game Announcement',
         },
     }, {
         'url': 'http://achievementhunter.roosterteeth.com/episode/off-topic-the-achievement-hunter-podcast-2016-i-didn-t-think-it-would-pass-31',
@@ -48,7 +47,7 @@ class RoosterTeethIE(InfoExtractor):
     }]
 
     def _login(self):
-        (username, password) = self._get_login_info()
+        username, password = self._get_login_info()
         if username is None:
             return
 
