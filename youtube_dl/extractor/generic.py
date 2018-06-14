8ea9ae60086a8d7dabf570928e6b2692c59babeb
Mohammed Yaseen Mowzer <yaseen@mowzer.co.za>
2018-06-14 17:12:33 +0200

    Continue trying after _parse_jwplayer_data fails

--- youtube_dl/extractor/generic.py
+++ youtube_dl/extractor/generic.py
@@ -3119,9 +3119,13 @@ class GenericIE(InfoExtractor):
         jwplayer_data = self._find_jwplayer_data(
             webpage, video_id, transform_source=js_to_json)
         if jwplayer_data:
-            info = self._parse_jwplayer_data(
-                jwplayer_data, video_id, require_title=False, base_url=url)
-            return merge_dicts(info, info_dict)
+            try:
+                info = self._parse_jwplayer_data(
+                    jwplayer_data, video_id, require_title=False, base_url=url)
+                return merge_dicts(info, info_dict)
+            except ExtractorError, e:
+                self.to_screen(e.msg_without_bug_report)
+                self.to_screen("Trying different extractor")
 
         # Video.js embed
         mobj = re.search(
