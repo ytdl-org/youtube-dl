50a0d47bf6e4befc6f4149eb529ea08d9a421f88
Mohammed Al Sahaf <msaa1990@gmail.com>
2018-08-25 11:54:27 +0300

    [webofstories] capture playlist members by id attribute

--- youtube_dl/extractor/webofstories.py
+++ youtube_dl/extractor/webofstories.py
@@ -133,10 +133,10 @@ class WebOfStoriesPlaylistIE(InfoExtractor):
         webpage = self._download_webpage(url, playlist_id)
 
         entries = [
-            self.url_result('http://www.webofstories.com/play/%s/%d' % (playlist_id, video_number), 'WebOfStories')
-            for video_number in range(1, len(re.findall(r'class="playListItem\s+"', webpage)) + 2)
+            self.url_result('http://www.webofstories.com/play/%s/%s' % (playlist_id, video_number), 'WebOfStories')
+            for video_number in range(1, len(re.findall(r'id="(\d+)"', webpage)) + 1)
         ]
-
+        print(entries[0])
         title = self._search_regex(
             r'<div id="speakerName">\s*<span>([^<]+)</span>',
             webpage, 'speaker', default=None)
