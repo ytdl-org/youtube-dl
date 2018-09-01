deeb21c8f26607453f571ab0621056e44b76e4fc
Gorfiend <Gorfiend@gmail.com>
2018-08-31 21:14:01 -0400

    [niconico] Encode json data in request for formats (closes #17393)

--- youtube_dl/extractor/niconico.py
+++ youtube_dl/extractor/niconico.py
@@ -252,7 +252,7 @@ class NiconicoIE(InfoExtractor):
                     },
                     'timing_constraint': 'unlimited'
                 }
-            }))
+            }).encode("utf-8"))
 
         resolution = video_quality.get('resolution', {})
 
