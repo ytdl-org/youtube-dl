8f6f102fcdca22559b22c6e608d22bbe7d7d2717
Eitan Postavsky <eitan.postavsky@gmail.com>
2018-10-06 12:35:35 -0400

    [franceinter] download both video and audio

--- youtube_dl/extractor/franceinter.py
+++ youtube_dl/extractor/franceinter.py
@@ -26,15 +26,12 @@ class FranceInterIE(InfoExtractor):
             'note': 'Audio + video (Dailymotion embed)',
             'url': 'https://www.franceinter.fr/emissions/l-instant-m/l-instant-m-13-fevrier-2018',
             'info_dict': {
-                'id': 'x6eow0v',
-                'ext': 'mp4',
-                'title': 'Propagande, stéréotypes, spectaculaire : les jeux vidéo font-ils du mal à l\'Histoire ?',
-                'description': 'Le youtubeur Nota Bene pour \"History’s Creed\", la nouvelle websérie d\'ARTE Creative qui explore la relation Jeux vidéo et Histoire. Que nos enfants peuvent-ils apprendre de la Révolution française en jouant à « Assasin’s Creed » ? Et quelle vision auront-il de la Seconde Guerre mondiale en plongeant dans « Call of duty » ?   Les jeux vidéo constituent un support majeur de représentation de l’Histoire avec un grand H. Simple décor ? Prétexte narratif ? Ou nouvelle lecture du passé ?  Le jeu vidéo est devenu un médium si puissant et si mondialisé qu’il nous faut non seulement interroger la crédibilité de ses représentations, mais aussi l’idéologie de ses récits. Mon invité n’ignore rien des stéréotypes historiques, du spectaculaire trompeur et des tentations propagandistes auxquelles peuvent céder les jeux vidéo. Ils n’en demeurent pas moins, à ses yeux, un pont formidable vers la connaissance.      Ben dit Nota Bene, sur Youtube, sa chaîne d’histoire y est massivement suivie. Il y raconte l’Histoire de manière ludique et pointue à la fois. Sans hésiter à piocher dans la pop culture, type Harry Potter ou Game of Thrones…    History’s Creed, c’est à voir sur le site d’Arte, arte creative, c’est en 10 petits épisodes. Et c’est formidable.  L\'Instant M  , l’invité était Nota Bene (9h40 - 13 Février 2018) Retrouvez L\'Instant M sur www.franceinter.fr',
-                'uploader_id': 'x2q2ez',
-                'timestamp': 1518516881,
-                'uploader': 'France Inter',
+                'id': 'l-instant-m/l-instant-m-13-fevrier-2018',
+                'title': 'Propagande, stéréotypes, spectaculaire : les jeux vidéo font-ils du mal à l\'Histoire ?',
+                'description': 'Le youtubeur Nota Bene pour \\\"History’s Creed\\\", la nouvelle websérie d\'ARTE Creative qui explore la relation Jeux vidéo et Histoire',
                 'upload_date': '20180213',
             },
+            'playlist_count': 2,
         },
     ]
 
@@ -42,17 +39,6 @@ class FranceInterIE(InfoExtractor):
         video_id = self._match_id(url)
 
         webpage = self._download_webpage(url, video_id)
-
-        # If there is a video version, use that instead
-        maybe_video_uuid = re.search(r'data-video-anchor-target=["\']([^"\']+)', webpage)
-        if maybe_video_uuid:
-            video_uuid = maybe_video_uuid.group(1)
-            video_url = self._search_regex(
-                r'(?sx)data-uuid=["\']%s.*?<iframe[^>]*src=["\']([^"\']+)' % video_uuid,
-                webpage, 'video url', fatal=False, group=1)
-
-            if video_url:
-                return self.url_result(video_url)
 
         audio_url = self._search_regex(
             r'(?s)<div[^>]+class=["\']page-diffusion["\'][^>]*>.*?<button[^>]+data-url=(["\'])(?P<url>(?:(?!\1).)+)\1',
@@ -73,7 +59,7 @@ class FranceInterIE(InfoExtractor):
         else:
             upload_date = None
 
-        return {
+        audio = {
             'id': video_id,
             'title': title,
             'description': description,
@@ -83,3 +69,27 @@ class FranceInterIE(InfoExtractor):
                 'vcodec': 'none',
             }],
         }
+
+        # If there is a video, return playlist of audio + video, else just audio
+        maybe_video_uuid = re.search(r'data-video-anchor-target=["\']([^"\']+)', webpage)
+        if maybe_video_uuid:
+            video_uuid = maybe_video_uuid.group(1)
+            video_url = self._search_regex(
+                r'(?sx)data-uuid=["\']%s.*?<iframe[^>]*src=["\']([^"\']+)' % video_uuid,
+                webpage, 'video url', fatal=False, group=1)
+
+            if video_url:
+                video = {'_type': 'url', 'url': video_url}
+
+                return {
+                    '_type': 'playlist',
+                    'id': video_id,
+                    'title': title,
+                    'description': description,
+                    'upload_date': upload_date,
+                    'entries': [audio, video]
+                }
+            else:
+                return audio
+        else:
+            return audio
