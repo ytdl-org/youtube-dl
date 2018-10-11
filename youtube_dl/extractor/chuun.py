b5f363c294f0cad3160645d3d8354c279e20d868
sichuan-pepper <43799410+sichuan-pepper@users.noreply.github.com>
2018-10-11 22:19:59 +0900

    [Chuun] Add new extractor

---
+++ youtube_dl/extractor/chuun.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .common import InfoExtractor
+
+import re
+
+
+class ChuunIE(InfoExtractor):
+    _VALID_URL = r'https://chuun\.ctv\.co\.jp/player/(?P<video_id>[0-9]+)'
+    _TEST = {
+        'url': 'https://chuun.ctv.co.jp/player/1',
+        'md5': '964452757b7c2ae1dd35e019068a3fae',
+        'info_dict': {
+            'id': '1',
+            'ext': 'mp4',
+            'title': '#0　中京テレビ放送　初来社　2015年8月23日配信分',
+            'description': "太田上田 #0　中京テレビ放送　初来社　2015年8月23日配信分",
+            'thumbnail': r're:^https?://.+\.(jpg|png)$',
+        }
+    }
+
+    def _real_extract(self, url):
+        mobj = re.match(self._VALID_URL, url)
+        video_id = mobj.group('video_id')
+        webpage = self._download_webpage(url, video_id)
+        m3u8_url = re.findall(r"'movieAccessUrl':'(http[^']+)'", webpage)[0]
+        title = self._og_search_title(webpage) or self._html_search_regex(r'<title>(.+)</title>', webpage, 'page_title')
+        description = self._html_search_meta('description', webpage)
+        thumbnail = self._og_search_thumbnail(webpage)\
+            or self._html_search_regex(r"'programThumbnailUrlLarge':'(http[^']+)'", webpage, 'large_thumbnail')\
+            or self._html_search_regex(r"'programThumbnailUrl':'(http[^']+)'", webpage, 'medium_thumbnail')\
+            or self._html_search_regex(r"'programThumbnailUrlSmall':'(http[^']+)'", webpage, 'small_thumbnail')
+        formats = self._extract_m3u8_formats(m3u8_url, video_id, ext='mp4')
+        self._sort_formats(formats)
+        return {
+            'id': video_id,
+            'url': url,
+            'title': title,
+            'description': description,
+            'thumbnail': thumbnail,
+            'formats': formats,
+        }
