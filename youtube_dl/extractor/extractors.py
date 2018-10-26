de27a78e97c1a1fd70b05e40db54adf571ca08ed
sichuan-pepper <huajiao.sichuan.pepper@gmail.com>
2018-10-27 03:40:44 +0900

    [twitcasting] Add extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1200,6 +1200,7 @@ from .tweakers import TweakersIE
 from .twentyfourvideo import TwentyFourVideoIE
 from .twentymin import TwentyMinutenIE
 from .twentythreevideo import TwentyThreeVideoIE
+from .twitcasting import TwitcastingIE
 from .twitch import (
     TwitchVideoIE,
     TwitchChapterIE,
