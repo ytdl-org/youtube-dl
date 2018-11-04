f3bffc9ec623583bd52454d9e73109c3ba88720e
Ashwin Dhakaita <ashwingpdhakaita@gmail.com>
2018-11-04 09:38:58 +0530

    [news18] Add new extractor

--- youtube_dl/extractor/news18.py
+++ youtube_dl/extractor/news18.py
@@ -6,6 +6,15 @@ from .common import InfoExtractor
 
 class News18IE(InfoExtractor):
     _VALID_URL = r'''https?:\/\/www\.news18\.com[a-zA-Z0-9_\/-]+-(?P<id>\d+)\.html'''
+    _TEST = {
+        'url' : 'https://www.news18.com/news/ivideos/inside-naxal-bastion-news18-visits-the-villages-voting-first-time-ever-1928149.html',
+        'md5' : 'cb5a78310f3e583da5ba0de38b450938',
+        'info_dict': {
+            'id': '1928149',
+            'ext': 'mp4',
+            'title': 'Inside Naxal Bastion: News18 Visits The Villages Voting First Time Ever',
+        }
+    }
 
     def _real_extract(self, url):
         IE_NAME = 'News18'
@@ -14,10 +23,13 @@ class News18IE(InfoExtractor):
         video_url = self._search_regex(r'(?P<url>https?:\/\/vodpd\.news18\.com[\/\w_-]+\.mp4)', webpage, 'video URL',default='')
         title = self._og_search_title(webpage)
 
+        print(video_url)
+        print(video_id)
+        print(title)
         return {
             'url': video_url,
             'id': video_id,
             'title': title,
-            'ext': '.mp4'
+            'ext': 'mp4'
         }
 
