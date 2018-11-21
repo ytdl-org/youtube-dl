157eef3e635230cbba0dd0c74f7115029867533e
Sergey M․ <dstftw@gmail.com>
2018-11-22 02:08:41 +0700

    [setup.py] Add python 3.8 classifier

--- setup.py
+++ setup.py
@@ -135,6 +135,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: IronPython',
