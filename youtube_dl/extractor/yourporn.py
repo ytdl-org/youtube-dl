9dd0ce3b407777a559d1135c9f10b50aed4b3840
v-delta <vdelta@uber.space>
2019-01-14 13:10:36 +0100

    Fix extractor for yourporn.sexy (Closes #18815)

--- youtube_dl/extractor/yourporn.py
+++ youtube_dl/extractor/yourporn.py
@@ -27,7 +27,7 @@ class YourPornIE(InfoExtractor):
             self._search_regex(
                 r'data-vnfo=(["\'])(?P<data>{.+?})\1', webpage, 'data info',
                 group='data'),
-            video_id)[video_id]).replace('/cdn/', '/cdn3/')
+            video_id)[video_id]).replace('/cdn/', '/cdn4/')
 
         title = (self._search_regex(
             r'<[^>]+\bclass=["\']PostEditTA[^>]+>([^<]+)', webpage, 'title',
