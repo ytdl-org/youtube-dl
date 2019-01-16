5b2b26cb84671264f5161f5cba728526ad03ad28
Chun-Kwong Wong <chunkwong.wong@gmail.com>
2019-01-16 13:07:07 -0500

    [openload] Fixed openload extract by replacing PhantomJS with dryscrape

--- youtube_dl/YoutubeDL.py
+++ youtube_dl/YoutubeDL.py
@@ -94,7 +94,6 @@ from .utils import (
 )
 from .cache import Cache
 from .extractor import get_info_extractor, gen_extractor_classes, _LAZY_LOADER
-from .extractor.openload import PhantomJSwrapper
 from .downloader import get_suitable_downloader
 from .downloader.rtmp import rtmpdump_version
 from .postprocessor import (
@@ -2260,7 +2259,6 @@ class YoutubeDL(object):
 
         exe_versions = FFmpegPostProcessor.get_versions(self)
         exe_versions['rtmpdump'] = rtmpdump_version()
-        exe_versions['phantomjs'] = PhantomJSwrapper._version()
         exe_str = ', '.join(
             '%s %s' % (exe, v)
             for exe, v in sorted(exe_versions.items())
