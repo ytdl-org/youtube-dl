13d441b1b167d5123f49d239e5b106f5c73a9890
Pablo Castorino <castorinop@gmail.com>
2019-01-31 15:03:46 -0300

    working tests

--- youtube_dl/extractor/contar.py
+++ youtube_dl/extractor/contar.py
@@ -121,17 +121,25 @@ class ContarIE(ContarBaseIE):
     _VALID_URL = r'https?://(?:www\.)?cont\.ar/watch/(?P<id>%s)' % _UUID_RE
     _TEST = {
         'url': 'https://www.cont.ar/watch/d2815f05-f52f-499f-90d0-5671e9e71ce8',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
+        'md5': '72cfee8799d964291433004c557d0b2b',
         'info_dict': {
             'id': 'd2815f05-f52f-499f-90d0-5671e9e71ce8',
             'ext': 'mp4',
-            'title': 'Video title goes here',
-            'thumbnail': r're:^https?://.*\.jpg$',
+            'title': 'Matilde todos los d\u00edas',
+            'duration': 648,
+            'release_year': 2016,
+            'description': 'Matilde llega a la casa de su abuelo Barbacrespa y en ella accidentalmente rompe un coco m\u00e1gico que la dota de poder.',
+            'season_number': 1,
+            'episode_number': 1,
             # TODO more properties, either as:
             # * A value
             # * MD5 checksum; start the string with md5:
             # * A regular expression; start the string with re:
             # * Any Python type (for example int or float)
+        },
+        'params': {
+            'usenetrc': True,
+            'format': 'hls-4755-1'
         }
     }
             
@@ -149,17 +157,53 @@ class ContarSerieIE(ContarBaseIE):
     _VALID_URL = r'https?://(?:www\.)?cont\.ar/serie/(?P<id>%s)' % _UUID_RE
     _TEST = {
         'url': 'https://www.cont.ar/serie/353247d5-da97-4cb6-8571-c4fbab28c643',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
         'info_dict': {
-            'id': 'd2815f05-f52f-499f-90d0-5671e9e71ce8',
-            'ext': 'mp4',
-            'title': 'Video title goes here',
-            'thumbnail': r're:^https?://.*\.jpg$',
+            'id': '353247d5-da97-4cb6-8571-c4fbab28c643',
+            'title': 'Vidas de Radio',
+            'description': 'Ana Gerschenson conduce el ciclo que repasa historias de grandes personalidades que le dieron vida al medio; marcaron una época de la Argentina y de tu vida, esas voces amigas que estuvieron siempre y son Vidas De Radio.'
+            #'thumbnail': r're:^https?://.*\.jpg$',
             # TODO more properties, either as:
             # * A value
             # * MD5 checksum; start the string with md5:
             # * A regular expression; start the string with re:
             # * Any Python type (for example int or float)
+        },
+        'playlist_count': 11,
+        'playlist': [{
+            'md5': '651e129bae9f7ee7c4c83e1263b26828',
+            'info_dict': {
+                'id': '3414c62f-7b40-439e-b74d-1dd9b0190808',
+                'ext': 'mp4',
+                'title': 'Julio Lagos',
+                'duration': 3185,
+                'release_year': 2018,
+                'description': 'Ana Gerschenson conduce el ciclo que repasa historias de grandes personalidades que le dieron vida al medio. En esta emisi\u00f3n recibi\u00f3 a Julio Lagos para repasar su trayectoria y v\u00ednculo con la radio.',
+                'season_number': 1,
+                'episode_number': 11,
+            },
+            'params': {
+                'usenetrc': True,
+                'format': 'bestvideo',
+            }
+        }, {
+            'md5': '5b80df03801c2399f62da223f16bb801',
+            'info_dict': {
+                'id': '5972ae9a-43fe-4056-81bc-ab963c057cc6',
+                'ext': 'mp4',
+                'title': 'Cacho Fontana',
+                'release_year': 2018,
+                'duration': 3052,
+                'description': 'Cacho Fontana: \u201cMi primer amor fue la Radio\u201d',
+                'season_number': 1,
+                'episode_number': 3
+            },
+            'params': {
+                'skip_download': True,
+            },
+        }],
+        'params': {
+            'usenetrc': True,
+            'format': 'bestvideo',
         }
     }
         
@@ -192,17 +236,20 @@ class ContarChannelIE(ContarBaseIE):
     _VALID_URL = r'https?://(?:www\.)?cont\.ar/channel/(?P<id>%s)' % _UUID_RE
     _TEST = {
         'url': 'https://www.cont.ar/channel/242',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
         'info_dict': {
             'id': '242',
-            'ext': 'mp4',
-            'title': 'Video title goes here',
-            'thumbnail': r're:^https?://.*\.jpg$',
+            'title': 'PAKAPAKA',
+            'description': '\xa0PAKAPAKA'
             # TODO more properties, either as:
             # * A value
             # * MD5 checksum; start the string with md5:
             # * A regular expression; start the string with re:
             # * Any Python type (for example int or float)
+        },
+        'playlist_mincount': 68,
+        'params': {
+            'usenetrc': True,
+            'skip_download': True
         }
     }
     
@@ -226,16 +273,19 @@ class ContarBrowseIE(ContarBaseIE):
     _VALID_URL = r'https?://(?:www\.)?cont\.ar/browse/genre/(?P<id>%s)' % _UUID_RE
     _TEST = {
         'url': 'https://www.cont.ar/browse/genre/46',
-        'md5': 'TODO: md5 sum of the first 10241 bytes of the video file (use --test)',
         'info_dict': {
             'id': '46',
-            'title': 'Video title goes here',
-            'thumbnail': r're:^https?://.*\.jpg$',
+            'title': 'Infantil',
             # TODO more properties, either as:
             # * A value
             # * MD5 checksum; start the string with md5:
             # * A regular expression; start the string with re:
             # * Any Python type (for example int or float)
+        },
+        'playlist_mincount': 65,
+        'params': {
+            'usenetrc': True,
+            'skip_download': True
         }
     }
         
