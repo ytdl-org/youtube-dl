afc5de1952b1f119971cdd87ed33844bf8d65cd0
Oneboy1979 <devil1979@chello.at>
2019-02-16 14:43:50 +0100

    [servus] fix for new url format

--- youtube_dl/extractor/servus.py
+++ youtube_dl/extractor/servus.py
@@ -5,26 +5,25 @@ from .common import InfoExtractor
 
 
 class ServusIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?servus\.com/(?:at|de)/p/[^/]+/(?P<id>AA-\w+|\d+-\d+)'
+    _VALID_URL = r'https?://(?:www\.)?servus\.com/tv/videos/(?P<id>aa-\w+|\d+-\d+)'
     _TESTS = [{
-        'url': 'https://www.servus.com/de/p/Die-Gr%C3%BCnen-aus-Sicht-des-Volkes/AA-1T6VBU5PW1W12/',
+        'url': 'https://www.servus.com/tv/videos/aa-1t6vbu5pw1w12/',
         'md5': '046dee641cda1c4cabe13baef3be2c1c',
         'info_dict': {
-            'id': 'AA-1T6VBU5PW1W12',
+            'id': 'aa-1t6vbu5pw1w12',
             'ext': 'mp4',
             'title': 'Die Grünen aus Volkssicht',
             'description': 'md5:052b5da1cb2cd7d562ef1f19be5a5cba',
             'thumbnail': r're:^https?://.*\.jpg$',
         }
-    }, {
-        'url': 'https://www.servus.com/at/p/Wie-das-Leben-beginnt/1309984137314-381415152/',
-        'only_matching': True,
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
+        video_id = video_id.upper()
 
+        webpage = self._download_webpage(url, video_id)
+        
         title = self._og_search_title(webpage)
         description = self._og_search_description(webpage)
         thumbnail = self._og_search_thumbnail(webpage)
