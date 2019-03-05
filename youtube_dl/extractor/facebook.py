a551768acfd177e425f518c43a2992a50a2ff69f
Sergey M․ <dstftw@gmail.com>
2019-03-06 01:27:22 +0700

    [facebook] Improve uploader extraction (closes #20250)

--- youtube_dl/extractor/facebook.py
+++ youtube_dl/extractor/facebook.py
@@ -424,7 +424,7 @@ class FacebookIE(InfoExtractor):
         uploader = clean_html(get_element_by_id(
             'fbPhotoPageAuthorName', webpage)) or self._search_regex(
             r'ownerName\s*:\s*"([^"]+)"', webpage, 'uploader',
-            fatal=False) or self._og_search_title(webpage, fatal=False)
+            default=None) or self._og_search_title(webpage, fatal=False)
         timestamp = int_or_none(self._search_regex(
             r'<abbr[^>]+data-utime=["\'](\d+)', webpage,
             'timestamp', default=None))
