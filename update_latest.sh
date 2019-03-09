59f945e79961a89bdbf586a8f58d28e284146461
Sergey M․ <dstftw@gmail.com>
2019-03-09 18:25:26 +0700

    Start moving to ytdl-org

--- update_latest.sh
+++ update_latest.sh
@@ -13,7 +13,7 @@ cd "$(dirname $(readlink -f $0))"
 echo -n "$version" > latest_version
 
 echo -e "RewriteEngine On" > downloads/.htaccess
-echo -e "RewriteRule ^$ https://github.com/rg3/youtube-dl/releases" >> downloads/.htaccess
-echo -e "RewriteRule ^(\d{4}\.\d{2}\.\d{2}(?:\.\d+)?/?)$ https://github.com/rg3/youtube-dl/releases/tag/\$1" >> downloads/.htaccess
-echo -e "RewriteRule ^(\d{4}\.\d{2}\.\d{2}(?:\.\d+)?/.+)$ https://github.com/rg3/youtube-dl/releases/download/\$1" >> downloads/.htaccess
+echo -e "RewriteRule ^$ https://github.com/ytdl-org/youtube-dl/releases" >> downloads/.htaccess
+echo -e "RewriteRule ^(\d{4}\.\d{2}\.\d{2}(?:\.\d+)?/?)$ https://github.com/ytdl-org/youtube-dl/releases/tag/\$1" >> downloads/.htaccess
+echo -e "RewriteRule ^(\d{4}\.\d{2}\.\d{2}(?:\.\d+)?/.+)$ https://github.com/ytdl-org/youtube-dl/releases/download/\$1" >> downloads/.htaccess
 echo -e "RewriteRule latest(.*) /downloads/$1\$1 [L,R=302]" >> downloads/.htaccess
