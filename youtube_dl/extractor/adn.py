2bbde1d09afb2225fb7bd245bcf77a0715a58f29
Remita Amine <remitamine@gmail.com>
2019-04-04 17:59:20 +0100

    [adn] fix subtitle compatibility with ffmpeg

--- youtube_dl/extractor/adn.py
+++ youtube_dl/extractor/adn.py
@@ -81,10 +81,10 @@ class ADNIE(InfoExtractor):
             ssa = '''[Script Info]
 ScriptType:V4.00
 [V4 Styles]
-Format:Name,Fontname,Fontsize,PrimaryColour,Bold,BorderStyle,Outline,Alignment,MarginL,MarginR,MarginV
-Style:Default,Arial,18,16777215,-1,1,1,2,20,20,20
+Format: Name,Fontname,Fontsize,PrimaryColour,SecondaryColour,TertiaryColour,BackColour,Bold,Italic,BorderStyle,Outline,Shadow,Alignment,MarginL,MarginR,MarginV,AlphaLevel,Encoding
+Style: Default,Arial,18,16777215,16777215,16777215,0,-1,0,1,1,0,2,20,20,20,0,0
 [Events]
-Format:Marked,Start,End,Style,Text'''
+Format: Marked,Start,End,Style,Name,MarginL,MarginR,MarginV,Effect,Text'''
             for current in sub:
                 start, end, text, line_align, position_align = (
                     float_or_none(current.get('startTime')),
@@ -94,7 +94,7 @@ Format:Marked,Start,End,Style,Text'''
                 if start is None or end is None or text is None:
                     continue
                 alignment = self._POS_ALIGN_MAP.get(position_align, 2) + self._LINE_ALIGN_MAP.get(line_align, 0)
-                ssa += os.linesep + 'Dialogue:Marked=0,%s,%s,Default,%s%s' % (
+                ssa += os.linesep + 'Dialogue: Marked=0,%s,%s,Default,,0,0,0,,%s%s' % (
                     self._ass_subtitles_timecode(start),
                     self._ass_subtitles_timecode(end),
                     '{\\a%d}' % alignment if alignment != 2 else '',
