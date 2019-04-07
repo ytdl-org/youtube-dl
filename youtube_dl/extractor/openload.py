bf6fb8b9dc921a30df24d9789d5bbb0ac5b370b0
ealgase <mostdigitsofpi@gmail.com>
2019-04-06 23:38:40 -0400

    [openload] add tests

--- youtube_dl/extractor/openload.py
+++ youtube_dl/extractor/openload.py
@@ -350,6 +350,12 @@ class OpenloadIE(InfoExtractor):
     }, {
         'url': 'https://oload.space/f/IY4eZSst3u8/',
         'only_matching': True,
+    }, {
+        'url': 'https://oladblock.services/f/b8NWEgkqNLI/',
+        'only_matching': True,
+    }, {
+        'url': 'https://oladblock.xyz/f/b8NWEgkqNLI/',
+        'only_matching': True,
     }]
 
     _USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
