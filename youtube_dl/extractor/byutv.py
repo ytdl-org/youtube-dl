c25d6881e88bc0d92d2a6aa7d6f167a247365130
Michael Tilbury <mtilbury@umich.edu>
2019-04-14 18:30:46 -0400

    [BYUtv] Add support for akamai VODs (addresses #20574)
    Fix code style on brackets (flake8)
    
    Add more information to test info_dict

--- youtube_dl/extractor/byutv.py
+++ youtube_dl/extractor/byutv.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import re
 
 from .common import InfoExtractor
+from ..utils import url_basename
 
 
 class BYUtvIE(InfoExtractor):
@@ -23,6 +24,27 @@ class BYUtvIE(InfoExtractor):
         },
         'add_ie': ['Ooyala'],
     }, {
+        'url': 'https://www.byutv.org/player/a5467e14-c7f2-46f9-b3c2-cb31a56749c6/byu-soccer-w-argentina-vs-byu-4419',
+        'info_dict': {
+            'id': 'a5467e14-c7f2-46f9-b3c2-cb31a56749c6',
+            'display_id': 'byu-soccer-w-argentina-vs-byu-4419',
+            'ext': 'mp4',
+            'title': 'Argentina vs. BYU (4/4/19)',
+            'length': '02:05:43',
+        },
+    }, {
+        'url': 'https://www.byutv.org/player/a5467e14-c7f2-46f9-b3c2-cb31a56749c6/byu-soccer-w-argentina-vs-byu-4419',
+        'info_dict': {
+            'id': 'a5467e14-c7f2-46f9-b3c2-cb31a56749c6',
+            'display_id': 'byu-soccer-w-argentina-vs-byu-4419',
+            'ext': 'mp4',
+            'title': 'Argentina vs. BYU (4/4/19)',
+            'length': '02:05:43',
+        },
+        'params': {
+            'skip_download': True
+        },
+    }, {
         'url': 'http://www.byutv.org/watch/6587b9a3-89d2-42a6-a7f7-fd2f81840a7d',
         'only_matching': True,
     }, {
@@ -33,9 +55,8 @@ class BYUtvIE(InfoExtractor):
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('id')
-        display_id = mobj.group('display_id') or video_id
 
-        ep = self._download_json(
+        info = self._download_json(
             'https://api.byutv.org/api3/catalog/getvideosforcontent', video_id,
             query={
                 'contentid': video_id,
@@ -44,15 +65,31 @@ class BYUtvIE(InfoExtractor):
             }, headers={
                 'x-byutv-context': 'web$US',
                 'x-byutv-platformkey': 'xsaaw9c7y5',
-            })['ooyalaVOD']
+            })
 
-        return {
-            '_type': 'url_transparent',
-            'ie_key': 'Ooyala',
-            'url': 'ooyala:%s' % ep['providerId'],
-            'id': video_id,
-            'display_id': display_id,
-            'title': ep.get('title'),
-            'description': ep.get('description'),
-            'thumbnail': ep.get('imageThumbnail'),
-        }
+        if 'ooyalaVOD' in info:
+            ep = info['ooyalaVOD']
+            return {
+                '_type': 'url_transparent',
+                'ie_key': 'Ooyala',
+                'url': 'ooyala:%s' % ep['providerId'],
+                'id': video_id,
+                'display_id': mobj.group('display_id') or video_id,
+                'title': ep.get('title'),
+                'description': ep.get('description'),
+                'thumbnail': ep.get('imageThumbnail'),
+            }
+        elif 'dvr' in info:
+            ep = info['dvr']
+            formats = self._extract_m3u8_formats(
+                ep['videoUrl'], video_id, 'mp4', entry_protocol='m3u8_native'
+            )
+            return {
+                'formats': formats,
+                'id': video_id,
+                'display_id': url_basename(url),
+                'title': ep.get('title'),
+                'description': ep.get('description'),
+                'thumbnail': ep.get('imageThumbnail'),
+                'length': ep.get('length'),
+            }
