6104cc15915abfe113152d24b18c84178e027265
ealgase <mostdigitsofpi@gmail.com>
2019-04-15 23:09:05 -0400

    [openload] add test for oladblock.me

--- youtube_dl/extractor/openload.py
+++ youtube_dl/extractor/openload.py
@@ -356,6 +356,9 @@ class OpenloadIE(InfoExtractor):
     }, {
         'url': 'https://oladblock.xyz/f/b8NWEgkqNLI/',
         'only_matching': True,
+    }, {
+        'url': 'https://oladblock.me/f/b8NWEgkqNLI/',
+        'only_matching': True,
     }]
 
     _USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
