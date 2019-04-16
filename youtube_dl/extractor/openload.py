f114e43d3845b13227d4a1251bfb7d2750e76032
ealgase <mostdigitsofpi@gmail.com>
2019-04-15 23:08:28 -0400

    [openload] add oladblock.me domain

--- youtube_dl/extractor/openload.py
+++ youtube_dl/extractor/openload.py
@@ -243,7 +243,7 @@ class PhantomJSwrapper(object):
 
 
 class OpenloadIE(InfoExtractor):
-    _DOMAINS = r'(?:openload\.(?:co|io|link|pw)|oload\.(?:tv|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|pw|live|space)|oladblock\.(?:services|xyz))'
+    _DOMAINS = r'(?:openload\.(?:co|io|link|pw)|oload\.(?:tv|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|pw|live|space)|oladblock\.(?:services|xyz|me))'
     _VALID_URL = r'''(?x)
                     https?://
                         (?P<host>
