6123630c0d94d144f5e1f5a5d114f50294515fda
Jakub Wilk <jwilk@jwilk.net>
2019-04-24 11:54:00 +0200

    Update youtube_dl/extractor/bajeczki.py
    Co-Authored-By: Monastario <marcinrogacki@gmail.com>

--- youtube_dl/extractor/bajeczki.py
+++ youtube_dl/extractor/bajeczki.py
@@ -5,7 +5,7 @@ from .common import InfoExtractor
 
 
 class BajeczkiIE(InfoExtractor):
-    _VALID_URL = r'http?://(?:www\.)?bajeczki\.org/(?P<id>.*)'
+    _VALID_URL = r'https?://(?:www\.)?bajeczki\.org/(?P<id>.*)'
     _TEST = {
         'url': 'http://bajeczki.org/psi-patrol/pieski-ratuja-przyjaciol-ksiezniczki/',
         'md5': '01f72e7e641448785db6a9bd77a94b31',
