11ec06de7f01bced1f4f4b6484e5190cbb9ed9e3
Sergey M․ <dstftw@gmail.com>
2019-05-24 00:35:46 +0700

    [24video] Add support for 24video.site (closes #21193)

--- youtube_dl/extractor/twentyfourvideo.py
+++ youtube_dl/extractor/twentyfourvideo.py
@@ -14,7 +14,7 @@ from ..utils import (
 
 class TwentyFourVideoIE(InfoExtractor):
     IE_NAME = '24video'
-    _VALID_URL = r'https?://(?P<host>(?:www\.)?24video\.(?:net|me|xxx|sexy?|tube|adult))/(?:video/(?:view|xml)/|player/new24_play\.swf\?id=)(?P<id>\d+)'
+    _VALID_URL = r'https?://(?P<host>(?:www\.)?24video\.(?:net|me|xxx|sexy?|tube|adult|site))/(?:video/(?:view|xml)/|player/new24_play\.swf\?id=)(?P<id>\d+)'
 
     _TESTS = [{
         'url': 'http://www.24video.net/video/view/1044982',
@@ -41,6 +41,9 @@ class TwentyFourVideoIE(InfoExtractor):
         'only_matching': True,
     }, {
         'url': 'http://www.24video.tube/video/view/2363750',
+        'only_matching': True,
+    }, {
+        'url': 'https://www.24video.site/video/view/2640421',
         'only_matching': True,
     }]
 
