5611ac8b67fa1015374deae9cde1496a0b5fe280
Avichai Cohen <cavichai@videocites.com>
2019-06-11 14:18:10 +0300

    meaningless comment

--- youtube_dl/extractor/facebook.py
+++ youtube_dl/extractor/facebook.py
@@ -428,6 +428,7 @@ class FacebookIE(InfoExtractor):
         timestamp = int_or_none(self._search_regex(
             r'<abbr[^>]+data-utime=["\'](\d+)', webpage,
             'timestamp', default=None))
+        # adding a meaningless comment
         thumbnail = self._og_search_thumbnail(webpage)
 
         view_count = parse_count(self._search_regex(
