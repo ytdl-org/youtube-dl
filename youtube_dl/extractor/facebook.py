36effdc65370cbb5d94d4ddb1edf135dd3514244
Avichai Cohen <cavichai@videocites.com>
2019-06-11 12:28:15 +0300

    just changing a comment

--- youtube_dl/extractor/facebook.py
+++ youtube_dl/extractor/facebook.py
@@ -458,7 +458,7 @@ class FacebookIE(InfoExtractor):
             r'[\'\"]ownerid[\'\"]\s*:\s*[\'\"](\d+)[\'\"]', tahoe_data.secondary,
             'uploader_id', fatal=False)
 
-        # just adding a comment
+        # just changing a comment
         thumbnail = self._og_search_thumbnail(webpage)
 
         view_count = parse_count(self._search_regex(
