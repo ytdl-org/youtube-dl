ee722ada9b74f4e5ccd9da20794a73f3a75e0216
Avichai Cohen <cavichai@videocites.com>
2019-06-11 18:26:01 +0300

    adding a new comment now to see what's up with pull bot

--- youtube_dl/extractor/facebook.py
+++ youtube_dl/extractor/facebook.py
@@ -428,6 +428,7 @@ class FacebookIE(InfoExtractor):
         timestamp = int_or_none(self._search_regex(
             r'<abbr[^>]+data-utime=["\'](\d+)', webpage,
             'timestamp', default=None))
+        # adding a new comment now to see what's up
         thumbnail = self._og_search_thumbnail(webpage)
 
         view_count = parse_count(self._search_regex(
