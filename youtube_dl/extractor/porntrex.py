54e063db97644909f1c1c548dd071076dad39b43
nindogo <nindogo@gmail.com>
2019-06-27 11:27:40 +0300

    A few more clean ups to porntrex.py.

--- youtube_dl/extractor/porntrex.py
+++ youtube_dl/extractor/porntrex.py
@@ -80,8 +80,10 @@ class PornTrexIE(PornTrexBaseIE):
             thumbnails.append({'url': thumbs})
 
         formats = []
-        movie_urls = re.findall(r"'(https://www.porntrex.com/get_file/.*?)/'", webpage)
+        movie_urls = re.findall(r"[\"'](https://www.porntrex.com/get_file/.*?)/[\"']", webpage)
         for movie_url in movie_urls:
+            if re.search(r'screenshots', movie_url):
+                continue
             formats.append({'url': movie_url,
                             'height': int(self._search_regex(r'_(\d+)p\.',
                                                              movie_url,
