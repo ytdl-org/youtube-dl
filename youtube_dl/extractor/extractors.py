df5c728b065da43f43377f56bcb4b7e8a3f8ced9
geditorit <52565706+geditorit@users.noreply.github.com>
2019-07-14 15:25:04 +0700

    Update extractors.py

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -404,10 +404,6 @@ from .fusion import FusionIE
 from .fxnetworks import FXNetworksIE
 from .gaia import GaiaIE
 from .gameinformer import GameInformerIE
-from .gameone import (
-    GameOneIE,
-    GameOnePlaylistIE,
-)
 from .gamespot import GameSpotIE
 from .gamestar import GameStarIE
 from .gaskrank import GaskrankIE
