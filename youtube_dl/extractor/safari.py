18d9939ae3cc7d332d1e6abd55195a5807cdba80
phan-ctrl <54398886+phan-ctrl@users.noreply.github.com>
2019-08-26 22:42:28 +0700

    update fix for safari

--- youtube_dl/extractor/safari.py
+++ youtube_dl/extractor/safari.py
@@ -70,11 +70,11 @@ class SafariBaseIE(InfoExtractor):
 
         # oreilly serves two same groot_sessionid cookies in Set-Cookie header
         # and expects first one to be actually set
-        self._apply_first_set_cookie_header(urlh, 'groot_sessionid')
         # Fix issue #22161
         # We need 2 more cookies to be able to login to oreilly
-        self._apply_first_set_cookie_header(urlh, 'orm-jwt')
-        self._apply_first_set_cookie_header(urlh, 'orm-rt')
+        cookies = ['groot_sessionid', 'orm-jwt', 'orm-rt']
+        for cookie in cookies:
+            self._apply_first_set_cookie_header(urlh, cookie)
 
         _, urlh = self._download_webpage_handle(
             auth.get('redirect_uri') or next_uri, None, 'Completing login',)
