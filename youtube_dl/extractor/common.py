400dcfe257e9c380c78a74f807a8437267ab1d48
Jeremie J. Jarosh <jeremie@jarosh.org>
2019-08-31 15:49:15 -0500

    [extractor/common] Allow protocols to be skipped in _extract_akamai_formats

--- youtube_dl/extractor/common.py
+++ youtube_dl/extractor/common.py
@@ -2547,27 +2547,29 @@ class InfoExtractor(object):
                 entries.append(media_info)
         return entries
 
-    def _extract_akamai_formats(self, manifest_url, video_id, hosts={}):
+    def _extract_akamai_formats(self, manifest_url, video_id, hosts={}, skip_protocols=[]):
         formats = []
-        hdcore_sign = 'hdcore=3.7.0'
-        f4m_url = re.sub(r'(https?://[^/]+)/i/', r'\1/z/', manifest_url).replace('/master.m3u8', '/manifest.f4m')
-        hds_host = hosts.get('hds')
-        if hds_host:
-            f4m_url = re.sub(r'(https?://)[^/]+', r'\1' + hds_host, f4m_url)
-        if 'hdcore=' not in f4m_url:
-            f4m_url += ('&' if '?' in f4m_url else '?') + hdcore_sign
-        f4m_formats = self._extract_f4m_formats(
-            f4m_url, video_id, f4m_id='hds', fatal=False)
-        for entry in f4m_formats:
-            entry.update({'extra_param_to_segment_url': hdcore_sign})
-        formats.extend(f4m_formats)
-        m3u8_url = re.sub(r'(https?://[^/]+)/z/', r'\1/i/', manifest_url).replace('/manifest.f4m', '/master.m3u8')
-        hls_host = hosts.get('hls')
-        if hls_host:
-            m3u8_url = re.sub(r'(https?://)[^/]+', r'\1' + hls_host, m3u8_url)
-        formats.extend(self._extract_m3u8_formats(
-            m3u8_url, video_id, 'mp4', 'm3u8_native',
-            m3u8_id='hls', fatal=False))
+        if 'f4m' not in skip_protocols:
+            hdcore_sign = 'hdcore=3.7.0'
+            f4m_url = re.sub(r'(https?://[^/]+)/i/', r'\1/z/', manifest_url).replace('/master.m3u8', '/manifest.f4m')
+            hds_host = hosts.get('hds')
+            if hds_host:
+                f4m_url = re.sub(r'(https?://)[^/]+', r'\1' + hds_host, f4m_url)
+            if 'hdcore=' not in f4m_url:
+                f4m_url += ('&' if '?' in f4m_url else '?') + hdcore_sign
+            f4m_formats = self._extract_f4m_formats(
+                f4m_url, video_id, f4m_id='hds', fatal=False)
+            for entry in f4m_formats:
+                entry.update({'extra_param_to_segment_url': hdcore_sign})
+            formats.extend(f4m_formats)
+        if 'm3u8' not in skip_protocols:
+            m3u8_url = re.sub(r'(https?://[^/]+)/z/', r'\1/i/', manifest_url).replace('/manifest.f4m', '/master.m3u8')
+            hls_host = hosts.get('hls')
+            if hls_host:
+                m3u8_url = re.sub(r'(https?://)[^/]+', r'\1' + hls_host, m3u8_url)
+            formats.extend(self._extract_m3u8_formats(
+                m3u8_url, video_id, 'mp4', 'm3u8_native',
+                m3u8_id='hls', fatal=False))
         return formats
 
     def _extract_wowza_formats(self, url, video_id, m3u8_entry_protocol='m3u8_native', skip_protocols=[]):
