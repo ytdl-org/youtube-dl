894b3826f5a2e1742010c20554a6a1b9e98a51ee
sofutru <54445344+sofutru@users.noreply.github.com>
2019-10-04 18:52:15 +0700

    [youtube] Add support for yt.lelux.fi (#22597)

--- youtube_dl/extractor/youtube.py
+++ youtube_dl/extractor/youtube.py
@@ -391,6 +391,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
                             (?:www\.)?tube\.poal\.co/|
                             (?:www\.)?vid\.wxzm\.sx/|
                             (?:www\.)?yt\.elukerio\.org/|
+                            (?:www\.)?yt\.lelux\.fi/|
                             (?:www\.)?kgg2m7yk5aybusll\.onion/|
                             (?:www\.)?qklhadlycap4cnod\.onion/|
                             (?:www\.)?axqzx4s6s54s32yentfqojs3x5i7faxza6xo3ehd4bzzsg2ii4fv2iid\.onion/|
