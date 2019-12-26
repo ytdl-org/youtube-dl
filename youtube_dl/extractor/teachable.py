d4d3dfba6d291ba641bbd04077a630b4b8e9b9ea
Sergey M․ <dstftw@gmail.com>
2019-12-27 00:18:37 +0700

    [teachable] Improve locked lessons detection (#23528)

--- youtube_dl/extractor/teachable.py
+++ youtube_dl/extractor/teachable.py
@@ -165,7 +165,10 @@ class TeachableIE(TeachableBaseIE):
             if any(re.search(p, webpage) for p in (
                     r'class=["\']lecture-contents-locked',
                     r'>\s*Lecture contents locked',
-                    r'id=["\']lecture-locked')):
+                    r'id=["\']lecture-locked',
+                    # https://academy.tailoredtutors.co.uk/courses/108779/lectures/1955313
+                    r'class=["\'](?:inner-)?lesson-locked',
+                    r'>LESSON LOCKED<')):
                 self.raise_login_required('Lecture contents locked')
 
         title = self._og_search_title(webpage, default=None)
