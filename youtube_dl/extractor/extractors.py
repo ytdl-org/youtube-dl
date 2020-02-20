4c06db5dd5be822fcf9cb8ec73ae55924ff04e0b
dmsummers <dsummers@cheesycactus.com>
2020-02-20 14:33:05 -0600

    [simplecast] Add new extractor(closes #24107)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1061,6 +1061,11 @@ from .shared import (
     VivoIE,
 )
 from .showroomlive import ShowRoomLiveIE
+from .simplecast import (
+    SimplecastIE,
+    SimplecastEpisodeIE,
+    SimplecastPodcastIE,
+)
 from .sina import SinaIE
 from .sixplay import SixPlayIE
 from .skyit import (
