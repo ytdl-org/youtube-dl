b5879f6e4466a8573ce58ffc39d3f7b541d9c98a
rubyist <rubyist.personal@openmailbox.org>
2020-02-27 19:10:31 -0800

    Don't use _html_search_regex when there's no html to filter out

--- youtube_dl/extractor/matter.py
+++ youtube_dl/extractor/matter.py
@@ -22,19 +22,19 @@ class MatterIE(InfoExtractor):
             "https://api.matter.online/api/v1/open-graph/tracks/%s/embedded" % track_id, track_id
         )
 
-        author = self._html_search_regex(
+        author = self._search_regex(
             r'<a href="https://app.matter.online/artists/[^"]+" target="[^"]+">([^<]+)</a>',
             webpage, "author"
         )
-        title = self._html_search_regex(
+        title = self._search_regex(
             r'<a href="https://app.matter.online/tracks/\d+" target="[^"]+">([^<]+)</a>',
             webpage, "title"
         )
-        download_url = self._html_search_regex(
+        download_url = self._search_regex(
             r'<source src="(https://matter-production.s3.amazonaws.com/audios/[^\.]+\.[^"]+)"/>',
             webpage, "download_url"
         )
-        artwork = self._html_search_regex(
+        artwork = self._search_regex(
             r'style="background: url\((https://matter-production.s3.amazonaws.com/images/[^\.]+\.[^\)]+)\)',
             webpage, "artwork"
         )
