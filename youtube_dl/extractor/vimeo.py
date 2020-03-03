12ee431676bb655f04c7dd416a73c1f142ed368d
Remita Amine <remitamine@gmail.com>
2020-03-03 12:33:38 +0100

    [vimeo] fix showcase password protected video extraction(closes #24224)

--- youtube_dl/extractor/vimeo.py
+++ youtube_dl/extractor/vimeo.py
@@ -585,7 +585,7 @@ class VimeoIE(VimeoBaseInfoExtractor):
                 url = 'https://vimeo.com/' + video_id
         elif is_player:
             url = 'https://player.vimeo.com/video/' + video_id
-        elif any(p in url for p in ('play_redirect_hls', 'moogaloop.swf')):
+        elif any(p in url for p in ('play_redirect_hls', 'moogaloop.swf', '/album/', '/showcase/')):
             url = 'https://vimeo.com/' + video_id
 
         try:
