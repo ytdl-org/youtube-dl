47c434ba41ecbdca8067b7aab5e2d8fbbd3d9d98
Dante <m.hussain@wsu.edu>
2020-03-08 20:12:34 -0700

    yash version 2

--- youtube_dl/extractor/sonuma.py
+++ youtube_dl/extractor/sonuma.py
@@ -5,14 +5,15 @@ from .common import InfoExtractor
 
 class sonumaIE(InfoExtractor):
 
-	_VALID_URL=r'(?:https?://)?(?:www\.)?sonuma\.be/archive/[a-z-0-9]*'
+	_VALID_URL=r'(?:https?://)?(?:www\.)?sonuma\.be/archive/'
 
 	def _real_extract(self,url):
 		video_id=self._match_id(url)
-		webpage = self._download_webpage("https://www.sonuma.be/archive/"+video_id)
+		webpage = self._download_webpage("https://www.sonuma.be/archive/%s/"video_id , video_id)
 		title = self._html_search_regex(r'<h1>(.+?)</h1>', webpage, 'title')
 
-
+		download_url=self._html_search_regex(
+			r'https://vod.infomaniak.com/redirect/sonumasa_2_vod/web2-39166/copy-32/d611823b-d693-29b6-e040-010a07647b9b.mp4?sKey=2faa94b1da19002ca8b8abe944a7ecc8',webpage,"title")
 		return{
 		'id':video_id,
 		'title':title,
