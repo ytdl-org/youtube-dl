6549db78f21a7c6a002aba0e07f7450f5d4c5413
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2020-03-26 17:42:17 +0700

    [binustv] Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -107,6 +107,7 @@ from .bilibili import (
     BilibiliAudioAlbumIE,
     BiliBiliPlayerIE,
 )
+from .binustv import BinusTVAppIE
 from .biobiochiletv import BioBioChileTVIE
 from .bitchute import (
     BitChuteIE,
