5b0d10037623a1271a182078a05b2a6e1f70e2b3
Dante <m.hussain@wsu.edu>
2020-04-01 18:15:51 -0700

    acidcow_ver_pull

--- youtube_dl/extractor/acidcow.py
+++ youtube_dl/extractor/acidcow.py
@@ -9,12 +9,7 @@ class acidcowIE(InfoExtractor):
     This class should be used to handle videos. Another class (TODO) will be
     used to implement playlists or other content.
     """
-    # _VALID_URL = r'https?://app.matter.online/tracks/((?P<id>\d+)-(?P<title>\S+))/?'
-    # VALID_URL = r'https?://acidcow.com/video/([0-9]+-(?P<title>\S+))/?'
-    # # r'https://cdn.acidcow.com/pics/%s/video/(\S+)' % video_id, video_id
-    # r'<video src="https://cdn.acidcow.com/pics/([0-9]+/(?P<title>\S+))"/>',
 
-    # _VALID_URL = r'https?://acidcow\.com/video/[0-9]+\S+'
     _VALID_URL = r'https?://acidcow\.com/video/(?P<id>\d+)-\S+'
 
     _TESTS = {
@@ -24,24 +19,13 @@ class acidcowIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        '''
-        webpage = self._download_webpage(
-            r'https://cdn\.acidcow\.com/pics/[0-9]+/video/\S', video_id
-        )
-        '''
+
         webpage = self._download_webpage(
             url, video_id
         )
 
         title = self._html_search_regex(r'<title>(.+?)</title>', webpage, 'title')
-        '''
-        download_url = self._html_search_regex(
 
-            r'<video src="https://cdn\.acidcow\.com/pics/[0-9]+/video/\S+" .+',
-
-            webpage, "download_url"
-        )
-        '''
         download_url = self._html_search_regex(
 
             r'(https://cdn\.acidcow\.com/pics/[0-9]+/video/\S+\.mp4)',
