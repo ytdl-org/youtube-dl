4e7b5bba5fb73502476c61e4931284c9c3d3d232
Sergey M․ <dstftw@gmail.com>
2020-04-05 21:27:36 +0700

    [mofosex] Add support for generic embeds (closes #24633)

--- youtube_dl/extractor/generic.py
+++ youtube_dl/extractor/generic.py
@@ -60,6 +60,7 @@ from .tnaflix import TNAFlixNetworkEmbedIE
 from .drtuber import DrTuberIE
 from .redtube import RedTubeIE
 from .tube8 import Tube8IE
+from .mofosex import MofosexEmbedIE
 from .spankwire import SpankwireIE
 from .youporn import YouPornIE
 from .vimeo import VimeoIE
@@ -2716,6 +2717,11 @@ class GenericIE(InfoExtractor):
         tube8_urls = Tube8IE._extract_urls(webpage)
         if tube8_urls:
             return self.playlist_from_matches(tube8_urls, video_id, video_title, ie=Tube8IE.ie_key())
+
+        # Look for embedded Mofosex player
+        mofosex_urls = MofosexEmbedIE._extract_urls(webpage)
+        if mofosex_urls:
+            return self.playlist_from_matches(mofosex_urls, video_id, video_title, ie=MofosexEmbedIE.ie_key())
 
         # Look for embedded Spankwire player
         spankwire_urls = SpankwireIE._extract_urls(webpage)
