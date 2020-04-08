1a44fd01069d9a8b540de0284656f02c770c4522
MaximZ21 <razel.agressor.21@gmail.com>
2020-04-08 17:36:01 +0600

    Minor change to satisfy flake8

--- youtube_dl/extractor/vlaretv.py
+++ youtube_dl/extractor/vlaretv.py
@@ -21,7 +21,7 @@ class VlaretvPlaylistIE(InfoExtractor):
         webpage = self._download_webpage(url, playlist_id)
         urls = re.findall(r'<a href="(.+?)" class="video_thumbnail"', webpage)
         title = self._html_search_regex(r'<title>(.+?) \| Vlare</title>', webpage, 'title')
-        
+
         # When playlist points to deleted video there is an "error" in the url (Ex. https://vlare.tv/v/error/3257)
         entries = [self.url_result(urljoin('https://vlare.tv', u)) for u in urls if 'error' not in u]
 
