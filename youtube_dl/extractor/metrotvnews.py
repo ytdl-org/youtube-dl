58be8f1966a42b70f7b9fc28afe7d5fe9556da12
Dante <m.hussain@wsu.edu>
2020-04-08 20:40:51 -0700

    Formatting of code according to convention

--- youtube_dl/extractor/metrotvnews.py
+++ youtube_dl/extractor/metrotvnews.py
@@ -3,21 +3,15 @@ from __future__ import unicode_literals
 from .common import InfoExtractor
 
 
-
-
 class MetrotvnewsIE(InfoExtractor):
 
     _VALID_URL = r'https:\/\/www.metrotvnews\.com\/play\/(?P<id>\S+)-\S+'
 
-
     def _real_extract(self, url):
-
         video_id = self._match_id(url)
-
         webpage = self._download_webpage(
             url, video_id
         )
-
         title = self._html_search_regex(r'<title>(.+) - www.metrotvnews.com<\/title>', webpage, 'title')
 
         download_url = self._html_search_regex(
