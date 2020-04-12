d878b8dd2b675c75ea52793507e805d928886fd3
Dante <m.hussain@wsu.edu>
2020-04-11 22:21:09 -0700

    Test added

--- youtube_dl/extractor/acidcow.py
+++ youtube_dl/extractor/acidcow.py
@@ -12,10 +12,30 @@ class acidcowIE(InfoExtractor):
 
     _VALID_URL = r'https?://acidcow\.com/video/(?P<id>\d+)-\S+'
 
-    _TESTS = {
-        # TODO: Implement
+    _TESTS = [{
+        'url': 'https://acidcow.com/video/118851-learning_to_follow_patterns_with_dad.html',
+        'info_dict': {
+            'id': '118851',
+            'ext': 'mp4',
+            'title': 'learning_to_follow_patterns_with_dad',
 
-    }
+        },
+    }, {
+        'url': 'https://acidcow.com/video/116642-that_was_really_close.html',
+        'info_dict': {
+            'id': '116642',
+            'ext': 'mp4',
+            'title': 'that_was_really_close',
+        },
+    },  {
+        'url': 'https://acidcow.com/video/118850-it_reminds_me_of_cars_scene.html',
+        'info_dict': {
+            'id': '118850',
+            'ext': 'mp4',
+            'title': 'it_reminds_me_of_cars_scene',
+        }
+
+    }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
