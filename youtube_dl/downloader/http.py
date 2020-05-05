6c907eb33ffd79ef9c4761c6460acb31b2eded46
Sergey M․ <dstftw@gmail.com>
2020-05-05 21:30:27 +0700

    [downloader/http] Request last data block of exact remaining size
    Always request last data block of exact size remaining to download if possible not the current block size.

--- youtube_dl/downloader/http.py
+++ youtube_dl/downloader/http.py
@@ -227,7 +227,7 @@ class HttpFD(FileDownloader):
             while True:
                 try:
                     # Download and write
-                    data_block = ctx.data.read(block_size if not is_test else min(block_size, data_len - byte_counter))
+                    data_block = ctx.data.read(block_size if data_len is None else min(block_size, data_len - byte_counter))
                 # socket.timeout is a subclass of socket.error but may not have
                 # errno set
                 except socket.timeout as e:
