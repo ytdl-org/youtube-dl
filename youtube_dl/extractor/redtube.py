1dc339351595bed325b6983a5d5065c01e821290
dave loyall <sebboh@truth.the-good-guys.net>
2020-05-09 10:41:22 -0500

    update title regex to match current site

--- youtube_dl/extractor/redtube.py
+++ youtube_dl/extractor/redtube.py
@@ -57,7 +57,7 @@ class RedTubeIE(InfoExtractor):
 
         if not info.get('title'):
             info['title'] = self._html_search_regex(
-                (r'<h(\d)[^>]+class="(?:video_title_text|videoTitle)[^"]*">(?P<title>(?:(?!\1).)+)</h\1>',
+                (r'<h(\d)[^>]+class="(?:video_title_text|videoTitle|video_title)[^"]*">(?P<title>(?:(?!\1).)+)</h\1>',
                  r'(?:videoTitle|title)\s*:\s*(["\'])(?P<title>(?:(?!\1).)+)\1',),
                 webpage, 'title', group='title',
                 default=None) or self._og_search_title(webpage)
