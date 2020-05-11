90cb8d994ce6290bfb3f1af7398d7c6c3badec66
misha <shmelev1996@mail.ru>
2020-05-11 17:59:41 +0300

    [mailru] Fix extraction (closes #24530)

--- youtube_dl/extractor/mailru.py
+++ youtube_dl/extractor/mailru.py
@@ -128,6 +128,8 @@ class MailRuIE(InfoExtractor):
                 'http://api.video.mail.ru/videos/%s.json?new=1' % video_id,
                 video_id, 'Downloading video JSON')
 
+        video_key = self._get_cookies('https://my.mail.ru').get('video_key')
+        key = 'video_key=%s' % video_key.value
         formats = []
         for f in video_data['videos']:
             video_url = f.get('url')
@@ -140,6 +142,9 @@ class MailRuIE(InfoExtractor):
                 'url': video_url,
                 'format_id': format_id,
                 'height': height,
+                'http_headers': {
+                    'Cookie': key,
+                },
             })
         self._sort_formats(formats)
 
