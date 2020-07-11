a75f38347cb0f49f75ed4ad7ad86eea99cdedd8f
rli99 <rogerli99999@gmail.com>
2020-07-11 14:38:34 +1000

    Fix title display name

--- youtube_dl/extractor/mildom.py
+++ youtube_dl/extractor/mildom.py
@@ -39,7 +39,7 @@ class MildomIE(InfoExtractor):
         if not title:
             title = self._html_search_meta(
                 ['og:description', 'description'],
-                webpage, 'description', default=None)
+                webpage, 'title', default=None)
         thumbnail = playback_data.get('video_pic')
         if not thumbnail:
             thumbnail = self._html_search_meta(
