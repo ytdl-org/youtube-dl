b78835f3847e9b437592c5bacdd1105a96bd7a6f
Stephen Fluin <stephen.fluin@gmail.com>
2020-08-08 01:03:33 -0700

    [tiktok] rewrite for new API and website

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1143,10 +1143,7 @@ from .thisamericanlife import ThisAmericanLifeIE
 from .thisav import ThisAVIE
 from .thisoldhouse import ThisOldHouseIE
 from .threeqsdn import ThreeQSDNIE
-from .tiktok import (
-    TikTokIE,
-    TikTokUserIE,
-)
+from .tiktok import TikTokIE
 from .tinypic import TinyPicIE
 from .tmz import (
     TMZIE,
