832b943193a44c60cbf103e18600526791d07663
bhodaya <bhodaya@videocites.com>
2020-08-16 16:49:51 +0300

    fix uploader handle fb

--- test/ci/test_facebook.py
+++ test/ci/test_facebook.py
@@ -32,6 +32,7 @@ class facebookMetaData(unittest.TestCase):
         ydl = youtube_dl.YoutubeDL(params)
         info = ydl.extract_info(url, download=False)
         self.assertGreater(info.get('comment_count'), 0)
+        self.assertTrue(info.get('uploader_handle'), 0)
 
     def test_metadata_fetch_with_log_in(self):
         url = "https://www.facebook.com/oristandup/videos/675360549895283"
