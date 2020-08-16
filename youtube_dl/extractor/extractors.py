b50f352f59ecb1de8261a47fac32c03b9bcbd594
sxvghd <sxvghd@firemail.cc>
2020-08-16 15:40:59 +0200

    [doodstream] new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -293,6 +293,7 @@ from .discoverynetworks import DiscoveryNetworksDeIE
 from .discoveryvr import DiscoveryVRIE
 from .disney import DisneyIE
 from .dispeak import DigitallySpeakingIE
+from .doodstream import DoodStreamIE
 from .dropbox import DropboxIE
 from .dw import (
     DWIE,
