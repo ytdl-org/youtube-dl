5bda5abae832b57d5dcb7040ef333ef3544c18ee
Tony Izzo <anthony.w.izzo@gmail.com>
2020-09-02 18:52:17 -0400

    Add support for FuboTV as AdobePass provider

--- youtube_dl/extractor/adobepass.py
+++ youtube_dl/extractor/adobepass.py
@@ -20,6 +20,11 @@ from ..utils import (
 
 
 MSO_INFO = {
+    'FuboTV': {
+        'name': 'FuboTV',
+        'username_field': 'username',
+        'password_field': 'password',
+    },
     'DTV': {
         'name': 'DIRECTV',
         'username_field': 'username',
