139cb94276d41750f168219ab171b4d28e717b65
Unknown <blackjack4494@web.de>
2020-09-03 05:38:02 +0200

    flake8 hotfix

--- devscripts/make_contributing.py
+++ devscripts/make_contributing.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 
-import io
+# import io
 import optparse
-import re
+# import re
 
 
 def main():
@@ -11,23 +11,23 @@ def main():
     options, args = parser.parse_args()
     if len(args) != 2:
         parser.error('Expected an input and an output filename')
-'''
-    infile, outfile = args
+
+
+"""     infile, outfile = args
 
     with io.open(infile, encoding='utf-8') as inf:
         readme = inf.read()
 
-    bug_text = re.search(
-        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
-    dev_text = re.search(
-        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING youtube-dlc',
-        readme).group(1)
+    bug_text = re.search( """
+# r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
+# dev_text = re.search(
+# r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING youtube-dlc',
+"""         readme).group(1)
 
     out = bug_text + dev_text
 
     with io.open(outfile, 'w', encoding='utf-8') as outf:
-        outf.write(out)
-'''
+        outf.write(out) """
 
 if __name__ == '__main__':
     main()
