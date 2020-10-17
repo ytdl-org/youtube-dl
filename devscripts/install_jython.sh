acbafdf190b92d9c24764c287cd4905ba6095be9
Maksymilian Potok <makspotok@gmail.com>
2020-10-17 15:29:02 +0200

    fix installing/hardcoding

--- devscripts/install_jython.sh
+++ devscripts/install_jython.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-
-wget http://central.maven.org/maven2/org/python/jython-installer/2.7.1/jython-installer-2.7.1.jar
-java -jar jython-installer-2.7.1.jar -s -d "$HOME/jython"
+ 
+version="jython-installer-2.7.2.jar"
+url="https://repo1.maven.org/maven2/org/python/jython-installer/2.7.2/jython-installer-2.7.2.jar"
+pkg="java"
+pkg_ok=$(dpkg-query -W --showformat='${Status}\n' $pkg | grep "install ok installed")
+ 
+echo "Downloading source"
+#wget $url
+echo "Installing jython"
+if [ "" = "$pkg_ok" ]; then 
+	echo "$pkg is not installed. Setting this up now."
+	apt-get update
+	apt-get install openjdk-8-jdk
+	java - version
+fi
+java -jar $version -s -d "$HOME/jython"
+echo "Installing nose"
 $HOME/jython/bin/jython -m pip install nose
