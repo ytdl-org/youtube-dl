9acbf774dc04b886efcf69077e3dd60791247bf0
Joshua Lochner <admin@xenova.com>
2020-11-23 15:50:20 +0200

    [MedalTV] Formatting and other changes

--- youtube_dl/extractor/medaltv.py
+++ youtube_dl/extractor/medaltv.py
@@ -45,63 +45,78 @@ class MedalTVIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        hydration_data = self._search_regex(r'<script>.*hydrationData\s*=\s*({.+?})\s*</script>', webpage, 'hydration data', default='{}')
-        parsed = self._parse_json(hydration_data, video_id)
+        hydration_data = self._search_regex(
+            r'<script[^>]*>\s*(?:var\s*)?hydrationData\s*=\s*({.+?})\s*</script>',
+            webpage, 'hydration data', default='{}', fatal=True)
+        parsed = self._parse_json(hydration_data, video_id, fatal=True)
 
-        # This is necessary because the id of the author is not known in advance.
-        # Will not raise an issue if no profile can be found as this is optional.
-        author_info = try_get(parsed.get('profiles'), lambda x: list(x.values())[0], dict) or {}
-        author_url = 'https://medal.tv/users/{}'.format(author_info.get('id')) if author_info.get('id') else None
-
-        clip_info = try_get(parsed.get('clips'), lambda x: x.get(video_id), dict) or {}
+        clip_info = try_get(parsed, lambda x: x['clips'][video_id], dict) or {}
         if(not clip_info):
-            raise ExtractorError('Could not find video information.', video_id=video_id)
+            raise ExtractorError('Could not find video information.',
+                                 video_id=video_id)
 
-        if('error' in clip_info):
-            if(clip_info.get('error') == 404):
-                raise ExtractorError('That clip does not exist.', expected=True, video_id=video_id)
+        error = clip_info.get('error')
+        if(error):
+            if(error == 404):
+                raise ExtractorError('That clip does not exist.',
+                                     expected=True, video_id=video_id)
             else:
-                raise ExtractorError('An unknown error occurred ({}).'.format(clip_info.get('error')), video_id=video_id)
+                raise ExtractorError('An unknown error occurred ({0}).'.format(error),
+                                     video_id=video_id)
 
-        source_aspect_ratio = try_get(clip_info, lambda x: x.get('sourceWidth') / x.get('sourceHeight'), float) or (16 / 9)
+        # Necessary because the id of the author is not known in advance.
+        # Won't raise an issue if no profile can be found as this is optional.
+        author_info = try_get(parsed,
+                              lambda x: list(x['profiles'].values())[0], dict
+                              ) or {}
+        author_id = author_info.get('id')
+        author_url = 'https://medal.tv/users/{0}'.format(author_id) if author_id else None
 
-        # ordered from worst to best quality
+        width = int_or_none(clip_info.get('sourceWidth'))
+        height = int_or_none(clip_info.get('sourceHeight'))
+
+        aspect_ratio = (width / height) if(width and height) else (16 / 9)
+
+        # ordered from lowest to highest resolution
         heights = (144, 240, 360, 480, 720, 1080)
 
         formats = []
         thumbnails = []
 
         for height in heights:
-            format_key = '{}p'.format(height)
-            video_key = 'contentUrl{}'.format(format_key)
-            thumbnail_key = 'thumbnail{}'.format(format_key)
-            width = int(round(source_aspect_ratio * height))
+            format_key = '{0}p'.format(height)
+            video_key = 'contentUrl{0}'.format(format_key)
+            thumbnail_key = 'thumbnail{0}'.format(format_key)
+            width = int(round(aspect_ratio * height))
 
             # Second condition needed as sometimes medal says
             # they have a format when in fact it is another format.
-            if(video_key in clip_info and format_key in clip_info.get(video_key)):
+            format_url = clip_info.get(video_key)
+            if(format_url and format_key in format_url):
                 formats.append({
-                    'url': clip_info.get(video_key),
+                    'url': format_url,
                     'format_id': format_key,
                     'width': width,
                     'height': height
                 })
 
-            if(thumbnail_key in clip_info and format_key in clip_info.get(thumbnail_key)):
+            thumbnail_url = clip_info.get(thumbnail_key)
+            if(thumbnail_url and format_key in thumbnail_url):
                 thumbnails.append({
                     'id': format_key,
-                    'url': clip_info.get(thumbnail_key),
+                    'url': thumbnail_url,
                     'width': width,
                     'height': height
                 })
 
         # add source to formats
-        if('contentUrl' in clip_info):
+        source_url = clip_info.get('contentUrl')
+        if(source_url):
             formats.append({
-                'url': clip_info.get('contentUrl'),
+                'url': source_url,
                 'format_id': 'source',
-                'width': clip_info.get('sourceWidth'),
-                'height': clip_info.get('sourceHeight')
+                'width': width,
+                'height': height
             })
 
         return {
@@ -113,7 +128,7 @@ class MedalTVIE(InfoExtractor):
 
             'uploader': author_info.get('displayName'),
             'timestamp': float_or_none(clip_info.get('created'), 1000),
-            'uploader_id': author_info.get('id'),
+            'uploader_id': author_id,
             'uploader_url': author_url,
 
             'duration': float_or_none(clip_info.get('videoLengthSeconds')),
