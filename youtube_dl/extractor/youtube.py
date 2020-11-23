323427281899cf3527e504ac7058f5b10aad5d61
Sergey M․ <dstftw@gmail.com>
2020-11-23 22:34:27 +0700

    [youtube:tab] PEP 8

--- youtube_dl/extractor/youtube.py
+++ youtube_dl/extractor/youtube.py
@@ -2621,11 +2621,11 @@ class YoutubeTabIE(YoutubeBaseInfoExtractor):
         'url': 'https://www.youtube.com/c/CommanderVideoHq/live',
         'only_matching': True,
     },
-    # TODO
-    # {
-    #     'url': 'https://www.youtube.com/TheYoungTurks/live',
-    #     'only_matching': True,
-    # }
+        # TODO
+        # {
+        #     'url': 'https://www.youtube.com/TheYoungTurks/live',
+        #     'only_matching': True,
+        # }
     ]
 
     def _extract_channel_id(self, webpage):
