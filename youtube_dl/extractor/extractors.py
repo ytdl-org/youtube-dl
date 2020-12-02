14bedfc938d9cba043e6aa79e43a7ad16343164a
Ashutosh Chaudhary <216.ashuttosh@gmail.com>
2020-12-03 04:15:26 +0530

    Added Mxplayer
    Fixed regex

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -666,6 +666,7 @@ from .mtv import (
 )
 from .muenchentv import MuenchenTVIE
 from .mwave import MwaveIE, MwaveMeetGreetIE
+from .mxplayer import MxplayerIE
 from .mychannels import MyChannelsIE
 from .myspace import MySpaceIE, MySpaceAlbumIE
 from .myspass import MySpassIE
