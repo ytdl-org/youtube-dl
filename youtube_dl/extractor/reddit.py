76fe4ba3b22a689ba7b5c6ea9064796ce1f4af55
Trevor Nelson <25140503+trevnels@users.noreply.github.com>
2020-12-17 11:21:36 -0500

    [redditr] Extract duration (#27426)

--- youtube_dl/extractor/reddit.py
+++ youtube_dl/extractor/reddit.py
@@ -7,6 +7,7 @@ from ..utils import (
     ExtractorError,
     int_or_none,
     float_or_none,
+    try_get,
     url_or_none,
 )
 
@@ -59,6 +60,7 @@ class RedditRIE(InfoExtractor):
             'timestamp': 1501941939,
             'upload_date': '20170805',
             'uploader': 'Antw87',
+            'duration': 12,
             'like_count': int,
             'dislike_count': int,
             'comment_count': int,
@@ -123,6 +125,10 @@ class RedditRIE(InfoExtractor):
             'thumbnail': url_or_none(data.get('thumbnail')),
             'timestamp': float_or_none(data.get('created_utc')),
             'uploader': data.get('author'),
+            'duration': int_or_none(try_get(
+                data,
+                (lambda x: x['media']['reddit_video']['duration'],
+                 lambda x: x['secure_media']['reddit_video']['duration']))),
             'like_count': int_or_none(data.get('ups')),
             'dislike_count': int_or_none(data.get('downs')),
             'comment_count': int_or_none(data.get('num_comments')),
