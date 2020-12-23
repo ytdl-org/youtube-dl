f4ce9bb132c72f8ba31f5d16a2b537f988bc9c7c
kirill <kirill.okhotnikov@gmail.com>
2020-12-23 05:30:09 +0100

    New extractor for player.pl

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -868,6 +868,7 @@ from .platzi import (
     PlatziIE,
     PlatziCourseIE,
 )
+from .playerpl import PlayerPlIE
 from .playfm import PlayFMIE
 from .playplustv import PlayPlusTVIE
 from .plays import PlaysTVIE
