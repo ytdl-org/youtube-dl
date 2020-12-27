794771a164009ff94046c98d8a7d45f7706547af
JamKage <JamKage@users.noreply.github.com>
2020-12-27 17:36:21 +0000

    [go] Added support for FXNetworks (#26826)
    Co-authored-by: James Kirrage <james.kirrage@mortgagegym.com>
    
    closes #13972
    closes #22467
    closes #23754

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -399,7 +399,6 @@ from .fujitv import FujiTVFODPlus7IE
 from .funimation import FunimationIE
 from .funk import FunkIE
 from .fusion import FusionIE
-from .fxnetworks import FXNetworksIE
 from .gaia import GaiaIE
 from .gameinformer import GameInformerIE
 from .gamespot import GameSpotIE
