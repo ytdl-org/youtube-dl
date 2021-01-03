8522bcd97c4173407261a3fa0283dd7800c39601
Remita Amine <remitamine@gmail.com>
2021-01-03 12:12:06 +0100

    [stitcher] Add support for shows and show metadata extraction(closes #20510)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1092,7 +1092,10 @@ from .spike import (
     BellatorIE,
     ParamountNetworkIE,
 )
-from .stitcher import StitcherIE
+from .stitcher import (
+    StitcherIE,
+    StitcherShowIE,
+)
 from .sport5 import Sport5IE
 from .sportbox import SportBoxIE
 from .sportdeutschland import SportDeutschlandIE
