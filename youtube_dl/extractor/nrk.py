8a3797a4abdc0b63b6fcbd1fbc9d81acc57ec142
Remita Amine <remitamine@gmail.com>
2021-01-04 12:16:54 +0100

    [nrk] fix extraction for videos without a legalAge rating

--- youtube_dl/extractor/nrk.py
+++ youtube_dl/extractor/nrk.py
@@ -223,12 +223,12 @@ class NRKIE(NRKBaseIE):
         legal_age = try_get(
             data, lambda x: x['legalAge']['body']['rating']['code'], compat_str)
         # https://en.wikipedia.org/wiki/Norwegian_Media_Authority
-        if legal_age == 'A':
-            age_limit = 0
-        elif legal_age.isdigit():
-            age_limit = int_or_none(legal_age)
-        else:
-            age_limit = None
+        age_limit = None
+        if legal_age:
+            if legal_age == 'A':
+                age_limit = 0
+            elif legal_age.isdigit():
+                age_limit = int_or_none(legal_age)
 
         is_series = try_get(data, lambda x: x['_links']['series']['name']) == 'series'
 
