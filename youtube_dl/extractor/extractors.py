419fa5584870706bd250ab99f129d539c2d5201f
Isaac-the-Man <steven97102@gmail.com>
2021-01-10 10:37:54 -0500

    [samplefocus] Add new extractor(closes #27763)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1033,6 +1033,7 @@ from .safari import (
     SafariApiIE,
     SafariCourseIE,
 )
+from .samplefocus import SampleFocusIE
 from .sapo import SapoIE
 from .savefrom import SaveFromIE
 from .sbs import SBSIE
