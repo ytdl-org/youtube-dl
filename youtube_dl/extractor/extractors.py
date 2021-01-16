dbd5c3f14cf6e1138d13c2ee86016a7b2d652024
Remita Amine <remitamine@gmail.com>
2021-01-16 18:12:05 +0100

    [spotify] Add new extractor for Spotify Podcasts(closes #27443)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1120,6 +1120,10 @@ from .stitcher import (
 from .sport5 import Sport5IE
 from .sportbox import SportBoxIE
 from .sportdeutschland import SportDeutschlandIE
+from .spotify import (
+    SpotifyIE,
+    SpotifyShowIE,
+)
 from .spreaker import (
     SpreakerIE,
     SpreakerPageIE,
