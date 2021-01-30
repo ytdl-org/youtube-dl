4277082394d56523eaf56cc648ebb471df88a1ff
Isaac-the-Man <steven97102@gmail.com>
2021-01-30 16:32:14 -0500

    [samplefocus] fixed coding convention

--- youtube_dl/extractor/samplefocus.py
+++ youtube_dl/extractor/samplefocus.py
@@ -2,14 +2,10 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..utils import (
-    get_element_by_class,
-    extract_attributes,
-    get_element_by_id)
 
 
 class SampleFocusIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?samplefocus\.com/samples/(?P<id>[\w,\-,0-9]+)'
+    _VALID_URL = r'https?://(?:www\.)?samplefocus\.com/samples/(?P<id>[^/?&#]+)'
     _TESTS = [{
         'url': 'https://samplefocus.com/samples/lil-peep-sad-emo-guitar',
         'md5': '48c8d62d60be467293912e0e619a5120',
@@ -18,7 +14,7 @@ class SampleFocusIE(InfoExtractor):
             'ext': 'mp3',
             'title': 'Lil Peep Sad Emo Guitar',
             'description': 'Listen to Lil Peep Sad Emo Guitar. Royalty-Free sound that is tagged as electric guitar, emo, guitar, and lil peep. Download for FREE + discover 1000\'s of sounds.',
-            'thumbnail': r're:^https?://.*\.png',
+            'thumbnail': r're:^https?://.+\.png',
             'license': 'Standard License'
         }
     }, {
@@ -34,11 +30,16 @@ class SampleFocusIE(InfoExtractor):
         webpage = self._download_webpage(url, video_id)
 
         title = self._og_search_title(webpage) or self._html_search_regex(r'<h1>(.+?)</h1>', webpage, 'title', default=video_id)
-        tb = self._og_search_thumbnail(webpage) or extract_attributes(get_element_by_class('waveform')).get('src')
 
         mp3_url = self._html_search_regex(
-            r'<meta itemprop="contentUrl" content="?(.+?)"?>',
-            webpage, 'mp3 url', fatal=False) or extract_attributes(get_element_by_id('sample_mp3')).get('value')
+            r'<input type="hidden" name="sample_mp3" id="sample_mp3" value="(.+\.mp3\?[0-9]+)" class="sample-mp3" />',
+            webpage, 'mp3', fatal=False) or self._html_search_regex(
+            r'<meta itemprop="contentUrl" content="?(.+\.mp3\?[0-9]+)"?>',
+            webpage, 'mp3 url')
+
+        tb = self._og_search_thumbnail(webpage) or self._html_search_regex(
+            r'<img class="waveform" style="background-color: #(?:[\w,0-9]{6})" src="(.+?)" />',
+            webpage, 'mp3', fatal=False)
 
         return {
             'id': video_id,
@@ -46,6 +47,6 @@ class SampleFocusIE(InfoExtractor):
             'url': mp3_url,
             'ext': 'mp3',
             'thumbnail': tb,
-            'description': self._html_search_meta('description', webpage, fatal=False),
+            'description': self._html_search_meta('description', webpage),
             'license': self._html_search_regex(r'<a href="/license">(.+?)</a>', webpage, 'license')
         }
