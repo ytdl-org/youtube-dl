7f8b8bc418b8831ea1c2ae8de64e3bf0e8b707f8
Remita Amine <remitamine@gmail.com>
2021-02-08 15:56:42 +0100

    [ign] fix extraction(closes #24771)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -470,8 +470,8 @@ from .hungama import (
 from .hypem import HypemIE
 from .ign import (
     IGNIE,
-    OneUPIE,
-    PCMagIE,
+    IGNVideoIE,
+    IGNArticleIE,
 )
 from .iheart import (
     IHeartRadioIE,
