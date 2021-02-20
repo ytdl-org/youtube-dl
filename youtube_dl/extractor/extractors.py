2bbe77d4847ce37ddaf29c9668456c36509c36ed
guredora <contact@guredora.com>
2021-02-21 03:46:49 +0900

    [NhkRadio] add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -770,6 +770,10 @@ from .nhk import (
     NhkVodIE,
     NhkVodProgramIE,
 )
+from .nhkRadio import (
+    NhkRadioIE,
+    NhkRadioProgramIE
+)
 from .nhl import NHLIE
 from .nick import (
     NickIE,
