ee3c3aec1720e2bf1ec500153970922bd677aa3d
Carlos Cardenas <calberto@umich.edu>
2021-04-24 17:55:24 -0400

    [Likee] Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -590,6 +590,10 @@ from .lifenews import (
     LifeNewsIE,
     LifeEmbedIE,
 )
+from .likee import (
+    LikeeIE,
+    LikeeUserIE,
+)
 from .limelight import (
     LimelightMediaIE,
     LimelightChannelIE,
