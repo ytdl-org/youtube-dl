6b7b90cebe12fc81405e62e54f92cc48a9c1a413
nuruvilu <nuruvilu@konpaku.tech>
2021-05-09 23:10:44 +0000

    [myinstants] Add new extractor

--- youtube_dl/extractor/myinstants.py
+++ youtube_dl/extractor/myinstants.py
@@ -2,10 +2,15 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..utils import determine_ext, int_or_none
+from ..utils import (
+    determine_ext,
+    str_to_int,
+    url_or_none
+)
 
 
 class MyinstantsIE(InfoExtractor):
+    IE_NAME = 'myinstants'
     _VALID_URL = r'https?://(?:www\.)?myinstants\.com/instant/(?P<id>[a-z0-9\-]+)'
     _TESTS = [{
         'url': 'https://www.myinstants.com/instant/nani-sore/',
@@ -41,25 +46,39 @@ class MyinstantsIE(InfoExtractor):
         title = self._html_search_regex(
             r'<h1[^>]+id=(["\'])instant-page-title\1[^>]*>(?P<title>[^<]+)',
             webpage, 'title', group='title')
-        uploader = self._search_regex(r'<div>Uploaded by <a href="/profile/([^/]+)/">', webpage, 'uploader', fatal=False)
-        file_url = self._og_search_property('audio', webpage)
+
+        uploader = self._search_regex(
+            r'<div[^>]*>Uploaded by <a[^>]+href=(["\'])/profile/(?P<uploader>.+?)/\1[^>]*>',
+            webpage, 'uploader', group='uploader', fatal=False)
+
+        file_url = url_or_none(self._og_search_property('audio', webpage, default=None))
+        if not file_url:
+            file_url = 'https://www.myinstants.com' + self._search_regex([
+                r'<div[^>]+id=(["\'])instant-page-button\1[^>]+onmousedown=(["\'])play\((?!\2)(["\'])(?P<file>.+?)\3\)\2[^>]*>',
+                r'<a[^>]+href=(["\'])(?P<file>.+?)\1[^>]+download[^>]*>',
+                r'<a[^>]+title=(["\'])Share to Whatsapp\1[^>]+onclick=(["\'])shareAudioMessage\((?!\2)(["\'])(?P<file>.+?)\3.+?\)\2[^>]*>'
+            ], webpage, 'file', group='file')
 
         return {
             'id': video_id,
             'ext': determine_ext(file_url),
             'title': title,
             'url': file_url,
-            'thumbnail': self._og_search_property('image', webpage, fatal=False),
+            'thumbnail': url_or_none(self._html_search_meta(
+                ['og:image', 'twitter:image'], webpage, fatal=False)),
             'uploader': uploader,
             'uploader_id': uploader,
             'uploader_url': 'https://www.myinstants.com/profile/' + uploader,
-            'view_count': int_or_none(self._search_regex(
-                r'<div>Uploaded by <a href="/profile/(?P<user>[^/]+)/">(?P<user2>[^/]+)</a> - (?P<views>[0-9,]+) views',
-                webpage, 'views', fatal=False)),
-            'like_count': int_or_none(self._search_regex(
-                r'<b>([0-9,]+) users</b>\sfavorited this sound button',
-                webpage, 'likes', fatal=False)),
+
+            'view_count': str_to_int(self._search_regex(
+                r'<div[^>]*>Uploaded by <a[^>]+href=(["\'])/profile/(.+?)/\1[^>]*>\2</a> - (?P<views>[0-9,]+) views',
+                webpage, 'views', group='views', fatal=False)),
+
+            'like_count': str_to_int(self._search_regex(
+                r'<b[^>]*>(?P<likes>[0-9,]+) users</b>\sfavorited this sound button',
+                webpage, 'likes', group='likes', fatal=False)),
+
             'categories': [self._html_search_regex(
-                r'<a href="/categories/(?P<caturl>[^/]+)/" title="(?P<cat>.+)">',
-                webpage, 'cat', group='cat', fatal=False)]
+                r'<a[^>]+href=(["\'])/categories/(.+?)/\1[^>]+title=(["\'])(?P<category>.+?)\3[^>]*>',
+                webpage, 'category', group='category', fatal=False)]
         }
