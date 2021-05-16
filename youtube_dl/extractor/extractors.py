a581a554044ae7224481dca2ac370b9425fe9466
Ratchanan Srirattanamet <peathot@hotmail.com>
2021-05-17 02:51:26 +0700

    [bigo] Add extractor (closes #18357)

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -114,6 +114,7 @@ from .bfmtv import (
 )
 from .bibeltv import BibelTVIE
 from .bigflix import BigflixIE
+from .bigo import BigoIE
 from .bild import BildIE
 from .bilibili import (
     BiliBiliIE,
