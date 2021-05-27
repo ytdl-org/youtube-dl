8ce25b2b265db05d42b67e2dabe1f101e85906ce
Ferdinand Bachmann <theferdi265@gmail.com>
2021-05-27 21:56:57 +0200

    [tugraz_tube] switch to non-deprecated info-dict layout

--- youtube_dl/extractor/tugraz_tube.py
+++ youtube_dl/extractor/tugraz_tube.py
@@ -1,6 +1,10 @@
-import re
+# coding: utf-8
+from __future__ import unicode_literals
+
 from .common import InfoExtractor
 from ..utils import ExtractorError, urlencode_postdata
+
+import re
 
 class TugrazTubeIE(InfoExtractor):
     IE_DESC = 'tube.tugraz.at'
@@ -63,7 +67,7 @@ class TugrazTubeIE(InfoExtractor):
         elif match.group('episode') is not None:
             return self._extract_episode(id)
         else:
-            return []
+            raise ExtractorError("No video found on page")
 
     def _get_series_info(self, id):
         data = self._download_json(
@@ -78,6 +82,9 @@ class TugrazTubeIE(InfoExtractor):
         )
 
         return data["catalogs"][0]["http://purl.org/dc/terms/"]
+
+    def _get_series_id(self, info):
+        return info["identifier"][0]["value"]
 
     def _get_series_title(self, info):
         return info["title"][0]["value"]
@@ -108,7 +115,7 @@ class TugrazTubeIE(InfoExtractor):
             }
         )
 
-        return data["search-results"]["result"][0]
+        return data["search-results"]["result"]
 
     def _get_episode_id(self, info):
         return info["id"]
@@ -123,23 +130,29 @@ class TugrazTubeIE(InfoExtractor):
         *_, ext = self._get_episode_download_url(info).rsplit('.', 1)
         return ext
 
+    def _build_series_info_dict(self, info, episodes):
+        return {
+            "_type": "playlist",
+            "id": self._get_series_id(info),
+            "title": self._get_series_title(info),
+            "entries": [
+                self._build_episode_info_dict(episode) for episode in episodes
+            ]
+        }
+
+    def _build_episode_info_dict(self, info):
+        return {
+            "id": self._get_episode_id(info),
+            "title": self._get_episode_title(info),
+            "url": self._get_episode_download_url(info),
+            "ext": self._get_episode_download_ext(info),
+        }
+
     def _extract_series(self, id):
         info = self._get_series_info(id)
         episodes = self._get_series_episode_info(id)
-
-        return [{
-            "id": self._get_episode_id(episode),
-            "url": self._get_episode_download_url(episode),
-            "ext": self._get_episode_download_ext(episode),
-            "title": self._get_episode_title(episode)
-        } for episode in episodes]
+        return self._build_series_info_dict(info, episodes)
 
     def _extract_episode(self, id):
         episode = self._get_episode_info(id)
-
-        return [{
-            "id": self._get_episode_id(episode),
-            "url": self._get_episode_download_url(episode),
-            "ext": self._get_episode_download_ext(episode),
-            "title": self._get_episode_title(episode)
-        }]
+        return self._build_episode_info_dict(episode)
