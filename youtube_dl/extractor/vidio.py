9f9c30a701c17830a711dbceba8043957a59d5c6
MinePlayersPE <mineplayerspealt@gmail.com>
2021-05-31 04:42:23 +0700

    [vidio] Support logins & premier-exclusive videos

--- youtube_dl/extractor/vidio.py
+++ youtube_dl/extractor/vidio.py
@@ -5,11 +5,14 @@ import re
 
 from .common import InfoExtractor
 from ..utils import (
+    ExtractorError,
+    get_element_by_class,
     int_or_none,
     parse_iso8601,
     str_or_none,
     strip_or_none,
     try_get,
+    urlencode_postdata,
 )
 
 
@@ -41,11 +44,49 @@ class VidioIE(InfoExtractor):
     }, {
         'url': 'https://www.vidio.com/watch/77949-south-korea-test-fires-missile-that-can-strike-all-of-the-north',
         'only_matching': True,
+    }, {
+        # Premier-exclusive video
+        'url': 'https://www.vidio.com/watch/1550718-stand-by-me-doraemon',
+        'only_matching': True
     }]
+    _LOGIN_URL = 'https://www.vidio.com/users/login'
+    _NETRC_MACHINE = 'vidio'
+
+    def _login(self):
+        username, password = self._get_login_info()
+        if username is None:
+            return
+
+        def is_logged_in():
+            res = self._download_json(
+                'https://www.vidio.com/interactions.json', None, 'Checking if logged in', fatal=False) or {}
+            return bool(res.get('current_user'))
+
+        if is_logged_in():
+            return
+
+        login_page = self._download_webpage(
+            self._LOGIN_URL, None, 'Downloading log in page')
+
+        login_form = self._form_hidden_inputs("login-form", login_page)
+        login_form.update({
+            'user[login]': username,
+            'user[password]': password,
+        })
+        login_post, login_post_urlh = self._download_webpage_handle(
+            self._LOGIN_URL, None, 'Logging in', data=urlencode_postdata(login_form), expected_status=[302, 401])
+
+        if login_post_urlh.status == 401:
+            reason = get_element_by_class('onboarding-form__general-error', login_post)
+            if reason:
+                raise ExtractorError(
+                    'Unable to log in: %s' % reason, expected=True)
+            raise ExtractorError('Unable to log in')
 
     def _real_initialize(self):
         self._api_key = self._download_json(
             'https://www.vidio.com/auth', None, data=b'')['api_key']
+        self._login()
 
     def _real_extract(self, url):
         video_id, display_id = re.match(self._VALID_URL, url).groups()
@@ -56,9 +97,28 @@ class VidioIE(InfoExtractor):
             })
         video = data['videos'][0]
         title = video['title'].strip()
+        is_premium = video.get('is_premium')
+        if is_premium:
+            sources = self._download_json(
+                'https://www.vidio.com/interactions_stream.json?video_id=%s&type=videos' % video_id,
+                display_id, note='Downloading premier API JSON')
+            if not (sources.get('source') or sources.get('source_dash')):
+                self.raise_login_required()
 
-        formats = self._extract_m3u8_formats(
-            data['clips'][0]['hls_url'], display_id, 'mp4', 'm3u8_native')
+            formats = []
+            if sources.get('source'):
+                hls_formats = self._extract_m3u8_formats(
+                    sources['source'], display_id, 'mp4', 'm3u8_native')
+                formats.extend(hls_formats)
+            if sources.get('source_dash'):  # TODO: Find video example with source_dash
+                dash_formats = self._extract_mpd_formats(
+                    sources['source_dash'], display_id, 'dash')
+                formats.extend(dash_formats)
+        else:
+            hls_url = data['clips'][0]['hls_url']
+            formats = self._extract_m3u8_formats(
+                hls_url, display_id, 'mp4', 'm3u8_native')
+
         self._sort_formats(formats)
 
         get_first = lambda x: try_get(data, lambda y: y[x + 's'][0], dict) or {}
