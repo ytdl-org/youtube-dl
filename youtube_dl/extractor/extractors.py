ff2e049d3e0ef8fdbbe15f3c5f6845cf63ab01b9
MinePlayersPE <mineplayerspealt@gmail.com>
2021-06-06 10:23:30 +0700

    [vidio] Revamp

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1429,7 +1429,11 @@ from .videomore import (
     VideomoreSeasonIE,
 )
 from .videopress import VideoPressIE
-from .vidio import VidioIE
+from .vidio import (
+    VidioIE,
+    VidioPremierIE,
+    VidioLiveIE,
+)
 from .vidlii import VidLiiIE
 from .vidme import (
     VidmeIE,
