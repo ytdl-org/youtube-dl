2065fe56de81a7ead34b04b954d63f05525f7485
Alexandre L <alex131089+git@gmail.com>
2021-06-12 02:30:27 +0200

    Reimplement MKV thumbnails embedding from https://github.com/ytdl-org/youtube-dl/pull/15445

--- youtube_dl/postprocessor/embedthumbnail.py
+++ youtube_dl/postprocessor/embedthumbnail.py
@@ -42,6 +42,10 @@ class EmbedThumbnailPP(FFmpegPostProcessor):
                 'Skipping embedding the thumbnail because the file is missing.')
             return [], info
 
+        # Early exit if the container is not supported
+        if info['ext'] not in ['mp3', 'm4a', 'mp4', 'mkv']:
+            raise EmbedThumbnailPPError('Only mp3, m4a/mp4 and mkv are supported for thumbnail embedding for now.')
+
         def is_webp(path):
             with open(encodeFilename(path), 'rb') as f:
                 b = f.read(12)
@@ -74,20 +78,45 @@ class EmbedThumbnailPP(FFmpegPostProcessor):
             os.rename(encodeFilename(escaped_thumbnail_jpg_filename), encodeFilename(thumbnail_jpg_filename))
             thumbnail_filename = thumbnail_jpg_filename
 
-        if info['ext'] == 'mp3':
-            options = [
-                '-c', 'copy', '-map', '0', '-map', '1',
-                '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (Front)"']
+        # Uses ffmpeg
+        if info['ext'] in ['mp3', 'mkv']:
+            if info['ext'] == 'mp3':
+                options = [
+                    '-c', 'copy', '-map', '0', '-map', '1',
+                    '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (Front)"']
+                input_paths = [filename, thumbnail_filename]
+            elif info['ext'] == 'mkv':
+                if thumbnail_filename.endswith('.png'):
+                    mimetype = 'image/png'
+                    extension = 'png'
+                else:
+                    mimetype = 'image/jpeg'
+                    extension = 'jpg'
+
+                options = [
+                    '-c', 'copy',
+                    '-map', '0',
+                    '-attach', thumbnail_filename,
+                    # https://matroska.org/technical/cover_art/index.html as pointed in #6046
+                    # No orientation detection nor dimensions checking/convertion
+                    '-metadata:s:t', 'filename=cover_land.%s' % extension,
+                    # If not given : "[matroska @ 000001458de38840] Attachment stream 2 has no mimetype tag and it cannot be deduced from the codec id."
+                    '-metadata:s:t', 'mimetype=%s' % mimetype,
+                    # Use metadata "title" so it is set as MATROSKA_ID_FILEDESC - optional
+                    # https://github.com/FFmpeg/FFmpeg/blob/9cfdf0e3322b9a451277cf36406ac4a8e4e3da74/libavformat/matroskaenc.c#L1762
+                    '-metadata:s:t', 'title=Thumbnail']
+                input_paths = [filename]
 
             self._downloader.to_screen('[ffmpeg] Adding thumbnail to "%s"' % filename)
 
-            self.run_ffmpeg_multiple_files([filename, thumbnail_filename], temp_filename, options)
+            self.run_ffmpeg_multiple_files(input_paths, temp_filename, options)
 
             if not self._already_have_thumbnail:
                 os.remove(encodeFilename(thumbnail_filename))
             os.remove(encodeFilename(filename))
             os.rename(encodeFilename(temp_filename), encodeFilename(filename))
 
+        # Uses AtomicParsley
         elif info['ext'] in ['m4a', 'mp4']:
             atomicparsley = next((x
                                   for x in ['AtomicParsley', 'atomicparsley']
@@ -124,7 +153,5 @@ class EmbedThumbnailPP(FFmpegPostProcessor):
             else:
                 os.remove(encodeFilename(filename))
                 os.rename(encodeFilename(temp_filename), encodeFilename(filename))
-        else:
-            raise EmbedThumbnailPPError('Only mp3 and m4a/mp4 are supported for thumbnail embedding for now.')
 
         return [], info
