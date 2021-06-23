fba9258cce9bcdc1cb04c9ef56112b8a76851e9f
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-06-22 21:24:18 -0400

    [PATCH] [vocaroo] Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1475,6 +1475,7 @@ from .vlive import (
     VLivePostIE,
     VLiveChannelIE,
 )
+from .vocaroo import VocarooIE
 from .vodlocker import VodlockerIE
 from .vodpl import VODPlIE
 from .vodplatform import VODPlatformIE
