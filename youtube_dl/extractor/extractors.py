b9b3231316b032c7d43bfd4f4493a0cfe8ffbe43
God <genocidalstudios@gmail.com>
2021-07-26 15:06:45 -0400

    [piczel] Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -907,6 +907,7 @@ from .picarto import (
     PicartoIE,
     PicartoVodIE,
 )
+from .piczel import PiczelIE
 from .piksel import PikselIE
 from .pinkbike import PinkbikeIE
 from .pinterest import (
