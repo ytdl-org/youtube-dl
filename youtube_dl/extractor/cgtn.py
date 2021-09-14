d6903eb12d276213094ff0f4d5dc6a796f176189
chao813 <chaoo.yuan@gmail.com>
2021-09-14 20:35:14 +0800

    add description, category, author, and timestamp to info

--- youtube_dl/extractor/cgtn.py
+++ youtube_dl/extractor/cgtn.py
@@ -1,8 +1,12 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-from .common import InfoExtractor
+import datetime
 
+from .common import InfoExtractor
+from ..utils import (
+    unified_timestamp,
+)
 
 class CGTNIE(InfoExtractor):
     _VALID_URL = r'https?://news\.cgtn\.com/news/[0-9]{4}-[0-9]{2}-[0-9]{2}/[a-zA-Z0-9-]+-(?P<id>[a-zA-Z0-9-]+)/index\.html'
@@ -14,6 +18,8 @@ class CGTNIE(InfoExtractor):
                 'ext': 'mp4',
                 'title': 'Up and Out of Poverty Ep. 1: A solemn promise',
                 'thumbnail': r're:^https?://.*\.jpg$',
+                'timestamp': 1615295940,
+                'upload_date': '20210309',
             }
         },
         {
@@ -23,6 +29,11 @@ class CGTNIE(InfoExtractor):
                 'ext': 'mp4',
                 'title': 'China, Indonesia vow to further deepen maritime cooperation',
                 'thumbnail': r're:^https?://.*\.jpg$',
+                'description': 'China and Indonesia vowed to upgrade their cooperation into the maritime sector and also for political security, economy, and cultural and people-to-people exchanges.',
+                'author': 'CGTN',
+                'category': 'China',
+                'timestamp': 1622950200,
+                'upload_date': '20210606',
             }
         }
     ]
@@ -34,6 +45,15 @@ class CGTNIE(InfoExtractor):
         title = self._html_search_regex(r'<div class="news-title">(.+?)</div>', webpage, 'title')
         download_url = self._html_search_regex(r'data-video ="(?P<url>.+m3u8)"', webpage, 'download_url')
         thumbnail = self._html_search_regex(r'<div class="cg-player-container J_player_container" .*? data-poster="(?P<thumbnail>.+jpg)" (.*?)>', webpage, 'thumbnail', fatal=False)
+        description = self._html_search_meta(('og:description', 'twitter:description', ), webpage, fatal=False)
+        category = self._html_search_regex(r'<span class="section">\s*(.+?)\s*</span>', webpage, 'category', fatal=False)
+        datetime_str = self._html_search_regex(r'<span class="date">\s*(.+?)\s*</span>', webpage, 'datetime_str', fatal=False)
+        author = self._html_search_regex(r'<div class="news-author-name">\s*(.+?)\s*</div>', webpage, 'author', default=None, fatal=False)
+        
+        timestamp = None
+        if datetime_str:
+            formatted_datetime_str = datetime.datetime.strptime(datetime_str, '%H:%M, %d-%b-%Y').strftime('%Y/%m/%d %H:%M')
+            timestamp = unified_timestamp(formatted_datetime_str) - 8 * 3600
 
         formats = self._extract_m3u8_formats(
             download_url, video_id, 'mp4',
@@ -42,6 +62,10 @@ class CGTNIE(InfoExtractor):
         return {
             'id': video_id,
             'title': title,
+            'description': description,
             'thumbnail': thumbnail,
-            'formats': formats
+            'formats': formats,
+            'category': category,
+            'author': author,
+            'timestamp': timestamp
         }
