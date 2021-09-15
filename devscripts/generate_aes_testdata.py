aa6f0402766f74916a35932764997812c9568cbc
sulyi <sulyi.gbox@gmail.com>
2021-09-16 00:55:43 +0200

    [aes] Fix devscript
    I'd rather remove `generate_aes_testdata.py`

--- devscripts/generate_aes_testdata.py
+++ devscripts/generate_aes_testdata.py
@@ -7,14 +7,13 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from youtube_dl.utils import intlist_to_bytes
-from youtube_dl.aes import aes_encrypt, key_expansion
+from youtube_dl.aes import aes_encrypt
 
 secret_msg = b'Secret message goes here'
 
 
 def hex_str(int_list):
-    return codecs.encode(intlist_to_bytes(int_list), 'hex')
+    return codecs.encode(int_list, 'hex')
 
 
 def openssl_encode(algo, key, iv):
@@ -24,20 +23,20 @@ def openssl_encode(algo, key, iv):
     return out
 
 
-iv = key = [0x20, 0x15] + 14 * [0]
+iv = key = b' \x15' + b'\x00' * 14
 
 r = openssl_encode('aes-128-cbc', key, iv)
 print('aes_cbc_decrypt')
 print(repr(r))
 
 password = key
-new_key = aes_encrypt(password, key_expansion(password))
+new_key = aes_encrypt(password, password)
 r = openssl_encode('aes-128-ctr', new_key, iv)
 print('aes_decrypt_text 16')
 print(repr(r))
 
-password = key + 16 * [0]
-new_key = aes_encrypt(password, key_expansion(password)) * (32 // 16)
+password = key + b'\x00' * 16
+new_key = aes_encrypt(password, password) * (32 // 16)
 r = openssl_encode('aes-256-ctr', new_key, iv)
 print('aes_decrypt_text 32')
 print(repr(r))
