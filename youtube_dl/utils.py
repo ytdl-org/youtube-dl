f02f87db8e2186ed4df4721c1e57269a3fdf44c5
Sacha Arnoud <ubuntu@vps-e11b9046.vps.ovh.net.novalocal>
2021-09-21 16:33:20 +0000

    json.dump bug fix

--- youtube_dl/utils.py
+++ youtube_dl/utils.py
@@ -1830,7 +1830,7 @@ def write_json_file(obj, fn):
 
     try:
         with tf:
-            json.dump(obj)
+            json.dump(obj, tf)
         if sys.platform == 'win32':
             # Need to remove existing file on Windows, else os.rename raises
             # WindowsError or FileExistsError.
