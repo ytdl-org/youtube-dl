f561e0d81786534993b8392d9c75074042c0330b
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2021-10-09 14:11:43 +0300

    Add Glomex IEs
    * Add new IEs
      * GlomexBaseIE: Base IE class
      * GlomexIE: Extract videos from video.glomex.com (by deferring to
        glomex:embed)
      * GlomexEmbedIE: Extract Glomex videos by matching the player URL
    * Query the API to extract metadata, detect video formats and get the
      respective (JWT protected) stream/source URLs
    * The API query may return one or more videos: the latter case is
      treated as a playlist
      * As this is otherwise identically handled, a separate IE was not
        deemed necessary
      * However title and description fields are not set for playlist
        results
        * They do not exist in the parent object; obtaining them from the
          first entry is not indicative for the playlist content
        * As the playlist order is not always stable (this is true at least
          for related videos playlists), it makes writing test cases
          impossible
    * Let GenericIE detect embeds by matching all three integration methods:
      * HTML: glomex-player tag or data attributes
      * Javascript: naive parsing of inline scripts for string constants
        assigned to integration parameters
      * Iframe: src attribute GlomexEmbedIE._VALID_URL
    * Let GlomexIE and the former embed detection pass the origin URL to
      GlomexEmbedIE by smuggling it in the player URL, as this is an
      expected parameter in API requests
    * Add test cases for both single videos and two playlist flavors

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1038,6 +1038,10 @@ from .rutube import (
     RutubePersonIE,
     RutubePlaylistIE,
 )
+from .glomex import (
+    GlomexIE,
+    GlomexEmbedIE,
+)
 from .rutv import RUTVIE
 from .ruutu import RuutuIE
 from .ruv import RuvIE
