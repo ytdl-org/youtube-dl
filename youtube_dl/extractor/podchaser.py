94c633d58a598334beb49bf2871de125839d4861
Conner <65339198+connercsbn@users.noreply.github.com>
2021-11-27 06:41:44 -0800

    Change to using Podchaser's API for data retrieval
    I've temporarily removed the
    `podchaser.com/creators/creator-<creatorid>` path from _VALID_URL,
    because I haven't figured out how to get a Creator's 4 or 5 digit
    numeric ID from the alphanumeric ID in the URL--at least not without
    downloading the page's HTML, which looks tricky atm with cloudflare

--- youtube_dl/extractor/podchaser.py
+++ youtube_dl/extractor/podchaser.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 from __future__ import unicode_literals
+import json
 import re
-from datetime import datetime
-from ..utils import float_or_none, try_get, str_to_int, unified_timestamp
+from ..utils import float_or_none, try_get, str_to_int, unified_timestamp, merge_dicts
 from ..compat import compat_str
 from .common import InfoExtractor
 
@@ -10,10 +10,8 @@ from .common import InfoExtractor
 class PodchaserIE(InfoExtractor):
     _VALID_URL = r'''(?x)
         https?://(?:www\.)?podchaser\.com/
-        (?:(?:podcasts)|(?:creators))
-        /[\w-]+-
         (?:
-            (?P<creator_id>[\d]+[\w]+)|(?P<podcast_id>[\d]+))
+            (?:podcasts/[\w-]+-(?P<podcast_id>[\d]+)))
         (?:/episodes/[\w\-]+-
             (?P<id>[\d]+))?'''
 
@@ -26,6 +24,7 @@ class PodchaserIE(InfoExtractor):
             'thumbnail': r're:^https?://.*\.jpg$',
             'ext': 'mp3',
             'categories': ['Comedy'],
+            'tags': ['comedy', 'dark humor'],
             'series': 'Cum Town',
             'duration': 3708,
             'timestamp': 1636531259,
@@ -36,63 +35,80 @@ class PodchaserIE(InfoExtractor):
         'info_dict': {
             'id': '28853',
             'title': 'The Bone Zone',
-            'description': 'md5:c39acd897170a8bf3ad94fc45dc25060',
+            'description': 'Podcast by The Bone Zone',
         },
-        'playlist_count': 6
-    }, {
-        'url': 'https://www.podchaser.com/creators/todd-glass-107ZzkFiEQ',
-        'info_dict': {
-            'id': '107ZzkFiEQ',
-            'title': 'Todd Glass',
-            'description': 'md5:0771e81d879f304f11254e5a56a97a58',
-        },
-        'playlist_mincount': 48
+        'playlist_count': 275
     }, {
         'url': 'https://www.podchaser.com/podcasts/sean-carrolls-mindscape-scienc-699349/episodes',
         'info_dict': {
             'id': '699349',
             'title': "Sean Carroll's Mindscape: Science, Society, Philosophy, Culture, Arts, and Ideas",
-            'description': 'md5:8692ce0c50cb900c5e4eb27b437dd67b'
+            'description': 'md5:2cbd8f4749891a84dc8235342e0b5ff1'
         },
-        'playlist_count': 25
+        'playlist_mincount': 199
     }]
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
-        audio_id, podcast_id, creator_id = mobj.group('id'), \
-            mobj.group('podcast_id'), mobj.group('creator_id')
-        webpage = self._download_webpage(url, audio_id)
-        page_title = self._html_search_meta(['title', 'og:title', 'twitter:title'], webpage, default=None) \
-            or self._search_regex(
-                r'<h1[^>]*>(.+?)</h1>', webpage, 'title', fatal=False, default="Podchaser Podcast")
-        page_description = self._html_search_meta(['description', 'og:description', 'twitter:description'], webpage, default=None)
+        audio_id, podcast_id = mobj.group('id'), mobj.group('podcast_id')
 
-        data = self._search_regex(
-            r'window.__APP_STATE__\s*=\s*(["\']?{.+?}["\']?);</script>', webpage, 'app state')
+        # If one episode
+        if audio_id:
+            episodes = [self._download_json("https://api.podchaser.com/episodes/%s" % audio_id, audio_id)]
 
-        while isinstance(data, compat_str):
-            data = self._parse_json(data, audio_id)
+        # Else get every episode available
+        else:
+            total_episode_count = self._download_json(
+                "https://api.podchaser.com/list/episode", podcast_id,
+                headers={'Content-Type': 'application/json;charset=utf-8'},
+                data=json.dumps({
+                    "filters": {"podcast_id": podcast_id}
+                }).encode()).get('total')
+            episodes = []
+            print(total_episode_count)
+            for i in range(total_episode_count // 100 + 1):
+                curr_episodes_data = self._download_json(
+                    "https://api.podchaser.com/list/episode", podcast_id,
+                    headers={'Content-Type': 'application/json;charset=utf-8'},
+                    data=json.dumps({
+                        "start": i * 100,
+                        "count": (i + 1) * 100,
+                        "sort_order": "SORT_ORDER_RECENT",
+                        "filters": {
+                            "podcast_id": podcast_id
+                        }, "options": {}
+                    }).encode())
+                curr_episodes = curr_episodes_data.get('entities') or []
+                if len(curr_episodes) + len(episodes) <= total_episode_count:
+                    episodes.extend(curr_episodes)
 
-        episodes = try_get(data, lambda x: x['podcast']['episodes']['entities'], dict) or {}
-        episode_list = [(episodes.get(episode_id), episode_id) for episode_id in episodes]
+        podcast_data = merge_dicts(
+            self._download_json("https://api.podchaser.com/podcasts/%s" % podcast_id, audio_id or podcast_id) or {},
+            episodes[0].get('podcast') or {} if episodes else {})
 
         entries = [{
-            'id': episode_id,
+            'id': compat_str(episode.get('id')),
             'title': episode.get('title'),
             'description': episode.get('description'),
             'url': episode.get('audio_url'),
-            'thumbnail': try_get(episode, lambda x: x['podcast']['image_url']),
+            'thumbnail': episode.get('image_url'),
             'duration': str_to_int(episode.get('length')),
             'timestamp': unified_timestamp(episode.get('air_date')),
             'rating': float_or_none(episode.get('rating')),
-            'categories': [x['text'] for x in try_get(episode, lambda x: x['podcast']['categories'], list) or []],
-            'tags': [tag['text'] for tag in episode.get('tags') or []],
-            'series': try_get(episode, lambda x: x['podcast']['title'], compat_str),
-        } for episode, episode_id in episode_list]
+            'categories': [
+                x.get('text') for x in
+                podcast_data.get('categories')
+                or try_get(podcast_data, lambda x: x['summary']['categories'], list) or []],
+            'tags': [tag.get('text') for tag in podcast_data.get('tags') or []],
+            'series': podcast_data.get('title'),
+        } for episode in episodes]
 
         if len(entries) > 1:
+            # Return playlist
             return self.playlist_result(
-                entries, playlist_id=(creator_id or podcast_id), playlist_title=page_title,
-                playlist_description=page_description)
+                entries, playlist_id=compat_str(podcast_data.get('id')),
+                playlist_title=podcast_data.get('title'),
+                playlist_description=podcast_data.get('description'))
 
+        # Return episode
         return entries[0]
