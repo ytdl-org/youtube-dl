5015290c3b5eaf3ab9e756271c0a4dc51914f537
Ondrej Bakan <ondrej@bakan.cz>
2021-12-31 00:48:17 +0100

    Quick fix of StreamCZ extractor.

--- youtube_dl/extractor/streamcz.py
+++ youtube_dl/extractor/streamcz.py
@@ -1,105 +1,95 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import hashlib
-import time
-
 from .common import InfoExtractor
+from ..compat import (
+    compat_str,
+)
 from ..utils import (
     int_or_none,
-    sanitized_Request,
+    js_to_json,
+    parse_codecs,
+    try_get,
+    urljoin,
 )
 
+class StreamCZBase(InfoExtractor):
+    def _extract_sdn_formats(self, sdn_url, video_id):
+        sdn_data = self._download_json(sdn_url, video_id)
+        formats = []
+        
+        mp4_formats = try_get(sdn_data, lambda x: x['data']['mp4'], dict)
+        for format_id, format_data in mp4_formats.items():
+            format = {
+                'format_id': 'http-%s' % format_id,
+                'container': 'mp4',
+                'width': int_or_none(format_data.get('resolution')[0]),
+                'height': int_or_none(format_data.get('resolution')[1]),
+                'tbr': int_or_none(format_data.get('bandwidth'), scale=1000),
+                'url': urljoin(sdn_url, format_data.get('url'))
+            }
+            format.update(parse_codecs(format_data.get('codec')))
+            formats.append(format)
+        
+        def get_url(format_id):
+            return try_get(pls, lambda x: x[format_id]['url'], compat_str)
+        
+        # PLS might contain further formats (DASH, HLS, HLS with fMP4)
+        pls = sdn_data.get('pls')
 
-def _get_api_key(api_path):
-    if api_path.endswith('?'):
-        api_path = api_path[:-1]
+        # DASH format
+        dash_rel_url = get_url('dash')
+        if dash_rel_url:
+            formats.extend(self._extract_mpd_formats(
+                urljoin(sdn_url, dash_rel_url), video_id, mpd_id='dash',
+                fatal=False
+            ))
+        
+        # HLS with fMP4 format
+        hls_rel_url = get_url('hls_fmp4')
+        if hls_rel_url:
+            formats.extend(self._extract_m3u8_formats(
+                urljoin(sdn_url, hls_rel_url), video_id, m3u8_id='hls_fmp4',
+                ext='mp4', fatal=False
+            ))
 
-    api_key = 'fb5f58a820353bd7095de526253c14fd'
-    a = '{0:}{1:}{2:}'.format(api_key, api_path, int(round(time.time() / 24 / 3600)))
-    return hashlib.md5(a.encode('ascii')).hexdigest()
+        self._sort_formats(formats)
+        return formats
 
-
-class StreamCZIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?stream\.cz/.+/(?P<id>[0-9]+)'
-    _API_URL = 'http://www.stream.cz/API'
-
-    _TESTS = [{
-        'url': 'http://www.stream.cz/peklonataliri/765767-ecka-pro-deti',
-        'md5': '934bb6a6d220d99c010783c9719960d5',
+class StreamCZIE(StreamCZBase):
+    _VALID_URL = r'https?://(?:www\.)?stream\.cz/(?:[^/]+)/.+-(?P<id>[0-9]+)'
+    _TEST = {
+        'url': 'https://www.stream.cz/kdo-to-mluvi/kdo-to-mluvi-velke-odhaleni-prinasi-novy-porad-uz-od-25-srpna-64087937',
         'info_dict': {
-            'id': '765767',
-            'ext': 'mp4',
-            'title': 'Peklo na talíři: Éčka pro děti',
-            'description': 'Taška s grónskou pomazánkou a další pekelnosti ZDE',
-            'thumbnail': 're:^http://im.stream.cz/episode/52961d7e19d423f8f06f0100',
-            'duration': 256,
-        },
-    }, {
-        'url': 'http://www.stream.cz/blanik/10002447-tri-roky-pro-mazanka',
-        'md5': '849a88c1e1ca47d41403c2ba5e59e261',
-        'info_dict': {
-            'id': '10002447',
-            'ext': 'mp4',
-            'title': 'Kancelář Blaník: Tři roky pro Mazánka',
-            'description': 'md5:3862a00ba7bf0b3e44806b544032c859',
-            'thumbnail': 're:^http://im.stream.cz/episode/537f838c50c11f8d21320000',
-            'duration': 368,
-        },
-    }]
+            'id': '64087937',
+            'title': 'Kdo to mluví? Velké odhalení přináší nový pořad už od 25. srpna',
+            'description': 'K ikonickým tvářím obrazovek a filmových pláten neodmyslitelně patří i jejich hlasy. Kdo se ale za nimi skrývá? Sledujte nový seriál ze zákulisí dabingu už od příštího úterý.',
+            'ext': 'mp4'
+            #'formats': 
+        }
+    }
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        api_path = '/episode/%s' % video_id
 
-        req = sanitized_Request(self._API_URL + api_path)
-        req.add_header('Api-Password', _get_api_key(api_path))
-        data = self._download_json(req, video_id)
+        webpage = self._download_webpage(url, video_id)
+        webpage_data = self._html_search_regex(r'window\.APP_SERVER_STATE\s=\s(\{[^;]*)', webpage, 'webpage_data')
+        webpage_json = self._parse_json(webpage_data, video_id, transform_source=js_to_json)
 
-        formats = []
-        for quality, video in enumerate(data['video_qualities']):
-            for f in video['formats']:
-                typ = f['type'].partition('/')[2]
-                qlabel = video.get('quality_label')
-                formats.append({
-                    'format_note': '%s-%s' % (qlabel, typ) if qlabel else typ,
-                    'format_id': '%s-%s' % (typ, f['quality']),
-                    'url': f['source'],
-                    'height': int_or_none(f['quality'].rstrip('p')),
-                    'quality': quality,
-                })
-        self._sort_formats(formats)
+        video_detail = try_get(webpage_json, lambda x: x['data']['fetchable']['episode']['videoDetail']['data'], dict)
+        
+        # metadata
+        title = video_detail.get('name')
+        description = video_detail.get('perex')
+        duration = video_detail.get('duration')
 
-        image = data.get('image')
-        if image:
-            thumbnail = self._proto_relative_url(
-                image.replace('{width}', '1240').replace('{height}', '697'),
-                scheme='http:',
-            )
-        else:
-            thumbnail = None
-
-        stream = data.get('_embedded', {}).get('stream:show', {}).get('name')
-        if stream:
-            title = '%s: %s' % (stream, data['name'])
-        else:
-            title = data['name']
-
-        subtitles = {}
-        srt_url = data.get('subtitles_srt')
-        if srt_url:
-            subtitles['cs'] = [{
-                'ext': 'srt',
-                'url': srt_url,
-            }]
+        sdn_url = video_detail.get('spl') + 'spl2,3,VOD'
+        formats = self._extract_sdn_formats(sdn_url, video_id)
 
         return {
             'id': video_id,
             'title': title,
-            'thumbnail': thumbnail,
+            'description': description,
+            'duration': duration,
             'formats': formats,
-            'description': data.get('web_site_text'),
-            'duration': int_or_none(data.get('duration')),
-            'view_count': int_or_none(data.get('views')),
-            'subtitles': subtitles,
         }
