628dfc240ebdb2ae2a76a4d8c3c7edea7f3af209
hatienl0i261299 <loiht.b17vt220@stu.ptit.edu.vn>
2022-03-26 23:15:09 +0700

    Update code

--- youtube_dl/extractor/zingmp3.py
+++ youtube_dl/extractor/zingmp3.py
@@ -4,19 +4,16 @@ from __future__ import unicode_literals
 import hashlib
 import hmac
 import re
-from youtube_dl.compat import compat_urllib_parse_urlencode
-
 from .common import InfoExtractor
 from ..utils import (
     int_or_none,
     try_get,
-    ExtractorError,
-    dict_get,
 )
+from ..compat import compat_urllib_parse_urlencode
 
 
 class ZingMp3BaseIE(InfoExtractor):
-    _VALID_URL_TMPL = r'https?://(?:mp3\.zing|zingmp3)\.vn/(?P<type>(?:%s))/[^/]+/(?P<id>\w+)($|\W)'
+    _VALID_URL_TMPL = r'https?://(?:mp3\.zing|zingmp3)\.vn/(?P<type>(?:%s))/[^/]+/(?P<id>\w+)(?:\.html|\?)'
     _GEO_COUNTRIES = ['VN']
     _DOMAIN = 'https://zingmp3.vn'
     _SLUG_API = {
@@ -34,7 +31,7 @@ class ZingMp3BaseIE(InfoExtractor):
 
     def _extract_item(self, item, song_id, type_url, fatal):
         item_id = item.get('encodeId') or song_id
-        title = dict_get(item, ('title', 'alias')) or item['title']
+        title = item.get('title') or item.get('alias')
 
         if type_url == 'video-clip':
             source = item.get('streaming')
@@ -61,15 +58,16 @@ class ZingMp3BaseIE(InfoExtractor):
                             'height': int_or_none(self._search_regex(
                                 r'^(\d+)p', res, 'resolution', default=None)),
                         })
-            else:
-                if v != 'VIP':
-                    formats.append({
-                        'ext': 'mp3',
-                        'format_id': k,
-                        'tbr': int_or_none(k),
-                        'url': self._proto_relative_url(v),
-                        'vcodec': 'none',
-                    })
+                continue
+            elif v == 'VIP':
+                continue
+            formats.append({
+                'ext': 'mp3',
+                'format_id': k,
+                'tbr': int_or_none(k),
+                'url': self._proto_relative_url(v),
+                'vcodec': 'none',
+            })
         if not formats:
             if not fatal:
                 return
@@ -95,54 +93,42 @@ class ZingMp3BaseIE(InfoExtractor):
             'id': item_id,
             'title': title,
             'formats': formats,
-            'thumbnail': try_get(item, lambda x: x['thumbnail']) or try_get(item, lambda x: x['thumbnailM']),
+            'thumbnail': item.get('thumbnail') or item.get('thumbnailM'),
             'subtitles': subtitles,
-            'duration': int_or_none(try_get(item, lambda x: x['duration'])),
+            'duration': int_or_none(item.get('duration')),
             'track': title,
-            'artist': try_get(item, lambda x: x['artistsNames']) or try_get(item, lambda x: x['artists_names']),
-            'album': try_get(album, lambda x: x['name']) or try_get(album, lambda x: x['title']),
-            'album_artist': try_get(album, lambda x: x['artistsNames']) or try_get(album, lambda x: x['artists_names']),
+            'artist': item.get('artistsNames') or item.get('artists_names'),
+            'album': album.get('name') or album.get('title'),
+            'album_artist': album.get('artistsNames') or album.get('artists_names'),
         }
+
+    def _real_initialize(self):
+        if not self._downloader.params.get("cookiefile"):
+            self._request_webpage(self.get_api_with_signature(name_api=self._SLUG_API['bai-hat'], param={'id': ''}),
+                                  None, note='Updating cookies')
 
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
-
         song_id = mobj.group('id')
         type_url = mobj.group('type')
         api = self.get_api_with_signature(name_api=self._SLUG_API[type_url], param={'id': song_id})
-
-        # Check the cookie file, if cookie file doesn't exist, create a dummy request to update the cookie
-        if not self._downloader.params.get('cookiefile'):
-            self._download_json(api, video_id=song_id, note='Dummy request to update cookie')
-
-        info = self._download_json(api, video_id=song_id)
-
-        if not info:
-            raise ExtractorError('Can not extract data.')
-
-        return self._process_data(info.get('data'), song_id, type_url)
+        return self._process_data(self._download_json(api, song_id)['data'], song_id, type_url)
 
     def get_api_with_signature(self, name_api, param):
-        sha256 = self.sha256_params(''.join('%s=%s' % (k, v) for k, v in param.items()))
-
+        param.update({'ctime': '1'})
+        sha256 = hashlib.sha256(''.join(f'{i}={param[i]}' for i in sorted(param)).encode('utf-8')).hexdigest()
         data = {
             'apiKey': self._API_KEY,
-            'sig': self.hmac512_string('%s%s' % (name_api, sha256))
+            'sig': hmac.new(self._SECRET_KEY, f'{name_api}{sha256}'.encode('utf-8'), hashlib.sha512).hexdigest(),
+            **param,
         }
-        data.update(param)
-        return '%s%s?%s' % (self._DOMAIN, name_api, compat_urllib_parse_urlencode(data))
-
-    def sha256_params(self, string):
-        return hashlib.sha256(string.encode('utf-8')).hexdigest()
-
-    def hmac512_string(self, string):
-        return hmac.new(self._SECRET_KEY, string.encode('utf-8'), hashlib.sha512).hexdigest()
+        return f'{self._DOMAIN}{name_api}?{compat_urllib_parse_urlencode(data)}'
 
 
 class ZingMp3IE(ZingMp3BaseIE):
     _VALID_URL = ZingMp3BaseIE._VALID_URL_TMPL % 'bai-hat|video-clip|embed'
     _TESTS = [{
-        'url': 'https://zingmp3.vn/bai-hat/Xa-Mai-Xa-Bao-Thy/ZWZB9WAB.html',
+        'url': 'https://mp3.zing.vn/bai-hat/Xa-Mai-Xa-Bao-Thy/ZWZB9WAB.html',
         'md5': 'ead7ae13693b3205cbc89536a077daed',
         'info_dict': {
             'id': 'ZWZB9WAB',
@@ -162,7 +148,7 @@ class ZingMp3IE(ZingMp3BaseIE):
         },
     }, {
         'url': 'https://zingmp3.vn/video-clip/Suong-Hoa-Dua-Loi-K-ICM-RYO/ZO8ZF7C7.html',
-        'md5': 'e9c972b693aa88301ef981c8151c4343',
+        'md5': 'c7f23d971ac1a4f675456ed13c9b9612',
         'info_dict': {
             'id': 'ZO8ZF7C7',
             'title': 'Sương Hoa Đưa Lối',
@@ -171,6 +157,22 @@ class ZingMp3IE(ZingMp3BaseIE):
             'duration': 207,
             'track': 'Sương Hoa Đưa Lối',
             'artist': 'K-ICM, RYO',
+            'album': 'Sương Hoa Đưa Lối (Single)',
+            'album_artist': 'K-ICM, RYO',
+        },
+    }, {
+        'url': 'https://zingmp3.vn/bai-hat/Nguoi-Yeu-Toi-Lanh-Lung-Sat-Da-Mr-Siro/ZZ6IW7OU.html',
+        'md5': '3e9f7a9bd0d965573dbff8d7c68b629d',
+        'info_dict': {
+            'id': 'ZZ6IW7OU',
+            'title': 'Người Yêu Tôi Lạnh Lùng Sắt Đá',
+            'ext': 'mp3',
+            'thumbnail': r're:^https?://.+\.jpg',
+            'duration': 303,
+            'track': 'Người Yêu Tôi Lạnh Lùng Sắt Đá',
+            'artist': 'Mr. Siro',
+            'album': 'Người Yêu Tôi Lạnh Lùng Sắt Đá (Single)',
+            'album_artist': 'Mr. Siro',
         },
     }, {
         'url': 'https://zingmp3.vn/embed/song/ZWZEI76B?start=false',
@@ -197,6 +199,14 @@ class ZingMp3AlbumIE(ZingMp3BaseIE):
         },
         'playlist_count': 9,
     }, {
+        'url': 'https://zingmp3.vn/album/Nhung-Bai-Hat-Hay-Nhat-Cua-Mr-Siro-Mr-Siro/ZWZAEZZD.html',
+        'info_dict': {
+            '_type': 'playlist',
+            'id': 'ZWZAEZZD',
+            'title': 'Những Bài Hát Hay Nhất Của Mr. Siro',
+        },
+        'playlist_count': 49,
+    }, {
         'url': 'http://mp3.zing.vn/playlist/Duong-Hong-Loan-apollobee/IWCAACCB.html',
         'only_matching': True,
     }, {
@@ -207,10 +217,10 @@ class ZingMp3AlbumIE(ZingMp3BaseIE):
 
     def _process_data(self, data, song_id, type_url):
         def entries():
-            for item in (try_get(data, lambda x: x['song']['items']) or []):
+            for item in try_get(data, lambda x: x['song']['items']) or []:
                 entry = self._extract_item(item, song_id, type_url, False)
                 if entry:
                     yield entry
 
-        album_id = data.get('id') or data.get('encodeId')
-        return self.playlist_result(entries(), album_id, data.get('name') or data.get('title'))
+        return self.playlist_result(entries(), data.get('id') or data.get('encodeId'),
+                                    data.get('name') or data.get('title'))
