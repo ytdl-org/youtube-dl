a10f613d62eacbd0ddedfcc2f70ee34d088be4a6
Ehtisham Sabir <Shaamdbz@gmail.com>
2022-04-04 17:47:05 +0500

    fixed Test part for the wikimedia extractor
    python test/test_download.py TestDownload.test_Wikimedia
    
    [wikimedia.org] Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.webm: Downloading webpage
    [wikimedia.org] Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.webm: Extracting information
    [info] Writing video description metadata as JSON to: test_Wikimedia_Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.info.json
    [debug] Invoking downloader on 'https://upload.wikimedia.org/wikipedia/commons/transcoded/d/d7/Die_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS.webm/Die_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS.webm.480p.vp9.webm'
    [download] Destination: test_Wikimedia_Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.webm
    [download] 100% of 10.00KiB in 00:00
    .
    ----------------------------------------------------------------------
    Ran 1 test in 2.262s
    
    OK

--- youtube_dl/extractor/wikimedia.py
+++ youtube_dl/extractor/wikimedia.py
@@ -10,24 +10,26 @@ class WikimediaIE(InfoExtractor):
     _VALID_URL = r'https://commons.wikimedia.org/wiki/File:(?P<id>[^/]+)'
 
     _TEST = {
-        'url': 'https://upload.wikimedia.org/wikipedia/commons/transcoded/d/d7/Die_Temperaturkurve_der_Erde_%28ZDF'
-               '%2C_Terra_X%29_720p_HD_50FPS.webm/Die_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS'
-               '.webm.480p.vp9.webm',
-        'description': 'Deutsch:  Beschreibung auf der Seite: "Im Verlauf der Erdgeschichte glich das Klima einer '
-                       'Achterbahnfahrt. Die „Fieberkurve“ unseres Planeten zeigt die globalen Temperaturschwankungen '
-                       'bis heute – rekonstruiert anhand von historischen Klimadaten."\nZu Wikimedia Commons '
-                       'hochgeladen von: PantheraLeo1359531.\nHinweise zur Weiterverwendung: '
-                       'https://www.zdf.de/dokumentation/terra-x/terra-x-creative-commons-cc-100.html'
-                       '.\nVereinfachender Verlauf in der Geschichte der Erde, für die Zukunft spätestens ab dem Jahr '
-                       '2050 mit spekulativem Verlauf in der Prognose (ausgeprägtes Global-warming-Szenario ist '
-                       'dargestellt).English:  Climate change, Temperature in history of Earth, Video of Terra X.',
-        'ext': 'webm', 'id': 'Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.webm',
-        'title': 'Die Temperaturkurve der Erde (ZDF, Terra X) 720p HD 50FPS.webm - Wikimedia Commons',
-        'license': 'This file is licensed under the Creative Commons Attribution 4.0 International license.',
-        'author': 'ZDF/Terra X/Gruppe 5/Luise Wagner, Jonas Sichert, Andreas Hougardy', 'subtitles': {'nl': [
-            {'ext': 'srt',
-             'url': 'https://commons.wikimedia.org/w/api.php?action=timedtext&lang=nl&title=File'
-                    '%3ADie_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS.webm&trackformat=srt'}]}}
+        'url': 'https://commons.wikimedia.org/wiki/File:Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS.webm',
+        'info_dict': {
+            'description': 'Deutsch:  Beschreibung auf der Seite: "Im Verlauf der Erdgeschichte glich das Klima einer Achterbahnfahrt. Die „Fieberkurve“ unseres Planeten zeigt die globalen Temperaturschwankungen bis heute – rekonstruiert anhand von historischen Klimadaten."\nZu Wikimedia Commons hochgeladen von: PantheraLeo1359531.\nHinweise zur Weiterverwendung: https://www.zdf.de/dokumentation/terra-x/terra-x-creative-commons-cc-100.html.\nVereinfachender Verlauf in der Geschichte der Erde, für die Zukunft spätestens ab dem Jahr 2050 mit spekulativem Verlauf in der Prognose (ausgeprägtes Global-warming-Szenario ist dargestellt).English:  Climate change, Temperature in history of Earth, Video of Terra X.',
+            'ext': 'webm',
+            'id': 'Die_Temperaturkurve_der_Erde_(ZDF,_Terra_X)_720p_HD_50FPS',
+            'title': 'Die Temperaturkurve der Erde (ZDF, Terra X) 720p HD 50FPS.webm - Wikimedia Commons',
+            'license': 'This file is licensed under the Creative Commons Attribution 4.0 International license.',
+            'author': 'ZDF/Terra X/Gruppe 5/Luise Wagner, Jonas Sichert, Andreas Hougardy',
+            'subtitles': {'de': [
+                {'ext': 'srt',
+                 'url': 'https://commons.wikimedia.org/w/api.php?action=timedtext&amp&title=File%3ADie_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS.webm&amp&lang=de&amp&trackformat=srt'}],
+                'en-gb': [
+                    {'ext': 'srt',
+                     'url': 'https://commons.wikimedia.org/w/api.php?action=timedtext&amp&title=File%3ADie_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS.webm&amp&lang=en-gb&amp&trackformat=srt'}],
+                'nl': [
+                    {'ext': 'srt',
+                     'url': 'https://commons.wikimedia.org/w/api.php?action=timedtext&amp&title=File%3ADie_Temperaturkurve_der_Erde_%28ZDF%2C_Terra_X%29_720p_HD_50FPS.webm&amp&lang=nl&amp&trackformat=srt'}
+                ]}
+        }
+    }
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
@@ -60,6 +62,6 @@ class WikimediaIE(InfoExtractor):
             sub = 'https://commons.wikimedia.org/w/api' + sub + 'srt'
             lang = sub[sub.find('lang=') + 5:]
             lang = lang[:lang.find('&')]
-            sub = sub.replace(';','&')
+            sub = sub.replace(';', '&')
             info['subtitles'][lang] = [{"ext": "srt", "url": sub}]
         return info
