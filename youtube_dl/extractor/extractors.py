876d9b8f49c23aa5df79d451cea52e7a909c770d
dirkf <fieldhouse@gmx.net>
2022-04-28 01:17:43 +0100

    [TalkTV] Add extractors for TalkTV (UK) shows and series

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1214,6 +1214,10 @@ from .tagesschau import (
     TagesschauPlayerIE,
     TagesschauIE,
 )
+from .talktv import (
+    TalkTVIE,
+    TalkTVSeriesIE,
+)
 from .tass import TassIE
 from .tbs import TBSIE
 from .tdslifeway import TDSLifewayIE
