3687a4f9bdb025855acd2a4e782748c8e3810765
dirkf <fieldhouse@gmx.net>
2022-06-04 08:07:54 +0100

    [DoodStream] Add extractor from yt-dlp back-port and improve

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -299,6 +299,7 @@ from .democracynow import DemocracynowIE
 from .dfb import DFBIE
 from .dhm import DHMIE
 from .digg import DiggIE
+from .doodstream import DoodStreamIE
 from .dotsub import DotsubIE
 from .douyutv import (
     DouyuShowIE,
