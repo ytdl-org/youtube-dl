0afddd0b9ae23e32d809e2f4edd5574e28d28487
wangchangren <changren.wcr@alibaba-inc.com>
2022-06-18 08:56:44 +0800

    fix: refactor two extractors into one

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -975,10 +975,7 @@ from .presstv import PressTVIE
 from .prosiebensat1 import ProSiebenSat1IE
 from .puls4 import Puls4IE
 from .pyvideo import PyvideoIE
-from .qingting import (
-    QingTingMobileIE,
-    QingTingDeskTopIE,
-)
+from .qingting import QingTingIE
 from .qqmusic import (
     QQMusicIE,
     QQMusicSingerIE,
--- youtube_dl/extractor/qingting.py
+++ youtube_dl/extractor/qingting.py
@@ -1,46 +1,17 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import re
-
 from .common import InfoExtractor
 
 from youtube_dl import utils
 
-class QingTingMobileIE(InfoExtractor):
-    IE_NAME = 'QingTing'
-    _VALID_URL = r'(?:https?://)?(?:www\.)?m\.(?:qingting\.fm|qtfm\.cn)/vchannels/\d+/programs/(?P<id>\d+)'
-    _TEST = {
-        'url': 'https://m.qingting.fm/vchannels/378005/programs/22257411/',
-        'md5': '47e6a94f4e621ed832c316fd1888fb3c',
-        'info_dict': {
-            'id': '22257411',
-            'ext': 'mp3',
-            'title': '用了十年才修改，谁在乎教科书？-睡前消息-蜻蜓FM听头条',
-        }
-    }
 
-    def _real_extract(self, url):
-        video_id = self._match_id(url)
-        webpage = self._download_webpage(url, video_id)
-        title = self._html_search_regex(r'(?s)<title\b[^>]*>(.*)</title>', webpage, 'title', default=None) or self._og_search_title(webpage)
-        url = self._search_regex(
-            r'''("|')audioUrl\1\s*:\s*("|')(?P<url>(?:(?!\2).)*)\2''',
-            webpage, 'audio URL', group="url")
-        test_url = utils.url_or_none(url)
-        if not test_url:
-            raise utils.ExtractorError('Invalid audio URL %s' % (url,))
-        url = test_url
-        return {
-            'id': video_id,
-            'title': title,
-            'ext': 'mp3',
-            'url': url,
-        }
-
-class QingTingDeskTopIE(InfoExtractor):
+class QingTingIE(InfoExtractor):
     IE_NAME = 'QingTing'
-    _VALID_URL = r'(?:https?://)?(?:www\.)?(?:qingting\.fm|qtfm\.cn)/channels/\d+/programs/(?P<id>\d+)'
+    _VALID_URL = r'''(?x)
+                     (?:https?://)?(?:www\.)?
+                         (?P<m>m\.)?(?:qingting\.fm|qtfm\.cn)/(?(m)v|)
+                         channels/\d+/programs/(?P<id>\d+)'''
     _TEST = {
         'url': 'https://www.qingting.fm/channels/378005/programs/22257411/',
         'md5': '47e6a94f4e621ed832c316fd1888fb3c',
@@ -56,11 +27,27 @@ class QingTingDeskTopIE(InfoExtractor):
         webpage = self._download_webpage(url, video_id)
         title = self._html_search_regex(r'(?s)<title\b[^>]*>(.*)</title>', webpage, 'title',
                                         default=None) or self._og_search_title(webpage)
-        url = self._search_regex(
-            r'''("|')alternate\1\s*:\s*("|')(?P<url>(?:(?!\2).)*)\2''',
-            webpage, 'alternate URL', group="url")
-        test_url = utils.url_or_none(url)
-        if not test_url:
-            raise utils.ExtractorError('Invalid audio URL %s' % (url,))
-        url = test_url
-        return self.url_result(url=url, video_id=video_id, video_title=title)
+        urlType = self._search_regex(
+            self._VALID_URL,
+            url, 'audio URL', group="m")
+        if urlType == 'm.':
+            url = self._search_regex(
+                r'''("|')audioUrl\1\s*:\s*("|')(?P<url>(?:(?!\2).)*)\2''',
+                webpage, 'audio URL', group="url")
+            test_url = utils.url_or_none(url)
+            if not test_url:
+                raise utils.ExtractorError('Invalid audio URL %s' % (url,))
+            return {
+                'id': video_id,
+                'title': title,
+                'ext': 'mp3',
+                'url': test_url,
+            }
+        else:
+            url = self._search_regex(
+                r'''("|')alternate\1\s*:\s*("|')(?P<url>(?:(?!\2).)*)\2''',
+                webpage, 'alternate URL', group="url")
+            test_url = utils.url_or_none(url)
+            if not test_url:
+                raise utils.ExtractorError('Invalid audio URL %s' % (url,))
+            return self.url_result(url=test_url, video_id=video_id, video_title=title)
