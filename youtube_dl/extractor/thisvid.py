4c42bbaac9be47bcb7d292546becdde79f8b3fb5
dirkf <fieldhouse@gmx.net>
2022-06-24 00:05:27 +0100

    Update youtube_dl/extractor/thisvid.py

--- youtube_dl/extractor/thisvid.py
+++ youtube_dl/extractor/thisvid.py
@@ -169,10 +169,11 @@ class ThisVidMemberIE(InfoExtractor):
                 for m in re.finditer(r'''<a\b[^>]+\bhref\s*=\s*["'](?P<url>%s\b)[^>]+>''' % (ThisVidIE._VALID_URL, ), html):
                     yield m.group('url')
                 next_page = get_element_by_class('pagination-next', html)
-                next_page = urljoin(url, next_page
-                     and self._search_regex(
-                        r'''<a\b[^>]+\bhref\s*=\s*("|')(?P<url>(?:(?!\1).)+)''',
-                        next_page, 'next page link', group='url', default=None))
+                next_page = urljoin(url,
+                    next_page
+                    and self._search_regex(
+                            r'''<a\b[^>]+\bhref\s*=\s*("|')(?P<url>(?:(?!\1).)+)''',
+                            next_page, 'next page link', group='url', default=None))
                 if not next_page:
                     break
                 page_url, html = next_page, None
