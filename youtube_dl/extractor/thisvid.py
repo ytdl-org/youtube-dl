e599b787e3eb8991908254fcc8d9ff32cf34e96d
FraFraFra-LongD <85188920+FraFraFra-LongD@users.noreply.github.com>
2022-07-04 18:33:29 +0200

    Update youtube_dl/extractor/thisvid.py
    Co-authored-by: dirkf <fieldhouse@gmx.net>

--- youtube_dl/extractor/thisvid.py
+++ youtube_dl/extractor/thisvid.py
@@ -168,9 +168,23 @@ class ThisVidMemberIE(InfoExtractor):
                 for m in re.finditer(r'''<a\b[^>]+\bhref\s*=\s*["'](?P<url>%s\b)[^>]+>''' % (ThisVidIE._VALID_URL, ), html):
                     yield m.group('url')
                 next_page = get_element_by_class('pagination-next', html)
-                next_page = urljoin(url, self._search_regex(
-                    r'''<a\b[^>]+\bhref\s*=\s*("|')(?P<url>(?:(?!\1).)+)''',
-                    next_page, 'next page link', group='url', default=None) if next_page else None)
+                if next_page:
+                    # member list page
+                    next_page = urljoin(url, self._search_regex(
+                        r'''<a\b[^>]+\bhref\s*=\s*("|')(?P<url>(?:(?!\1).)+)''',
+                        next_page, 'next page link', group='url', default=None)
+                # in case a member page should have pagination-next with empty link, not just `else:`
+                if next_page is None:
+                    # playlist page
+                    parsed_url = compat_urlparse.urlparse(page_url)
+                    base_path, num = parsed_url.path.rsplit('/', 1)
+                    num = int_or_none(num)
+                    if num is None:
+                        base_path, num = parsed_url.path.rstrip('/'), 1
+                    parsed_url._replace(path=base_path + ('/%d' % (num + 1, )))
+                    next_page = compat_urlparse.urlunparse(parsed_url)
+                    if page_url == next_page:
+                        next_page = None
                 if not next_page:
                     break
                 page_url, html = next_page, None
