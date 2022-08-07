b590eeaccc28b0333c5db0e801ac4873e5458727
palewire <b@palewi.re>
2022-08-07 09:51:08 -0700

    Tidying

--- youtube_dl/extractor/gab.py
+++ youtube_dl/extractor/gab.py
@@ -9,37 +9,32 @@ from ..utils import clean_html, int_or_none, unified_timestamp
 class GabIE(InfoExtractor):
     """Extract videos from posts on Gab."""
 
-    _VALID_URL = r"https://gab\.com/@[^/]+/posts/(?P<id>[0-9]+)"
-    _TESTS = [
-        {
-            "url": "https://gab.com/INFOWARS/posts/108732338582726706",
-            "md5": "4a5fb1470c192e493d9efd6f19e514d3",
-            "info_dict": {
-                "id": "108732338582726706",
-                "ext": "mp4",
-                "title": "Untitled",
-                "timestamp": 1659835827,
-                "upload_date": "20220729",
-                "uploader": "INFOWARS",
-                "uploader_id": "INFOWARS",
-            },
+    _VALID_URL = r"https://gab\.com/[^/]+/posts/(?P<id>[0-9]+)"
+    _TEST = {
+        "url": "https://gab.com/INFOWARS/posts/108732338582726706",
+        "md5": "8283f5658ecc5b2e23364ede7089bfed",
+        "info_dict": {
+            "id": "108732338582726706",
+            "ext": "mp4",
+            "title": "NOW LIVE!!! ALEX'S WAR 🔥 🔥 🔥#AlexsWar is an unprecedented close examination of this guarded, mythic figure #AlexJones, and the story of the fracturing of the American narrative—through the eyes of this man who helped break it.🎥 PURCHASE NOW: https://www.infowarsstore.com/alex-jones-war-dvd⚔️ LEARN MORE: https://www.alexswar.com/#AlexJonesWasRight #INFOWARS #FreeSpeech @RealAlexJones",
+            "timestamp": 1659123823,
+            "upload_date": "20220729",
+            "uploader": "INFOWARS",
+            "uploader_id": "INFOWARS",
         },
-    ]
+    }
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        print(video_id)
 
         # Get data from API
         api_url = "https://gab.com/api/v1/statuses/" + video_id
         status = self._download_json(api_url, video_id)
-        print(status)
 
         # Pull out video
         attachments = status["media_attachments"]
         video = attachments[0]
         url = video["source_mp4"]
-        print(url)
 
         # Pull out metadata
         title = clean_html(status.get("content")).replace("\n", "") or "Untitled"
@@ -47,9 +42,7 @@ class GabIE(InfoExtractor):
         timestamp = unified_timestamp(status.get("created_at"))
         uploader = account.get("display_name")
         uploader_id = account.get("username")
-        uploader_url = (
-            "https://gab.com/@" + uploader_id if uploader_id else None
-        )
+        uploader_url = "https://gab.com/" + uploader_id if uploader_id else None
         repost_count = int_or_none(status.get("reblogs_count"))
         like_count = int_or_none(status.get("favourites_count"))
         comment_count = int_or_none(status.get("replies_count"))
