b14b4b8af50c5395777671f63212220d3ce1d582
palewire <b@palewi.re>
2022-08-08 11:01:58 -0700

    Single quotes

--- youtube_dl/extractor/parler.py
+++ youtube_dl/extractor/parler.py
@@ -14,29 +14,29 @@ class ParlerIE(InfoExtractor):
     _VALID_URL = r"https://parler\.com/feed/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})"
     _TESTS = [
         {
-            "url": "https://parler.com/feed/df79fdba-07cc-48fe-b085-3293897520d7",
-            "md5": "16e0f447bf186bb3cf64de5bbbf4d22d",
-            "info_dict": {
-                "id": "df79fdba-07cc-48fe-b085-3293897520d7",
-                "ext": "mp4",
-                "title": "Puberty-blocking procedures promoted by the Biden/Harris Admin are child abuse. The FDA has recently confirmed these hormones/drugs have extremely dangerous side effects, like brain swelling and vision loss.",
-                "timestamp": 1659744000,
-                "upload_date": "20220806",
-                "uploader": "Tulsi Gabbard",
-                "uploader_id": "TulsiGabbard",
+            'url': 'https://parler.com/feed/df79fdba-07cc-48fe-b085-3293897520d7',
+            'md5': '16e0f447bf186bb3cf64de5bbbf4d22d',
+            'info_dict': {
+                'id': 'df79fdba-07cc-48fe-b085-3293897520d7',
+                'ext': 'mp4',
+                'title': 'Puberty-blocking procedures promoted by the Biden/Harris Admin are child abuse. The FDA has recently confirmed these hormones/drugs have extremely dangerous side effects, like brain swelling and vision loss.',
+                'timestamp': 1659744000,
+                'upload_date': '20220806',
+                'uploader': 'Tulsi Gabbard',
+                'uploader_id': 'TulsiGabbard',
             },
         },
         {
-            "url": "https://parler.com/feed/a7406eb4-91e5-4793-b5e3-ade57a24e287",
-            "md5": "11687e2f5bb353682cee338d181422ed",
-            "info_dict": {
-                "id": "a7406eb4-91e5-4793-b5e3-ade57a24e287",
-                "ext": "mp4",
-                "title": "This man should run for office",
-                "timestamp": 1659657600,
-                "upload_date": "20220805",
-                "uploader": "Benny Johnson",
-                "uploader_id": "BennyJohnson",
+            'url': 'https://parler.com/feed/a7406eb4-91e5-4793-b5e3-ade57a24e287',
+            'md5': '11687e2f5bb353682cee338d181422ed',
+            'info_dict': {
+                'id': 'a7406eb4-91e5-4793-b5e3-ade57a24e287',
+                'ext': 'mp4',
+                'title': 'This man should run for office',
+                'timestamp': 1659657600,
+                'upload_date': '20220805',
+                'uploader': 'Benny Johnson',
+                'uploader_id': 'BennyJohnson',
             },
         },
     ]
