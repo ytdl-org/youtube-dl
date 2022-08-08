b1bbb03e0b8f026e78748f6bfd822691522ddf13
Ben Welsh <b@palewi.re>
2022-08-08 09:46:26 -0700

    Update youtube_dl/extractor/parler.py
    Co-authored-by: dirkf <fieldhouse@gmx.net>

--- youtube_dl/extractor/parler.py
+++ youtube_dl/extractor/parler.py
@@ -16,7 +16,8 @@ from ..utils import (
 class ParlerIE(InfoExtractor):
     """Extract videos from posts on Parler."""
 
-    _VALID_URL = r"https://parler\.com/feed/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})"
+    _UUID_RE = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+    _VALID_URL = r'https://parler\.com/feed/(?P<id>%s)' % (_UUID_RE, )
     _TESTS = [
         {
             "url": "https://parler.com/feed/df79fdba-07cc-48fe-b085-3293897520d7",
