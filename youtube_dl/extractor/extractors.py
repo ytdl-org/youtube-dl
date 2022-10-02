149e69ec98046879e3fdfed89b90a8c4f80589bd
Evan Zhang <evanzh15@gmail.com>
2022-10-02 12:48:33 -0400

    Nest Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -761,6 +761,7 @@ from .ndr import (
 from .ndtv import NDTVIE
 from .netzkino import NetzkinoIE
 from .nerdcubed import NerdCubedFeedIE
+from .nest import NestIE
 from .neteasemusic import (
     NetEaseMusicIE,
     NetEaseMusicAlbumIE,
