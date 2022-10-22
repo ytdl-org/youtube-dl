90be64f8f3585a5041cc20ebec46bff6d22387b4
Valentin Metz <valentin.metz@tum.de>
2022-10-22 14:29:16 +0200

    [rbgtum] Add new extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1005,6 +1005,10 @@ from .raywenderlich import (
     RayWenderlichIE,
     RayWenderlichCourseIE,
 )
+from .rbgtum import (
+    RbgTumIE,
+    RbgTumCourseIE,
+)
 from .rbmaradio import RBMARadioIE
 from .rds import RDSIE
 from .redbulltv import (
