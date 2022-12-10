52524e85f40867d3cd1b63970e7bb05baad8748f
Shiyang Zhu <zhuasy@umich.edu>
2022-12-10 15:16:03 -0500

    _real_extract() now iterates through possible manifest URLS to find the m3u8.

--- youtube_dl/extractor/senateisvp.py
+++ youtube_dl/extractor/senateisvp.py
@@ -5,50 +5,52 @@ import re
 from .common import InfoExtractor
 from ..utils import (
     ExtractorError,
+    url_or_none,
+    parse_duration,
     unsmuggle_url,
 )
 from ..compat import (
     compat_parse_qs,
-    compat_urlparse,
 )
 
-
 class SenateISVPIE(InfoExtractor):
+    # committee --> [stream_number, stream_domain, stream_id, msl3]
     _COMM_MAP = {
-        'ag':           [76440, 'https://ag-f.akamaihd.net',        2036803, 'agriculture'],
-        'aging':        [76442, 'https://aging-f.akamaihd.net',     2036801, 'aging'],
-        'approps':      [76441, 'https://approps-f.akamaihd.net',   2036802, 'appropriations'],
-        'armed':        [76445, 'https://armed-f.akamaihd.net',     2036800, 'armedservices'],
-        'banking':      [76446, 'https://banking-f.akamaihd.net',   2036799, 'banking'],
-        'budget':       [76447, 'https://budget-f.akamaihd.net',    2036798, 'budget'],
-        'cecc':         [76486, 'https://srs-f.akamaihd.net',       2036782, 'srs_cecc'],
-        'commerce':     [80177, 'https://commerce1-f.akamaihd.net', 2036779, 'commerce'],
-        'csce':         [75229, 'https://srs-f.akamaihd.net',       2036777, 'srs_srs'],
-        'dpc':          [76590, 'https://dpc-f.akamaihd.net',       None, 'dpc'],
-        'energy':       [76448, 'https://energy-f.akamaihd.net',    2036797, 'energy'],
-        'epw':          [76478, 'https://epw-f.akamaihd.net',       2036783, 'environment'],
-        'ethics':       [76449, 'https://ethics-f.akamaihd.net',    2036796, 'ethics'],
-        'finance':      [76450, 'https://finance-f.akamaihd.net',   2036795, 'finance_finance'],
-        'foreign':      [76451, 'https://foreign-f.akamaihd.net',   2036794, 'foreignrelations'],
-        'govtaff':      [76453, 'https://govtaff-f.akamaihd.net',   2036792, 'hsgac'],
-        'help':         [76452, 'https://help-f.akamaihd.net',      2036793, 'help'],
-        'indian':       [76455, 'https://indian-f.akamaihd.net',    2036791, 'indianaffairs'],
-        'intel':        [76456, 'https://intel-f.akamaihd.net',     2036790, 'intelligence'],
-        'intlnarc':     [76457, 'https://intlnarc-f.akamaihd.net',  None, 'internationalnarcoticscaucus'],
-        'jccic':        [85180, 'https://jccic-f.akamaihd.net',     2036778, 'jccic'],
-        'jec':          [76458, 'https://jec-f.akamaihd.net',       2036789, 'jointeconomic'],
-        'judiciary':    [76459, 'https://judiciary-f.akamaihd.net', 2036788,'judiciary'],
-        'rpc':          [76591, 'https://rpc-f.akamaihd.net',       None, 'rpc'],
-        'rules':        [76460, 'https://rules-f.akamaihd.net',     2036787, 'rules'],
-        'saa':          [76489, 'https://srs-f.akamaihd.net',       2036780, 'srs_saa'],
-        'smbiz':        [76461, 'https://smbiz-f.akamaihd.net',     2036786, 'smallbusiness'],
-        'srs':          [75229, 'https://srs-f.akamaihd.net',       2031966, 'srs_srs'],
-        'uscc':         [76487, 'https://srs-f.akamaihd.net',       2036781, 'srs_uscc'],
-        'vetaff':       [76462, 'https://vetaff-f.akamaihd.net',    2036785, 'veteransaffairs'],
-        'arch':         [None, 'https://ussenate-f.akamaihd.net/', None, None],
-        'uscp':         [None, '', 2043685, None, 'uscp'],
-        'cio':          [None, '', 2043686, None, 'cio'],
+        'ag':           [76440, 'https://ag-f.akamaihd.net',        2036803,    'agriculture'],
+        'aging':        [76442, 'https://aging-f.akamaihd.net',     2036801,    'aging'],
+        'approps':      [76441, 'https://approps-f.akamaihd.net',   2036802,    'appropriations'],
+        'armed':        [76445, 'https://armed-f.akamaihd.net',     2036800,    'armedservices'],
+        'banking':      [76446, 'https://banking-f.akamaihd.net',   2036799,    'banking'],
+        'budget':       [76447, 'https://budget-f.akamaihd.net',    2036798,    'budget'],
+        'cecc':         [76486, 'https://srs-f.akamaihd.net',       2036782,    'srs_cecc'],
+        'commerce':     [80177, 'https://commerce1-f.akamaihd.net', 2036779,    'commerce'],
+        'csce':         [75229, 'https://srs-f.akamaihd.net',       2036777,    'srs_srs'],
+        'dpc':          [76590, 'https://dpc-f.akamaihd.net',       None,       'dpc'],
+        'energy':       [76448, 'https://energy-f.akamaihd.net',    2036797,    'energy'],
+        'epw':          [76478, 'https://epw-f.akamaihd.net',       2036783,    'environment'],
+        'ethics':       [76449, 'https://ethics-f.akamaihd.net',    2036796,    'ethics'],
+        'finance':      [76450, 'https://finance-f.akamaihd.net',   2036795,    'finance_finance'],
+        'foreign':      [76451, 'https://foreign-f.akamaihd.net',   2036794,    'foreignrelations'],
+        'govtaff':      [76453, 'https://govtaff-f.akamaihd.net',   2036792,    'hsgac'],
+        'help':         [76452, 'https://help-f.akamaihd.net',      2036793,    'help'],
+        'indian':       [76455, 'https://indian-f.akamaihd.net',    2036791,    'indianaffairs'],
+        'intel':        [76456, 'https://intel-f.akamaihd.net',     2036790,    'intelligence'],
+        'intlnarc':     [76457, 'https://intlnarc-f.akamaihd.net',  None,       'internationalnarcoticscaucus'],
+        'jccic':        [85180, 'https://jccic-f.akamaihd.net',     2036778,    'jccic'],
+        'jec':          [76458, 'https://jec-f.akamaihd.net',       2036789,    'jointeconomic'],
+        'judiciary':    [76459, 'https://judiciary-f.akamaihd.net', 2036788,    'judiciary'],
+        'rpc':          [76591, 'https://rpc-f.akamaihd.net',       None,       'rpc'],
+        'rules':        [76460, 'https://rules-f.akamaihd.net',     2036787,    'rules'],
+        'saa':          [76489, 'https://srs-f.akamaihd.net',       2036780,    'srs_saa'],
+        'smbiz':        [76461, 'https://smbiz-f.akamaihd.net',     2036786,    'smallbusiness'],
+        'srs':          [75229, 'https://srs-f.akamaihd.net',       2031966,    'srs_srs'],
+        'uscc':         [76487, 'https://srs-f.akamaihd.net',       2036781,    'srs_uscc'],
+        'vetaff':       [76462, 'https://vetaff-f.akamaihd.net',    2036785,    'veteransaffairs'],
+        'arch':         [None,  'https://ussenate-f.akamaihd.net/', None,       None],
+        'uscp':         [None,  '',                                 2043685,    'uscp'],
+        'cio':          [None,  '',                                 2043686,    'cio'],
     }
+
     _IE_NAME = 'senate.gov'
     _VALID_URL = r'https?://(?:www\.)?senate\.gov/isvp/?\?(?P<qs>.+)'
     _TESTS = [{
@@ -56,7 +58,7 @@ class SenateISVPIE(InfoExtractor):
         'info_dict': {
             'id': 'judiciary031715',
             'ext': 'mp4',
-            'title': 'Integrated Senate Video Player',
+            'title': 'judiciary031715',
             'thumbnail': r're:^https?://.*\.(?:jpg|png)$',
         },
         'params': {
@@ -69,7 +71,8 @@ class SenateISVPIE(InfoExtractor):
         'info_dict': {
             'id': 'intel090613',
             'ext': 'mp4',
-            'title': 'Integrated Senate Video Player'
+            'title': 'intel090613',
+            'thumbnail': r're:^https?://.*\.(?:jpg|png)$',
         }
     }, {
         # From http://www.c-span.org/video/?96791-1
@@ -78,67 +81,83 @@ class SenateISVPIE(InfoExtractor):
     }]
 
     @staticmethod
+    # returns url from an iframe
     def _search_iframe_url(webpage):
         mobj = re.search(
-            r"<iframe[^>]+src=['\"](?P<url>https?://www\.senate\.gov/isvp/?\?[^'\"]+)['\"]",
+            r'''<iframe\b[^>]+\bsrc\s*=\s*(['"])(?P<url>https?://www\.senate\.gov/isvp/?\?(?:(?!\1)\S)+)''',
             webpage)
         if mobj:
             return mobj.group('url')
 
+    # returns stream_number, stream_domain, stream_id, and msl3
     def _get_info_for_comm(self, committee):
-        for entry in self._COMM_MAP:
-            if entry[0] == committee:
-                return entry[1:]
+        return self._COMM_MAP[committee][0:]
 
     def _real_extract(self, url):
+        # smuggled_data may contain a forced title that should be used
         url, smuggled_data = unsmuggle_url(url, {})
-
         qs = compat_parse_qs(re.match(self._VALID_URL, url).group('qs'))
-        if not qs.get('filename') or not qs.get('type') or not qs.get('comm'):
-            raise ExtractorError('Invalid URL', expected=True)
 
-        video_id = re.sub(r'.mp4$', '', qs['filename'][0])
+        # error handling for invalid URL - specify what
+        if not qs.get('comm'):
+            raise ExtractorError('''Invalid URL. Missing committee in query parameters.''', expected=True)
+        if not qs.get('filename'):
+            raise ExtractorError('''Invalid URL. Missing filename in query parameters.''', expected=True)
 
-        webpage = self._download_webpage(url, video_id)
+        committee = qs.get('comm')[0]
+        filename = qs.get('filename')[0]
+        video_id = re.sub(r'\.mp4$', '', filename)
 
-        if smuggled_data.get('force_title'):
+        # there is no point in pulling the title from the webpage since it always defaults to 'Integrated Senate Player'
+        title = filename
+        if smuggled_data:
             title = smuggled_data['force_title']
-        else:
-            title = self._html_search_regex(r'<title>([^<]+)</title>', webpage, video_id)
-        poster = qs.get('poster')
-        thumbnail = poster[0] if poster else None
+        stream_number, stream_domain, stream_id, msl3 = self._get_info_for_comm(committee)
 
-        video_type = qs['type'][0]
-        committee = video_type if video_type == 'arch' else qs['comm'][0]
-        stream_num, domain = self._get_info_for_comm(committee)
+        # the possible locations that the video could be contained at.
+        possible_manifest_urls = [
+            f'https://www-senate-gov-media-srs.akamaized.net/hls/live/{stream_id}/{committee}/{filename}/master.m3u8',
+            f'https://www-senate-gov-msl3archive.akamaized.net/{msl3}/{filename}_1/master.m3u8',
+            f'{stream_domain}/i/{filename}_1@{stream_number}/master.m3u8',
+            f'https://ussenate-f.akamaihd.net/i/{video_id}',
+        ]
 
+
+        # we iterate through the possible locations until we find a hit, that is when formats is filled.
         formats = []
-        if video_type == 'arch':
-            filename = video_id if '.' in video_id else video_id + '.mp4'
-            formats = [{
-                # All parameters in the query string are necessary to prevent a 403 error
-                'url': compat_urlparse.urljoin(domain, filename) + '?v=3.1.0&fp=&r=&g=',
-            }]
-        else:
-            hdcore_sign = 'hdcore=3.1.0'
-            url_params = (domain, video_id, stream_num)
-            f4m_url = '%s/z/%s_1@%s/manifest.f4m?' % url_params + hdcore_sign
-            m3u8_url = '%s/i/%s_1@%s/master.m3u8' % url_params
-            for entry in self._extract_f4m_formats(f4m_url, video_id, f4m_id='f4m'):
-                # URLs without the extra param induce an 404 error
-                entry.update({'extra_param_to_segment_url': hdcore_sign})
-                formats.append(entry)
-            for entry in self._extract_m3u8_formats(m3u8_url, video_id, ext='mp4', m3u8_id='m3u8'):
-                mobj = re.search(r'(?P<tag>(?:-p|-b)).m3u8', entry['url'])
+        for url in possible_manifest_urls:
+            print('!!!')
+            entries = self._extract_m3u8_formats(
+                url,
+                video_id,
+                ext='mp4',
+                m3u8_id='hls',
+                entry_protocol='m3u8_native',
+                fatal=False,
+            )
+            print('|-|-|-|')
+
+            for entry in entries:
+                mobj = re.search(r'(?P<tag>-[pb]).m3u8', entry['url'])
                 if mobj:
                     entry['format_id'] += mobj.group('tag')
                 formats.append(entry)
 
-            self._sort_formats(formats)
+            if formats:
+                break
+
+        self._sort_formats(formats)
+        thumbnail = url_or_none(qs.get('poster', [None])[-1])
+        start_time = parse_duration(qs.get('stt', [None])[-1])
+        stop_time = parse_duration(qs.get('dur', [None])[-1])
+        if stop_time is not None:
+            stop_time += start_time or 0
 
         return {
             'id': video_id,
             'title': title,
             'formats': formats,
             'thumbnail': thumbnail,
+            'start_time': start_time,
+            'stop_time': stop_time,
         }
