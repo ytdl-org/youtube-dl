29e7494495bad404e58033eeca54bf912f28d363
pukkandan <pukkandan.ytdlp@gmail.com>
2022-12-22 02:03:51 +0530

    Lint after merge

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -597,10 +597,7 @@ from .parliamentliveuk import ParliamentLiveUKIE
 from .patreon import PatreonIE
 from .pbs import PBSIE
 from .pearvideo import PearVideoIE
-from .peekvids import (
-    PeekVidsIE,
-    PlayVidsIE,
-)
+from .peekvids import PeekVidsIE, PlayVidsIE
 from .peertube import PeerTubeIE
 from .people import PeopleIE
 from .performgroup import PerformGroupIE
@@ -836,11 +833,7 @@ from .theweatherchannel import TheWeatherChannelIE
 from .thisamericanlife import ThisAmericanLifeIE
 from .thisav import ThisAVIE
 from .thisoldhouse import ThisOldHouseIE
-from .thisvid import (
-    ThisVidIE,
-    ThisVidMemberIE,
-    ThisVidPlaylistIE,
-)
+from .thisvid import ThisVidIE, ThisVidMemberIE, ThisVidPlaylistIE
 from .threeqsdn import ThreeQSDNIE
 from .tiktok import TikTokIE, TikTokUserIE
 from .tinypic import TinyPicIE
