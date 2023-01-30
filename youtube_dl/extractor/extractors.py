a777aeeda0598d18c2aba01a90d7fd4fcb621aaf
Glenn Pavlovic <glimrick@epilitimus.com>
2023-01-29 18:20:46 -0800

    combine all the rumble extractors, add rumble to generic.py, remove UsaWatchdogStory

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1049,11 +1049,7 @@ from .rtve import RTVEALaCartaIE, RTVELiveIE, RTVEInfantilIE, RTVELiveIE, RTVETe
 from .rtvnh import RTVNHIE
 from .rtvs import RTVSIE
 from .ruhd import RUHDIE
-from .rumble import (
-    RumbleEmbedIE,
-    RumblePageIE,
-    RumblePlaylistIE,
-)
+from .rumble import RumbleIE
 from .rutube import (
     RutubeIE,
     RutubeChannelIE,
@@ -1418,10 +1414,7 @@ from .urort import UrortIE
 from .urplay import URPlayIE
 from .usanetwork import USANetworkIE
 from .usatoday import USATodayIE
-from .usawatchdog import (
-    UsaWatchdogStoryIE,
-    UsaWatchdogIE,
-)
+from .usawatchdog import UsaWatchdogIE
 from .ustream import UstreamIE, UstreamChannelIE
 from .ustudio import (
     UstudioIE,
--- youtube_dl/extractor/usawatchdog.py
+++ youtube_dl/extractor/usawatchdog.py
@@ -4,30 +4,9 @@ import re
 
 from .common import InfoExtractor
 
-from .rumble import rumble_embedded_id
-
-
-class UsaWatchdogStoryIE(InfoExtractor):
-    _VALID_URL = r'^https?://(?:www\.)?usawatchdog\.com/(?P<id>[^/]+)'
-    _TEST = {
-        'url': 'https://usawatchdog.com/cv-19-vaccine-warning-cv-19-cure-must-watch-videos/',
-        'md5': 'bf40e20aebca9016ca195534028cbb6f',
-        'info_dict': {
-            'id': 'vcl8gx',
-            'ext': 'mp4',
-            'timestamp': 1617141926,
-            'upload_date': '20210330',
-            'title': u'Vaccine Warning \u2013 CV-19 Cure Must Watch Videos',
-        }}
-
-    def _real_extract(self, url):
-        title = self._match_id(url)
-        embeds = rumble_embedded_id(self._download_webpage(url, title))
-        return embeds[0] if embeds is not None else None
-
 
 class UsaWatchdogIE(InfoExtractor):
-    _VALID_URL = r'^https?://(?:www\.)?usawatchdog\.com/$'
+    _VALID_URL = r'^https?://(?:www\.)?usawatchdog\.com/?$'
     _TEST = {
         'url': 'https://usawatchdog.com/',
         'playlist_mincount': 15,
@@ -40,7 +19,6 @@ class UsaWatchdogIE(InfoExtractor):
         for mobj in re.finditer(r'front-view-title[^<]+<a.+href=["\'](?P<href>https?:(?:www\.)?//usawatchdog.com/[^/]+\/?)[^>]+>(?P<title>[^<]+)',
                                 self._download_webpage(url, 'Site Root')):
             matches.append(self.url_result(mobj.group('href'),
-                           'UsaWatchdogStory', None,
-                           mobj.group('title').encode('utf8')))
+                                           video_title=mobj.group('title').encode('utf8')))
 
         return self.playlist_result(matches, 'USA Watchdog')
