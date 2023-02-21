8b2b336337b83048648ca6810ac80656d44777a5
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2023-02-21 09:07:14 -0800

    Add GooglePhotosIE from tokune's ytdl fork

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -452,6 +452,7 @@ from .go import GoIE
 from .godtube import GodTubeIE
 from .golem import GolemIE
 from .googledrive import GoogleDriveIE
+from .googlephotos import GooglePhotosIE
 from .googlepodcasts import (
     GooglePodcastsIE,
     GooglePodcastsFeedIE,
