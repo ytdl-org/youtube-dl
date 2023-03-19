08599bea55fa6f7624df530bcd6efd388292bc36
dcsena <davisena145@gmail.com>
2023-03-19 18:20:12 -0400

    refactoring

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,9 @@
 import sys
+import platform
 
+print(platform.python_implementation())
 
-if sys.version[0:3] > '3.5' and sys.version[0:3] < '4.0':
+if sys.version[0:3] > '3.5' and sys.version[0:3] < '4.0' and platform.python_implementation() != 'pypy':
     import unittest
     import yourbase
 
