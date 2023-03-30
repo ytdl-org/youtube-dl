ddacbfd35d524726856d05789fdac5f2cfb19dff
Dennis Mo <dennismoyunfei@gmail.com>
2023-03-30 12:54:06 -0400

    update test script

--- devscripts/test-yourbase.sh
+++ devscripts/test-yourbase.sh
@@ -2,10 +2,13 @@
 
 # Only run in docker container
 # Run initial test with nosetest and yourbase
+echo "\n---------Running initial test------------\n\n"
 /app/devscripts/run_tests.sh
 
 # Edit some files
+echo "\n---------Editing files------------\n\n"
 echo "print('Hello World')" >> youtube_dl/extractor/buzzfeed.py
 
 # Run test again
+echo "\n--------Running test again------------\n\n"
 /app/devscripts/run_tests.sh
--- test/__init__.py
+++ test/__init__.py
@@ -7,7 +7,8 @@ import platform
 print("davi logs", sys.version[0:3])
 print("davi logs", platform.python_implementation())
 
-import unittest
-import yourbase
+if sys.version[0:3] == '3.6':
+    import unittest
+    import yourbase
 
-yourbase.attach(unittest)
+    yourbase.attach(unittest)
