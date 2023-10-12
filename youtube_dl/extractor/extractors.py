f677ef143c5ef254af6e03aaa22fc772db1bd0ee
Frank Villaro-Dixon <frank@villaro-dixon.eu>
2023-10-12 23:17:29 +0200

    [unige] Add new extractor
    This enables to extract videos posted to the University of Geneva mediaserver,
    located at https://mediaserver.unige.ch. It supports:
    - Single courses/videos
    - Whole course "playlists" (accessible by clicking on the course's
      title)
    
    Signed-off-by: Frank Villaro-Dixon <frank@villaro-dixon.eu>

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -1427,6 +1427,10 @@ from .ufctv import (
     UFCTVIE,
     UFCArabiaIE,
 )
+from .unige import (
+    UnigeIE,
+    UnigePlaylistIE,
+)
 from .uktvplay import UKTVPlayIE
 from .digiteka import DigitekaIE
 from .dlive import (
