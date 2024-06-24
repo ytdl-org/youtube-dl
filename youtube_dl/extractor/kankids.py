a4737bb7555d8f34a20168ab0a99e0a860727cca
deepspy <pavel.deepspy@gmail.com>
2024-06-24 23:12:03 +0300

    Fixed flake8 errors.

--- youtube_dl/extractor/kankids.py
+++ youtube_dl/extractor/kankids.py
@@ -7,6 +7,7 @@ import re
 CONTENT_DIR = r'/content/kids/'
 DOMAIN = r'kankids.org.il'
 
+
 class KanKidsIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?' +\
         DOMAIN.replace('.', '\.') + CONTENT_DIR +\
@@ -18,7 +19,7 @@ class KanKidsIE(InfoExtractor):
                 '_type': 'playlist',
                 'id': 'p-11732',
                 'title': 'בית ספר לקוסמים',
-                },
+            },
             'playlist_count': 60,
         },
         {
@@ -27,17 +28,17 @@ class KanKidsIE(InfoExtractor):
                 '_type': 'playlist',
                 'id': 'cramel_main',
                 'title': 'כראמל - עונה 1',
-                },
+            },
             'playlist_count': 21,
         },
-        ]
+    ]
 
     def _real_extract(self, url):
         m = super()._match_valid_url(url)
         series_id = m.group('id')
         category = m.group('category')
         playlist_season = m.group('season')
-        
+
         webpage = self._download_webpage(url, series_id)
 
         title_pattern = r'<title>(?P<title>.+) \|'
@@ -54,8 +55,8 @@ class KanKidsIE(InfoExtractor):
             series_id + r'/' +              # Series
             season + r'/' +                 # Season
             r'(?P<id>[0-9]+)/"' +           # Episode
-            r'.+title="(?P<title>.+)"'      # Title
-            , webpage))
+            r'.+title="(?P<title>.+)"',     # Title
+            webpage))
 
         entries = []
         content_dir = r'https://www.' + DOMAIN + content_dir
@@ -65,12 +66,12 @@ class KanKidsIE(InfoExtractor):
                 ie='Generic',
                 video_id=video_id,
                 video_title=title,
-                ))
+            ))
 
         return {
             '_type': 'playlist',
             'id': series_id,
             'title': series_title,
             'entries': entries,
-            }
+        }
 
