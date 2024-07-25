45ecff5db023bb441ab3b9334b6dc5287b287a3b
nicole trinity <nimble.embodiment.8@gmail.com>
2024-07-25 12:31:23 -0400

    bandlab extractor

--- youtube_dl/extractor/extractors.py
+++ youtube_dl/extractor/extractors.py
@@ -98,6 +98,7 @@ from .azmedien import AZMedienIE
 from .baidu import BaiduVideoIE
 from .bandaichannel import BandaiChannelIE
 from .bandcamp import BandcampIE, BandcampAlbumIE, BandcampWeeklyIE
+from .bandlab import BandlabIE, BandlabAlbumOrPlaylistIE
 from .bbc import (
     BBCCoUkIE,
     BBCCoUkArticleIE,
