1dc27e1c3bda9cb8f44b805c89918aa7d11ffcdc
dirkf <fieldhouse@gmx.net>
2025-03-07 19:40:53 +0000

    [JSInterp] Make indexing error handling more conformant * by default TypeError -> undefined, else raise * set allow_undefined=True/False to override

--- youtube_dl/jsinterp.py
+++ youtube_dl/jsinterp.py
@@ -672,14 +672,15 @@ class JSInterpreter(object):
         except Exception as e:
             raise self.Exception('Failed to evaluate {left_val!r:.50} {op} {right_val!r:.50}'.format(**locals()), expr, cause=e)
 
-    def _index(self, obj, idx, allow_undefined=True):
+    def _index(self, obj, idx, allow_undefined=None):
         if idx == 'length' and isinstance(obj, list):
             return len(obj)
         try:
             return obj[int(idx)] if isinstance(obj, list) else obj[compat_str(idx)]
         except (TypeError, KeyError, IndexError) as e:
-            if allow_undefined:
-                # when is not allowed?
+            # allow_undefined is None gives correct behaviour
+            if allow_undefined or (
+                    allow_undefined is None and not isinstance(e, TypeError)):
                 return JS_Undefined
             raise self.Exception('Cannot get index {idx!r:.100}'.format(**locals()), expr=repr(obj), cause=e)
 
